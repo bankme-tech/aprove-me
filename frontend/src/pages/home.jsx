9d11d15425cec4422b3894def766df11fee49363
Larissa Ramos <devlarissasr@gmail.com>
2023-02-08 13:46:57 +0100

    frontend: page home

---
+++ frontend/src/pages/home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Notifications from "../components/Notifications";
+import Navbar from "../components/Navbar";
+
+const home = () => {
+  return (
+    <>
+      <Navbar />
+      <Notifications />
+    </>
+  );
+};
+
+export default home;
