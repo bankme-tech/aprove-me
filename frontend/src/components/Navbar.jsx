3c164f36eb88c0bd0626ff64b5c928254659ba21
Larissa Ramos <devlarissasr@gmail.com>
2023-02-08 14:47:58 +0100

    frontend/navbar: fixing colors and positions

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -14,20 +14,20 @@ const NavBar = () => {
   return (
     <div className="fixed w-full h-28 shadow-xl z-[108]">
       <div className="flex justify-between items-center w-full h-full px-2 2xl:px-16">
-        <div className="p-3">
+        <div className="p-3 ml-6">
           <Image src="/logo.png" alt="logo" width={40} height={40} />
         </div>
         <div>
           <ul className="hidden md:flex">
             <Link href="/">
-              <li className="ml-10 text-sm hover:border-b">Home</li>
+              <li className=" ml-10 text-sm hover:border-b">Home</li>
             </Link>
             <Link href="/">
               <li className="ml-10 text-sm hover:border-b">List</li>
             </Link>
-            <Link href="/">
-              <li className="ml-10 text-sm hover:border-b">Add</li>
-            </Link>
+            <div className="ml-10 mr-10 flex items-center justify-center text-sm hover:border-b">
+              <button className="py-2">Add</button>
+            </div>
           </ul>
           <div onClick={handleNav} className="md:hidden">
             <AiOutlineMenu size={25} />
@@ -35,9 +35,7 @@ const NavBar = () => {
         </div>
       </div>
       <div
-        className={
-          !nav ? "md:hidden fixed left-0 top-0 w-full h-screen " : ""
-        }
+        className={!nav ? "md:hidden fixed left-0 top-0 w-full h-screen " : ""}
       >
         <div
           className={
@@ -48,7 +46,7 @@ const NavBar = () => {
         >
           <div>
             <div className="flex w-full items-center justify-between">
-              <div className="p-3">
+              <div className="p-3 ml-2">
                 <Image src="/logo.png" alt="logo" width={20} height={20} />
               </div>
               <div
