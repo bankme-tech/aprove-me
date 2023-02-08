4334d930cb4e40ec97ec20ed0cda0d7032f951a8
Larissa Ramos <devlarissasr@gmail.com>
2023-02-08 14:52:35 +0100

    backend: changing files default

--- api/src/main.ts
+++ api/src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
-  await app.listen(3000);
+  await app.listen(3003, () => {
+    console.log('Server is running on http://localhost:3003');
+  });
 }
 bootstrap();
