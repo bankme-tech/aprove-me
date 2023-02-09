afe79818ad589f19101b9788890616ba18c9973b
Larissa Ramos <devlarissasr@gmail.com>
2023-02-09 04:27:46 +0100

    backend: fix function receivable

--- api/src/modules/receivable/receivable.service.ts
+++ api/src/modules/receivable/receivable.service.ts
@@ -4,42 +4,31 @@ import { ReceivableDTO } from '../../dtos/receivable.dto';
 
 @Injectable()
 export class ReceivableService {
-    constructor(private prisma: PrismaService) {}
+  constructor(private prisma: PrismaService) {}
 
-    async create(assignorID: string, data: ReceivableDTO) {
-        const receivableExists = await this.prisma.receivable.findFirst({
-            where: {
-                id: data.id,
-            },
-        });
+  async create(assignorID: string, data: ReceivableDTO) {
+    const assignorExists = await this.prisma.assignor.findUnique({
+      where: {
+        id: assignorID,
+      },
+    });
 
-        const assignorExists = await this.prisma.assignor.findUnique({
-            where: {
-                id: assignorID,
-            },
-        });
-
-        if (receivableExists) {
-            throw new Error('Receivable already exists');
-        } else if (!assignorExists) {
-            throw new Error('Assignor not found');
-        }
-
-        const receivable = await this.prisma.receivable.create({
-            data: {
-                value: data.value,
-                emission_date: data.emission_date,
-                assignor: {
-                    connect: {
-                        id: assignorID,
-                    },
-                },
-            },
-
-        });
-
-        return receivable;
+    if (!assignorExists) {
+      throw new Error('Assignor not found');
     }
 
+    const receivable = await this.prisma.receivable.create({
+      data: {
+        value: data.value,
+        emission_date: data.emission_date,
+        assignor: {
+          connect: {
+            id: assignorID,
+          },
+        },
+      },
+    });
 
+    return receivable;
+  }
 }
