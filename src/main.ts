4603c9e293924ee598d23f9adbd3fefa17cb7b02
Guilherme Braga <bragaguilherme152@yahoo.com.br>
2023-02-25 20:50:12 -0300

    feat: add class validators and change loggers

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: ['debug', 'error', 'log', 'warn'],
+  });
   await app.listen(3000);
 }
 bootstrap();
