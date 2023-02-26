dc33a96811a00e4161053d576a7b51bdb6e040c3
Guilherme Braga <bragaguilherme152@yahoo.com.br>
2023-02-26 11:07:40 -0300

    feat: create login dto

---
+++ src/auth/dtos/user.dto.ts
@@ -0,0 +1,10 @@
+import { IsEmail, IsNotEmpty } from 'class-validator';
+import { MessagesHelper } from '../helpers/messages.helper';
+
+export class LoginDto {
+  @IsEmail({}, { message: MessagesHelper.AUTH_LOGIN_NOT_FOUND })
+  login: string;
+
+  @IsNotEmpty({ message: MessagesHelper.AUTH_PASSWORD_NOT_FOUND })
+  password: string;
+}
