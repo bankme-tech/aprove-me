3885d843b7faed06473d0c471a3da7503a930911
Guilherme Braga <bragaguilherme152@yahoo.com.br>
2023-02-26 20:55:21 -0300

    fix: verify if email already registered and complete login function to find user by email and check password

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,20 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, BadRequestException } from '@nestjs/common';
 import { User, Prisma } from '@prisma/client';
 import { PrismaService } from 'src/prisma.service';
 import { LoginDto } from './dtos/login.dto';
 import { RegisterDto } from './dtos/register.dto';
 import * as CryptoJS from 'crypto-js';
+import { MessagesHelper } from './helpers/messages.helper';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
 
-  login(dto: LoginDto) {
-    return dto;
+  async login(dto: LoginDto) {
+    const user = await this.prisma.user.findUnique({
+      where: { email: dto.email },
+    });
+    if (!user) {
+      throw new BadRequestException(
+        MessagesHelper.AUTH_PASSWORD_OR_LOGIN_NOT_FOUND,
+      );
+    }
+    if (user.password === dto.password) {
+      //Gerar o token e passar o dto como parâmetro
+    }
   }
 
   async register(dto: RegisterDto): Promise<User> {
     console.log('dto', dto);
+
+    if (await this.verifyExistentUser(dto.email)) {
+      throw new BadRequestException(MessagesHelper.REGISTER_EMAIL_FOUND);
+      //Implementar um auto-login
+    }
 
     dto.password = CryptoJS.AES.encrypt(
       dto.password,
