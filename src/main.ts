b9e800b63df7f44329fc4a3f4af7fefa15800808
Guilherme Braga <bragaguilherme152@yahoo.com.br>
2023-02-25 21:00:35 -0300

    feat: enable ValidationPipe

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -7,6 +9,12 @@ async function bootstrap() {
   });
 
   app.enableCors();
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
 
   await app.listen(3000);
 }
