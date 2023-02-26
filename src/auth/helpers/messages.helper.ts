8d1aaad3124f6d35310db24687efbdba6cb04889
Guilherme Braga <bragaguilherme152@yahoo.com.br>
2023-02-26 09:54:18 -0300

    feat: create validation messages to authentication

---
+++ src/auth/helpers/messages.helper.ts
@@ -0,0 +1,5 @@
+export const MessagesHelper = {
+  AUTH_LOGIN_NOT_FOUND: 'Não autorizado',
+  AUTH_PASSWORD_NOT_FOUND: 'Parece que a sua senha não foi cadastrada',
+  AUTH_PASSWORD_OR_LOGIN_NOT_FOUND: 'Email e/ou senha não encontrado!',
+};
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['debug', 'error', 'log', 'warn'],
