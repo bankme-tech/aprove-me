85c526ea786cf65067a70f5f02f479e5e1981224
Guilherme Braga <bragaguilherme152@yahoo.com.br>
2023-02-27 13:25:09 -0300

    feat: import assignor module on main module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { APP_GUARD } from '@nestjs/core';
 import { AuthModule } from './auth/auth.module';
 import { JwtAuthGuard } from './auth/guards/jwt.guard';
 import { PayableModule } from './payables/payables.module';
+import { AssignorModule } from './assignors/assignor.module';
 
 @Module({
-  imports: [AuthModule, PayableModule],
+  imports: [AuthModule, PayableModule, AssignorModule],
   controllers: [],
   providers: [{ provide: APP_GUARD, useClass: JwtAuthGuard }],
 })
