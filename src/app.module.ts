e503d964d1c48eb85749f00b8b64bed8ebdaba99
Guilherme Braga <bragaguilherme152@yahoo.com.br>
2023-02-27 12:59:27 -0300

    feat: import payable module on main module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { APP_GUARD } from '@nestjs/core';
 import { AuthModule } from './auth/auth.module';
 import { JwtAuthGuard } from './auth/guards/jwt.guard';
+import { PayableModule } from './payables/payables.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, PayableModule],
   controllers: [],
   providers: [{ provide: APP_GUARD, useClass: JwtAuthGuard }],
 })
