1f727d6c5e7d541fd3d7e7578da9b330f3d4bdf6
Guilherme Braga <bragaguilherme152@yahoo.com.br>
2023-02-27 13:23:25 -0300

    feat: create assignor messages

---
+++ src/assignors/helpers/messages.helper.ts
@@ -0,0 +1,7 @@
+export const MessagesHelper = {
+  NULLABLE_FIELD: 'O campo a seguir não pode ser nulo :',
+  VERIFY_ID_TYPE: 'O ID deve ser do tipo UUID',
+  VERIFY_LENGHT_STRING:
+    'O campo a seguir está excedendo o limite de caracteres permitidos :',
+  CHECK_EMAIL: 'O e-mail está inválido',
+};
