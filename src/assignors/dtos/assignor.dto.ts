fde27bc3e0180db8af3bc69e4bee34ed8e7187d5
Guilherme Braga <bragaguilherme152@yahoo.com.br>
2023-02-27 13:23:13 -0300

    feat: create assignor dto

---
+++ src/assignors/dtos/assignor.dto.ts
@@ -0,0 +1,28 @@
+import { IsEmail, IsNotEmpty, MaxLength } from 'class-validator';
+import { MessagesHelper } from '../helpers/messages.helper';
+
+export class AssignorDto {
+  @IsNotEmpty({ message: MessagesHelper.NULLABLE_FIELD + '$property' })
+  id: string;
+
+  @IsNotEmpty({ message: MessagesHelper.NULLABLE_FIELD + '$property' })
+  @MaxLength(30, { message: MessagesHelper.VERIFY_LENGHT_STRING + '$property' })
+  document: string;
+
+  @IsNotEmpty({ message: MessagesHelper.NULLABLE_FIELD + '$property' })
+  @MaxLength(140, {
+    message: MessagesHelper.VERIFY_LENGHT_STRING + '$property',
+  })
+  @IsEmail({}, { message: MessagesHelper.CHECK_EMAIL })
+  email: string;
+
+  @IsNotEmpty({ message: MessagesHelper.NULLABLE_FIELD + '$property' })
+  @MaxLength(20, { message: MessagesHelper.VERIFY_LENGHT_STRING + '$property' })
+  phone: string;
+
+  @IsNotEmpty({ message: MessagesHelper.NULLABLE_FIELD + '$property' })
+  @MaxLength(140, {
+    message: MessagesHelper.VERIFY_LENGHT_STRING + '$property',
+  })
+  name: string;
+}
