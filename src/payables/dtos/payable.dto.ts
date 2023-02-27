3ce2daffc0c2cedfbf2a0c08922bb4a75534b83a
Guilherme Braga <bragaguilherme152@yahoo.com.br>
2023-02-27 12:58:09 -0300

    feat: create payable dto

---
+++ src/payables/dtos/payable.dto.ts
@@ -0,0 +1,22 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  MinLength,
+  MaxLength,
+  Matches,
+} from 'class-validator';
+import { MessagesHelper } from '../helpers/messages.helper';
+
+export class PayableDto {
+  @IsNotEmpty({ message: MessagesHelper.NULLABLE_FIELD + '$property' })
+  id: string;
+
+  @IsNotEmpty({ message: MessagesHelper.NULLABLE_FIELD + '$property' })
+  value: number;
+
+  @IsNotEmpty({ message: MessagesHelper.NULLABLE_FIELD + '$property' })
+  emission_date: Date;
+
+  @IsNotEmpty({ message: MessagesHelper.NULLABLE_FIELD + '$property' })
+  id_assignor: string;
+}
