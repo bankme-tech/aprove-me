bd9fa6672342e936585182832a92e6ba5a6b2d9c
Thiago-F <thiago_fialho150@hotmail.com>
2023-06-22 17:04:19 -0300

    feat: add validation

--- server/src/modules/assignor/dto/create-assignor.dto.ts
+++ server/src/modules/assignor/dto/create-assignor.dto.ts
@@ -1,6 +1,24 @@
+import { IsEmail, IsNotEmpty, IsString, Length, Matches, Max } from "class-validator";
+
+// TODO - values to constant folder
 export class CreateAssignorDto {
+    @IsNotEmpty()
+    @IsString()
+    @Length(3, 30)
     document: string;
+
+    @IsNotEmpty()
+    @IsEmail()
+    @Length(3, 140)
     email: string;
+
+    @IsNotEmpty()
+    @Length(3, 20)
     phone: string;
+
+    @IsNotEmpty()
+    @Matches(/^(?=.*\s)[A-Za-zÀ-ÖØ-öø-ÿ\s']+$/, { 
+        message: 'Name must be with at least two words with valid characters.',
+    })
     name: string;
 }
