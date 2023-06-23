bbd1047c37f9f8e362202962ee209f39b316fb4d
Thiago-F <thiago_fialho150@hotmail.com>
2023-06-23 16:05:15 -0300

    chore: implement prisma seed

---
+++ server/prisma/seed.ts
@@ -0,0 +1,28 @@
+import { PrismaClient } from '@prisma/client'
+import * as bcrypt from 'bcrypt';
+
+const prisma = new PrismaClient()
+
+async function main() {
+    const adminUser = await prisma.assignor.create({
+        data: {
+            email: 'admin@admin.com',
+            phone: '11999999999',
+            document: '12345678910',
+            name: 'Admin',
+            password: await bcrypt.hash('admin', 10),
+            createdBy: 'admin',
+            updatedBy: 'admin'
+        },
+    });
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
