6aad206c5f153c46042cce4cf65bfda999a227fc
Thiago-F <thiago_fialho150@hotmail.com>
2023-06-23 15:17:34 -0300

    test: auth service and controller

--- server/src/modules/auth/auth.controller.spec.ts
+++ server/src/modules/auth/auth.controller.spec.ts
@@ -3,18 +3,63 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
 describe('AuthController', () => {
-  let controller: AuthController;
+  let sut: AuthController;
+  let authService: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
-      providers: [AuthService],
+      providers: [
+        {
+          provide: AuthService,
+          useValue: {
+            login: jest.fn().mockResolvedValue({ access_token: 'access_token' })
+          }
+        }
+      ],
     }).compile();
 
-    controller = module.get<AuthController>(AuthController);
+    sut = module.get<AuthController>(AuthController);
+    authService = module.get<AuthService>(AuthService);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(sut).toBeDefined();
+  });
+
+  describe('login', () => {
+    it('should call service with correct values', async () => {
+      const loginSpy = jest.spyOn(authService, 'login')
+
+      await sut.login(
+        {
+          email: 'any_email@mail.com',
+          password: 'any_password'
+        }, 
+        {
+          user: {id: 1}
+        }
+      )
+
+      expect(loginSpy).toHaveBeenCalledWith({
+        id: 1
+      })
+    });
+
+    it('should return a token on success', async () => {
+      const response = await sut.login(
+        {
+          email: 'any_email@mail.com',
+          password: 'any_password'
+        }, 
+        {
+          id: 1
+        }
+      )
+
+      expect(response).toEqual({
+        access_token: 'access_token'
+      })
+    });
   });
 });
--- server/src/modules/auth/auth.service.spec.ts
+++ server/src/modules/auth/auth.service.spec.ts
@@ -1,18 +1,98 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { AssignorService } from '../assignor/assignor.service';
+import { BcryptAdapter } from '../../infra/bcrypt/bcrypt-adapter';
+import { JwtService } from '@nestjs/jwt';
 
+const makeFakeAssignor = () => ({
+  id: 1, 
+  name: 'any_name',
+  email: 'any_email@mail.com'
+})
 describe('AuthService', () => {
-  let service: AuthService;
+  let sut: AuthService;
+  let assignorService: AssignorService;
+  let bcryptAdapter: BcryptAdapter;
+  let jwtService: JwtService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        {
+          provide: AssignorService,
+          useValue: {
+            findOneByEmail: jest.fn().mockResolvedValue({...makeFakeAssignor(), password: 'encrypted_password'})
+          }
+        },
+        {
+          provide: BcryptAdapter,
+          useValue: {
+            compare: jest.fn().mockResolvedValue(true)
+          }
+        },
+        {
+          provide: JwtService,
+          useValue: {
+            sign: jest.fn().mockReturnValue('access_token')
+          }
+        }
+      ],
     }).compile();
 
-    service = module.get<AuthService>(AuthService);
+    sut = module.get<AuthService>(AuthService);
+    assignorService = module.get<AssignorService>(AssignorService);
+    bcryptAdapter = module.get<BcryptAdapter>(BcryptAdapter);
+    jwtService = module.get<JwtService>(JwtService);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(sut).toBeDefined();
+  });
+
+  describe('validateUser', () => {
+    it('should call service with correct values', async () => {
+      const findOneSpy = jest.spyOn(assignorService, 'findOneByEmail')
+
+      await sut.validateUser('any_email@mail.com', 'any_password')
+      expect(findOneSpy).toHaveBeenCalledWith({ email: 'any_email@mail.com' })
+    });
+
+    it('should call bcrypt with correct values', async () => {
+      const compareSpy = jest.spyOn(bcryptAdapter, 'compare')
+      await sut.validateUser('any_email@mail.com', 'any_password')
+      expect(compareSpy).toHaveBeenCalledWith('any_password', 'encrypted_password')
+    });
+
+    it('should return null if passwords does not match', async () => {
+      jest.spyOn(bcryptAdapter, 'compare').mockResolvedValueOnce(false)
+      const result = await sut.validateUser('any_email@mail.com', 'any_password')
+      expect(result).toBeNull()
+    });
+
+    it('should return a assignor entity without password on success', async () => {
+      const result = await sut.validateUser('any_email@mail.com', 'any_password')
+      expect(result).toEqual(makeFakeAssignor())
+    });
+  });
+
+  describe('login', () => {
+    it('should call jwtService with correct values', async () => {
+      const signSpy = jest.spyOn(jwtService, 'sign')
+      await sut.login(makeFakeAssignor())
+      expect(signSpy).toHaveBeenCalledWith({
+        sub: makeFakeAssignor().id,
+        email: makeFakeAssignor().email,
+      }, {
+        secret: expect.any(String)
+      })
+    });
+    
+    it('should return a token on success', async () => {
+      const response = await sut.login(makeFakeAssignor())
+      expect(response).toEqual({
+        access_token: 'access_token'
+      })
+    });
   });
 });
