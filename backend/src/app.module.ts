9054f8c16e0bf995ee09cf58fabb03c9ca51af30
Lourenco Werneck <lourencovw@gmail.com>
2023-06-24 02:48:33 -0300

    Set up env variables

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
+import { PayablesModule } from './payables/payables.module';
+import { AssignorsModule } from './assignors/assignors.module';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot(), UsersModule, PayablesModule, AssignorsModule],
   controllers: [AppController],
   providers: [AppService],
 })
