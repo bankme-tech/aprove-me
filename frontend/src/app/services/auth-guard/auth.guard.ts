5944a8165299311cb49417b9c1077d5d82d1f5d2
Lourenco Werneck <lourencovw@gmail.com>
2023-06-26 10:13:53 -0300

    feat(auth): add auth guard

---
+++ frontend/src/app/services/auth-guard/auth.guard.ts
@@ -0,0 +1,25 @@
+import { inject } from '@angular/core';
+import { ActivatedRoute, CanActivateFn, Router } from '@angular/router';
+import { JwtService } from '../jwt/jwt.service';
+
+export const authGuard: CanActivateFn = (route, state) => {
+  const jwtService = inject(JwtService)
+
+  const user = jwtService.getLogin()
+  const isExpired = jwtService.isTokenExpired()
+  
+  console.log({user, isExpired});
+  
+  
+  if (jwtService.getLogin()) {
+    if (jwtService.isTokenExpired()) {
+      inject(Router).navigate([''])
+      return false 
+    } else {
+      return true;
+    }
+  } else {
+    return false 
+  }
+  return true
+}
