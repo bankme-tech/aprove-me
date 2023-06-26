872474350d2b0185d15170538a8880d12d3fa9cd
Lourenco Werneck <lourencovw@gmail.com>
2023-06-25 23:14:57 -0300

    test(assignors): add assignors unit tests

--- backend/src/assignors/assignors.controller.spec.ts
+++ backend/src/assignors/assignors.controller.spec.ts
@@ -1,20 +1,95 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AssignorsController } from './assignors.controller';
 import { AssignorsService } from './assignors.service';
+import { PrismaService } from '../prisma/prisma.service';
+
+import { Assignor } from '@prisma/client';
+import { HttpException, HttpStatus } from '@nestjs/common';
 
 describe('AssignorsController', () => {
-  let controller: AssignorsController;
+  let assignorsController: AssignorsController;
+  let assignorsService: AssignorsService;
+  const result = {
+    id: 'test',
+    document: 'test',
+    email: 'test',
+    phone: 'test',
+    name: 'test',
+    payables: [{
+      id: 'test',
+      value: 1,
+      emissionDate: new Date(),
+      assignorId: 'test'
+    }]
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AssignorsController],
-      providers: [AssignorsService],
+      providers: [AssignorsService, PrismaService],
     }).compile();
 
-    controller = module.get<AssignorsController>(AssignorsController);
+    assignorsService = module.get<AssignorsService>(AssignorsService);
+    assignorsController = module.get<AssignorsController>(AssignorsController);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(assignorsController).toBeDefined();
   });
+  describe('create', () => {
+    it('should return an Assignor object', async () => {
+      const result: Assignor = {
+        id: 'test',
+        document: 'test',
+        email: 'test',
+        phone: 'test',
+        name: 'test',
+      };
+      jest.spyOn(assignorsService, 'create').mockResolvedValue(result);
+
+      expect(await assignorsController.create(result)).toBe(result);
+    });
+  });
+  describe('findAll', () => {
+    it('should return an array of Assignors\' objects', async () => {
+      jest.spyOn(assignorsService, 'findAll').mockResolvedValue([result]);
+
+      expect(await assignorsController.findAll()).toStrictEqual([result]);
+    });
+  });
+  describe('findOne', () => {
+    it('should return an Assignor object', async () => {
+
+      jest.spyOn(assignorsService, 'findOne').mockResolvedValue(result);
+
+      expect(await assignorsController.findOne('test')).toBe(result);
+    });
+  });
+  describe('update', () => {
+    it('should return an Assignor object', async () => {
+      jest.spyOn(assignorsService, 'update').mockResolvedValue(result);
+
+      expect(await assignorsController.update('test', result)).toBe(result);
+    });
+  });
+  describe('remove', () => {
+    it('should throw NotFoundException', async () => {
+      jest.spyOn(assignorsService, 'remove').mockResolvedValue(result);
+      jest.spyOn(assignorsService, 'checkIfExists').mockResolvedValue(true);
+
+      expect(await assignorsController.remove('test')).toBe(result);
+    });
+    it('should return an Assignor object', async () => {
+
+      jest.spyOn(assignorsService, 'checkIfExists').mockResolvedValue(false);
+      try {
+        await assignorsController.remove('test')
+      } catch (error) {        
+        expect(error).toBeInstanceOf(HttpException)
+        expect(error.message).toBe('Not found')
+        expect(error.status).toBe(HttpStatus.NOT_FOUND)
+      }
+    });
+  });
+
 });
--- backend/src/assignors/assignors.service.spec.ts
+++ backend/src/assignors/assignors.service.spec.ts
@@ -1,18 +1,78 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AssignorsService } from './assignors.service';
+import { faker } from '@faker-js/faker/locale/pt_BR';
+import { PrismaService } from '../prisma/prisma.service';
 
 describe('AssignorsService', () => {
-  let service: AssignorsService;
+  let assignorsService: AssignorsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AssignorsService],
+      providers: [AssignorsService, PrismaService],
     }).compile();
 
-    service = module.get<AssignorsService>(AssignorsService);
+    assignorsService = module.get<AssignorsService>(AssignorsService);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(assignorsService).toBeDefined();
+  });
+  
+
+  it('should create a Assignor',async () => {
+    const expectedResponse = {
+      document: faker.helpers.replaceSymbolWithNumber('###########'),
+      email: faker.internet.email(),
+      phone: faker.phone.number('###########'),
+      name: faker.person.fullName()
+    }
+    const resp = await assignorsService.create(expectedResponse);
+    
+    expect(resp).toEqual(expect.objectContaining(expectedResponse));
+  });
+
+  it('should index Assignors',async () => {
+    const resp = await assignorsService.findAll();
+
+    expect(Object.keys(resp[0]))
+      .toStrictEqual(["id", "document", "email", "phone", "name", 'payables'])
+  });
+
+  it('should show an Assignor',async () => {
+    const expectedResponse = {
+      document: faker.helpers.replaceSymbolWithNumber('###########'),
+      email: faker.internet.email(),
+      phone: faker.phone.number('###########'),
+      name: faker.person.fullName()
+    }
+    const createdAssginor = await assignorsService.create(expectedResponse);
+    const resp = await assignorsService.findOne({id: createdAssginor.id});
+    
+    expect(resp).toEqual(expect.objectContaining(expectedResponse));
+  });
+
+  it('should update an Assignor',async () => {
+    const expectedResponse = {
+      document: faker.helpers.replaceSymbolWithNumber('###########'),
+      email: faker.internet.email(),
+      phone: faker.phone.number('###########'),
+      name: faker.person.fullName()
+    }
+    const createdAssginor = await assignorsService.create(expectedResponse);
+    
+    expect(createdAssginor).toEqual(expect.objectContaining(expectedResponse));
+  });
+
+  it('should delete an Assignor',async () => {
+    const expectedResponse = {
+      document: faker.helpers.replaceSymbolWithNumber('###########'),
+      email: faker.internet.email(),
+      phone: faker.phone.number('###########'),
+      name: faker.person.fullName()
+    }
+    const createdAssginor = await assignorsService.create(expectedResponse);
+    const resp = await assignorsService.remove({id:createdAssginor.id});
+    
+    expect(resp).toEqual(expect.objectContaining(expectedResponse));
   });
 });
