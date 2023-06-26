978a7a7a4efcd360c5875762cd07be47c40b2ad2
Lourenco Werneck <lourencovw@gmail.com>
2023-06-25 23:04:51 -0300

    feat(project): set up jwt

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { Public } from './users/decorators/public.decorator';
 
 @Injectable()
 export class AppService {
+  @Public()
   getHello(): string {
     return 'Hello World!';
   }
--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { JwtModule, JwtService } from '@nestjs/jwt';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthGuard } from './auth/auth.guard';
+import { PrismaService } from '../prisma/prisma.service';
+
 
 @Module({
+  imports: [
+    JwtModule.register({
+      global: true,
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '60s' },
+    })
+  ],
   controllers: [UsersController],
-  providers: [UsersService]
+  providers: [
+    UsersService,
+    { provide: APP_GUARD, useClass: AuthGuard },
+    JwtService,
+    PrismaService
+  ]
 })
-export class UsersModule {}
+export class UsersModule { }
