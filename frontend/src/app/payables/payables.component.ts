c8c8729908e8b8ceb9b7ad2b11a3f2aa414fb688
Lourenco Werneck <lourencovw@gmail.com>
2023-06-26 10:11:04 -0300

    feat(payable): add component

---
+++ frontend/src/app/payables/payables.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-payables',
+  templateUrl: './payables.component.html',
+  styleUrls: ['./payables.component.scss']
+})
+export class PayablesComponent {
+
+}
