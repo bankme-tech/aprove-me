11bb4c35f52668b0e670044a4c91993cbcfc11ac
Lourenco Werneck <lourencovw@gmail.com>
2023-06-28 05:40:21 -0300

    feat(edit-payable): add component

--- frontend/src/app/payables/edit-payables/edit-payables.component.ts
+++ frontend/src/app/payables/edit-payables/edit-payables.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ApiService, IAssignor, IPayable } from 'src/app/services/api/api.service';
 
 @Component({
   selector: 'app-edit-payables',
@@ -6,5 +10,51 @@ import { Component } from '@angular/core';
   styleUrls: ['./edit-payables.component.scss']
 })
 export class EditPayablesComponent {
+  payableForm = new FormGroup({
+    value: new FormControl('', [Validators.required]),
+    assignorId: new FormControl('', [Validators.required]),
+  });
+  payable: IAssignor[] = []
+  payableId: string = ''
+  assignors: IAssignor[] = []
 
+  constructor(
+    private api: ApiService,
+    private router: Router,
+    private snack: MatSnackBar,
+    private route: ActivatedRoute
+
+  ) {
+    this.payableId = this.route.snapshot.paramMap.get('id') || ''
+    this.start(this.payableId)
+  }
+
+  start(id: string) {
+    this.api.getAssignors().subscribe((assignors: IAssignor[]) => {
+      this.assignors = assignors
+    })
+    this.api.getPayable(id).subscribe((payable: IPayable) => {
+      this.payableForm.get('value')?.setValue(String(payable.value))
+      this.payableForm.get('assignorId')?.setValue(String(payable.assignorId))
+    })
+  }
+
+  onSubmit(): void {
+    const { value, assignorId } = this.payableForm.value
+
+    if (this.payableForm.invalid) {
+      return
+    }
+
+    const payload = {
+      value: Number(value),
+      assignorId: assignorId || '',
+    }
+    this.api
+      .updatePayable(this.payableId, payload)
+      .subscribe((item: IPayable) => {
+        this.snack.open('Payable updated', 'Close')
+        this.router.navigate(['/payables'])
+      });
+  }
 }
