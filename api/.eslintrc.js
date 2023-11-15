aa43aeecfbc16f8cabc82c177a06f6a232040c08
Fernando Lima <fernando.lima@luizalabs.com>
2023-11-15 13:49:17 -0300

    build: install husky, jest and eslint packages to improve development

--- api/.eslintrc.js
+++ api/.eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['@typescript-eslint/eslint-plugin',  'unused-imports', 'import'],
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
@@ -21,5 +21,7 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'unused-imports/no-unused-imports': 'error',
+    'import/no-duplicates': 'error',
   },
 };
