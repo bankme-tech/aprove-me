776c4c0fd4c56657d4cf1e6e44dfe77ef4711248
natividadesusana <susanajdsn@gmail.com>
2023-11-16 12:05:05 -0300

    feat: creating assignor module

---
+++ backend/src/assignor/assignor.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { AssignorService } from './assignor.service';
+import { AssignorController } from './assignor.controller';
+import { AssignorRepository } from './assignor.repository';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Module({
+  controllers: [AssignorController],
+  providers: [AssignorService, AssignorRepository, PrismaService],
+})
+export class AssignorModule {}
