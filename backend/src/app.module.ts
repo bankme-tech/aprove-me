09af2cf868af7eef075648c248b5a0eeecdede8e
natividadesusana <susanajdsn@gmail.com>
2023-11-16 11:48:53 -0300

    feat: adding imports

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { PayableModule } from './payable/payable.module';
+import { AssignorModule } from './assignor/assignor.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, PayableModule, AssignorModule],
   controllers: [AppController],
   providers: [AppService],
 })
