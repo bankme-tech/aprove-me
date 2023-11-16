1444b9cc6d0b53d5d96e32ac20a6ae0fd1f51e02
natividadesusana <susanajdsn@gmail.com>
2023-11-16 12:10:43 -0300

    feat: creating jest config file

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  moduleDirectories: ["node_modules", "src"],
+  transform: {
+    ".+\\.ts$": "ts-jest",
+  },
+  testMatch: ["<rootDir>/tests/**/*.spec.ts"],
+};
