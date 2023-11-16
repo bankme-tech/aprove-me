f1d303b2a5ea7d4b59a4f5969f197e9abc4e3812
natividadesusana <susanajdsn@gmail.com>
2023-11-16 14:05:45 -0300

    fix: path assignor module

--- backend/src/assignor/assignor.module.ts
+++ backend/src/assignor/assignor.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AssignorService } from './assignor.service';
 import { AssignorController } from './assignor.controller';
 import { AssignorRepository } from './assignor.repository';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Module({
   controllers: [AssignorController],
