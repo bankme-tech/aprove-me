4032b8618690589030c0408794f692bc990f6cc5
natividadesusana <susanajdsn@gmail.com>
2023-11-17 02:43:49 -0300

    chore: adding modules

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,9 +4,11 @@ import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { PayableModule } from './payable/payable.module';
 import { AssignorModule } from './assignor/assignor.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [PrismaModule, PayableModule, AssignorModule],
+  imports: [PrismaModule, PayableModule, AssignorModule, AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
