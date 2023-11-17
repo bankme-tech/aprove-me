31692ff292979eedf5fd4ee77ad0eb976540efd2
Fernando Lima <fernando.lima@luizalabs.com>
2023-11-17 11:49:39 -0300

    test: user service

--- api/src/modules/users/test/users.service.spec.ts
+++ api/src/modules/users/test/users.service.spec.ts
@@ -1,18 +1,33 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from '../users.service';
+import { mock } from 'jest-mock-extended';
+import { IUserRepository } from '../interface/users-repository.interface';
+import { UsersRepository } from '../../../infra/database/prisma/users.repository';
 
 describe('UsersService', () => {
   let service: UsersService;
+  const userRepositoryMock = mock<IUserRepository>();
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        { provide: UsersRepository, useValue: userRepositoryMock },
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('findByUsername()', () => {
+    it('should call UsersService with success and correct params', () => {
+      // ACT
+      service.findByUsername('username');
+
+      // ASSERT
+      expect(userRepositoryMock.findByUsername).toHaveBeenCalledWith(
+        'username',
+      );
+    });
   });
 });
