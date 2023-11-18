ee65436af4b44150943cd9bb6a7184a3a05e3a10
Fernando Lima <fernando.lima@luizalabs.com>
2023-11-18 15:25:08 -0300

    feat: create footer

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import '../styles/footer.css';
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <p>
+      Desenvolvido por Fernando Lima - Teste técnico para a Bankme<br /><br />
+      Bankme S.A.<br /><br />
+      CNPJ 39.158.212/0001-37 <br /><br />
+      Endereço: Avenida Maringá, nº 1765 - Piso Superior - Londrina/PR</p>
+    </footer>
+  );
+}
