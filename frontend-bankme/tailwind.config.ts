c396690bc4f5d5c83fcb6ee5276454c651c7262e
Daiane Bolzan <dainha85@gmail.com>
2023-11-21 18:53:54 -0300

    fix: update tailwind config to fix bug

--- frontend-bankme/tailwind.config.ts
+++ frontend-bankme/tailwind.config.ts
@@ -1,20 +1,25 @@
-import type { Config } from 'tailwindcss'
+/** @type {import('tailwindcss').Config} */
 
-const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
-}
-export default config
+module.exports = {
+	content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			backgroundImage: {
+				"gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+				"gradient-conic":
+					"conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+			},
+			colors: {
+				"base-green": "rgb(160, 201, 14)",
+				"base-blue": "rgb(0, 95, 254)",
+				"light-blue": "rgb(125, 246, 255)",
+				"medium-blue": "rgb(63, 241, 254)",
+			},
+		},
+	},
+	plugins: [],
+};
