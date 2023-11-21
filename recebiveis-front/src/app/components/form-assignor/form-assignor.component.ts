5313f6199eb7f80f21e752b0335096eb1b4428b9
tiagoGIM <herique.sa@gmail.com>
2023-11-21 05:01:38 -0300

    feat: input para reusar o formulario para editar existente

--- recebiveis-front/src/app/components/form-assignor/form-assignor.component.ts
+++ recebiveis-front/src/app/components/form-assignor/form-assignor.component.ts
@@ -1,5 +1,6 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs/internal/Observable';
 import { Assignor } from 'src/app/shared/interfaces/payables';
 
 @Component({
@@ -9,14 +10,27 @@ import { Assignor } from 'src/app/shared/interfaces/payables';
 })
 export class FormAssignorComponent {
   @Output() createAssignorEvent = new EventEmitter<Assignor>();
-  
-  assignor: FormGroup = new FormGroup({
-    document: new FormControl('', Validators.required),
-    name: new FormControl('', Validators.required),
-    phone: new FormControl('',Validators.required),
-    email: new FormControl('',Validators.email),
-  });
+  @Input() assignor$: Observable<Assignor> | undefined;
 
+ 
+  assignor: FormGroup = new FormGroup({});
+
+  ngOnInit(): void {
+    this.assignor = new FormGroup({
+      document: new FormControl('', Validators.required),
+      name: new FormControl('', Validators.required),
+      phone: new FormControl('', Validators.required),
+      email: new FormControl('', Validators.email),
+    });
+
+    // Se assignor$ não for nulo, inscreva-se para atualizações
+    if (this.assignor$) {
+      this.assignor$.subscribe((assignor) => {
+        // Atualiza o formulário quando o assignor$ emite um novo valor
+        this.assignor.patchValue(assignor);
+      });
+    }
+  }
   createAssignor(){
     let assignorPayload : Assignor = {...this.assignor.value}
     this.createAssignorEvent.emit(assignorPayload);
