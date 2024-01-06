bf4d1aed2d2c1bc21aacb226cc5baebc0744d2b4
Rafael Correa <rafael.chaves98@hotmail.com>
2024-01-06 14:05:32 -0300

    [fix] payable schema

--- apps/backend/src/domains/payable/payable.schema.ts
+++ apps/backend/src/domains/payable/payable.schema.ts
@@ -1,15 +1,18 @@
 import { z } from 'zod';
 
-export const createCatSchema = z.object({
-  id: z.string().uuid(),
-  value: z.number(),
-  emissionDate: z.date(),
-  assignor: z.object({
-    document: z.string().max(30),
-    email: z.string().max(140),
-    phone: z.string().max(20),
-    name: z.string().max(140),
-  }),
-});
+export const createPayableSchema = z
+  .object({
+    value: z.number(),
+    emissionDate: z.string().datetime(),
+    assignor: z
+      .object({
+        document: z.string().max(30),
+        email: z.string().max(140),
+        phone: z.string().max(20),
+        name: z.string().max(140),
+      })
+      .required(),
+  })
+  .required();
 
-export type CreateCatDto = z.infer<typeof createCatSchema>;
+export type CreatePayableDto = z.infer<typeof createPayableSchema>;
