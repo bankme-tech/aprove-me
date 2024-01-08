42d3c19acb8f208372a9c09bb53a5f7e2677dbe3
Rafael Correa <rafael.chaves98@hotmail.com>
2024-01-08 17:50:45 -0300

    [chore] added root path to public auth

--- apps/backend/src/domains/app/app.controller.ts
+++ apps/backend/src/domains/app/app.controller.ts
@@ -1,11 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Public } from 'src/decorators/public.decorator';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @Public()
   version() {
     return this.appService.version();
   }
