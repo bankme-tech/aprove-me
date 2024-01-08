9f762cc0f0e11265fecf54c1d43b19542d38de67
Rafael Correa <rafael.chaves98@hotmail.com>
2024-01-08 15:35:28 -0300

    [feat] updated auth service to use database

--- apps/backend/src/domains/auth/auth.service.ts
+++ apps/backend/src/domains/auth/auth.service.ts
@@ -1,12 +1,47 @@
-import { Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcryptjs';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import { PrismaService } from 'src/services/prisma.service';
 
 @Injectable()
 export class AuthService {
-  constructor(private jwt: JwtService) {}
+  constructor(
+    private jwt: JwtService,
+    private prisma: PrismaService,
+  ) {}
+
+  async signUp(login: string, password: string) {
+    const previous = await this.prisma.user.findUnique({ where: { login } });
+
+    if (previous) {
+      throw new BadRequestException([
+        { code: 'user_already_exists', message: 'User already registered' },
+      ]);
+    }
+
+    return this.prisma.user.create({
+      data: {
+        login,
+        password: await bcrypt.hash(password, 10),
+      },
+      select: {
+        id: true,
+        login: true,
+      },
+    });
+  }
 
   async signIn(login: string, password: string) {
-    return login === 'aprovame' && password === 'aprovame';
+    const user = await this.prisma.user.findUnique({ where: { login } });
+
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+      return false;
+    }
+
+    return {
+      id: user.id,
+      login: user.login,
+    };
   }
 
   token(payload: Record<string, unknown>) {
