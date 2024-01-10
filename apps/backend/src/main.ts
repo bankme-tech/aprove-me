39fef3e38c87f75e2e54373f405d48d980d253ed
Rafael Correa <rafael.chaves98@hotmail.com>
2024-01-09 21:29:58 -0300

    [feat] added customizable port for backend

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -3,6 +3,6 @@ import { AppModule } from './domains/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(5000);
+  await app.listen(process.env.PORT || 5000, '0.0.0.0');
 }
 bootstrap();
