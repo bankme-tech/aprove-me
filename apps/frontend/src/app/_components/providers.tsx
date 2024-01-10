4f6bc34cfe939b8edbfca8deef33eccd26607378
Rafael Correa <rafael.chaves98@hotmail.com>
2024-01-10 18:40:16 -0300

    [feat] added react query provider

---
+++ apps/frontend/src/app/_components/providers.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
+
+export default function Providers({ children }: { children: React.ReactNode }) {
+	return (
+		<QueryClientProvider client={queryClient}>
+			{children}
+		</QueryClientProvider>
+	)
+}
--- apps/frontend/src/app/layout.tsx
+++ apps/frontend/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import Providers from './_components/providers'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +17,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-BR">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+		<Providers>
+			{children}
+		</Providers>
+	  </body>
     </html>
   )
 }
