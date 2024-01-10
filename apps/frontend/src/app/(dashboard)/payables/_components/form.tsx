044624033f6057f73ad62e129e99b64adf6bcb2b
Rafael Correa <rafael.chaves98@hotmail.com>
2024-01-10 18:42:50 -0300

    [feat] enhanced payable form with combobox

--- apps/frontend/src/app/(dashboard)/payables/_components/form.tsx
+++ apps/frontend/src/app/(dashboard)/payables/_components/form.tsx
@@ -2,7 +2,11 @@
 
 import Field from "@/components/form/field";
 import Form from "@/components/form/form";
+import Combobox from "@/components/ui/combobox";
 import { Input } from "@/components/ui/input";
+import { index } from "@/server/assignor";
+import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
 
 type Props = {
 	action(data: FormData): Promise<any>;
@@ -12,6 +16,17 @@ type Props = {
 }
 
 export default function PayableForm(props: Props) {
+	const [search, setsearch] = useState("")
+	const { data } = useQuery({
+		queryKey: ["assignor", search],
+		async queryFn ({ queryKey }) {
+			const response = await index({ q: queryKey[1] })
+
+			return response?.data.map(t => ({ value: t.id, label: t.name })) || []
+		},
+		initialData: [],
+	})
+
 	return (
 		<Form action={props.action} data={props.data} className="max-w-sm">
 			<Field
@@ -27,7 +42,14 @@ export default function PayableForm(props: Props) {
 			<Field
 				name="assignor"
 				label="Cedente"
-				render={(field) => <Input {...field} />}
+				render={(field) => (
+					<Combobox
+						{...field}
+						options={data}
+						onSearchChange={setsearch}
+						placeholder="Pesquise um cedente"
+					/>
+				)}
 			/>
 
 			{props.children}
