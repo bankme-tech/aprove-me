a6ad80e299844dc5cdec3fce667b54db4cb4d474
Rafael Correa <rafael.chaves98@hotmail.com>
2024-01-10 15:17:38 -0300

    [feat] added initial payable api layer

---
+++ apps/frontend/src/server/payable.ts
@@ -0,0 +1,92 @@
+"use server"
+
+import action from "@/lib/action";
+import api from "@/lib/api";
+import { Pagination } from "@/types/general";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+
+type Payable = {
+	id: string;
+	document: string;
+	email: string;
+	phone: string;
+	name: string;
+}
+
+export const index = action(async (): Promise<Pagination<Payable>> => {
+	return api("/integrations/payable").then(t => t.json())
+})
+
+export const store = action(async (form) => {
+	const value = form.get("value")
+	const emissionDate = form.get("emissionDate")
+	const assignor = form.get("assignor")
+
+	const response = await api("/integrations/payable", {
+		method: "post",
+		body: JSON.stringify({
+			value,
+			emissionDate,
+			assignor,
+		})
+	}).then(t => t.json())
+	
+	if (response.error || response.message === "Unauthorized") {
+		if (typeof response.message === "string")
+			return { errors: [{ message: response.message }] }
+		else
+			return { errors: response.message }
+	}
+
+	revalidatePath("/payables")
+	redirect("/payables")
+})
+
+export const show = async (id: string): Promise<{data: Payable}> => {
+	return api(`/integrations/payable/${id}`).then(t => t.json())
+}
+
+export const update = action(async (form) => {
+	const id = form.get("id")
+	const value = form.get("value")
+	const emissionDate = form.get("emissionDate")
+	const assignor = form.get("assignor")
+
+	const response = await api(`/integrations/payable/${id}`, {
+		method: "PATCH",
+		body: JSON.stringify({
+			value,
+			emissionDate,
+			assignor,
+		})
+	}).then(t => t.json())
+	
+	if (response.error || response.message === "Unauthorized") {
+		if (typeof response.message === "string")
+			return { errors: [{ message: response.message }] }
+		else
+			return { errors: response.message }
+	}
+
+	revalidatePath(`/payables/${id}`)
+	redirect(`/payables/${id}`)
+})
+
+export const remove = action(async (form) => {
+	const id = form.get("id")
+
+	const response = await api(`/integrations/payable/${id}`, {
+		method: "delete",
+	}).then(t => t.json())
+	
+	if (response.error || response.message === "Unauthorized") {
+		if (typeof response.message === "string")
+			return { errors: [{ message: response.message }] }
+		else
+			return { errors: response.message }
+	}
+
+	revalidatePath("/payables")
+	redirect("/payables")
+})
