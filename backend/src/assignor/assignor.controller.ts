217b31ccca5504a2f89209cfa8267b86cae60b0e
kevinbzrra <kevinbzrra@gmail.com>
2024-02-06 19:10:46 -0300

    add AssignorService dependency to AssignorController

--- backend/src/assignor/assignor.controller.ts
+++ backend/src/assignor/assignor.controller.ts
@@ -1,16 +1,28 @@
-import { Controller, Get, Param, Post } from "@nestjs/common";
+import { Body, Controller, Get, Param, Post } from "@nestjs/common";
 import { AssignorService } from "./assignor.service";
+import { Assignor } from "./assignor.model";
 
 @Controller('assignor')
 export class AssignorController {
-    constructor(private readonly assignorService: AssignorService) {}
+    constructor(private readonly assignorService: AssignorService) { }
 
     @Get()
-    getAssignors() {}
+    getAssignors() {
+        return this.assignorService.getAllAssignors();
+    }
 
     @Get(':id')
-    getAssignor(@Param('id') id: string ) {}
+    getAssignor(@Param('id') id: string) {
+        return this.assignorService.getAssignorById(id);
+    }
 
     @Post()
-    createAssignor() {}
+    saveAssignor(
+        @Body() assignor: Assignor
+    ) {
+        const {document, email, phone, name} = assignor;
+
+        this.assignorService.saveAssignor(new Assignor(document, email, phone, name));
+        return;
+    }
 }
