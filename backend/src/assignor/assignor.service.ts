395d588a9bdf0052c74a9211525e24c52fc50b0a
kevinbzrra <kevinbzrra@gmail.com>
2024-02-06 18:57:13 -0300

    add AssignorService main methods

--- backend/src/assignor/assignor.service.ts
+++ backend/src/assignor/assignor.service.ts
@@ -1,6 +1,11 @@
 import { Injectable } from "@nestjs/common";
+import { Assignor } from "./assignor.model";
 
 @Injectable()
 export class AssignorService {
-    
+    getAllAssignors() {}
+
+    getAssignorById(id: string) {}
+
+    saveAssignor(assignor: Assignor) {}
 }
