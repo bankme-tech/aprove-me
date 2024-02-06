8182862c9231f88a1de859c1a2ec2b39f518b80d
kevinbzrra <kevinbzrra@gmail.com>
2024-02-06 18:50:45 -0300

    add AssignorController main routes

--- backend/src/assignor/assignor.controller.ts
+++ backend/src/assignor/assignor.controller.ts
@@ -1,6 +1,16 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Get, Param, Post } from "@nestjs/common";
+import { AssignorService } from "./assignor.service";
 
-@Controller()
+@Controller('assignor')
 export class AssignorController {
+    constructor(private readonly assignorService: AssignorService) {}
 
+    @Get()
+    getAssignors() {}
+
+    @Get(':id')
+    getAssignor(@Param('id') id: string ) {}
+
+    @Post()
+    createAssignor() {}
 }
