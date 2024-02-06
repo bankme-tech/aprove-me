7ee8191d161a0e491ae8fc3ef3a67a1742444d6d
kevinbzrra <kevinbzrra@gmail.com>
2024-02-06 18:15:31 -0300

    add Payable and Assignor modules to App module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { PayableModule } from './payable/payable.module';
+import { AssignorModule } from './assignor/assignor.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [PayableModule, AssignorModule]
 })
 export class AppModule {}
