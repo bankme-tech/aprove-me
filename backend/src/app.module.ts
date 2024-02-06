3757d8a94235ce174a25453cd2e6bbbf4b1488f5
kevinbzrra <kevinbzrra@gmail.com>
2024-02-06 20:00:41 -0300

    add prisma service

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PayableModule } from './payable/payable.module';
 import { AssignorModule } from './assignor/assignor.module';
+import { PrismaService } from './prisma.service';
 
 @Module({
-  imports: [PayableModule, AssignorModule]
+  imports: [PayableModule, AssignorModule],
+  providers: [PrismaService]
 })
 export class AppModule {}
