353c2a4156df93a813ba77f51c4817ba14423de7
kevinbzrra <kevinbzrra@gmail.com>
2024-02-07 17:59:45 -0300

    add length validation to Assignor attributes

--- backend/src/assignor/assignor.model.ts
+++ backend/src/assignor/assignor.model.ts
@@ -1,19 +1,23 @@
 import { Payable } from "@prisma/client";
-import { IsNotEmpty } from "class-validator";
+import { IsNotEmpty, Length } from "class-validator";
 
 export class Assignor {
     public id: string
 
     @IsNotEmpty()
+    @Length(0, 30)
     public document: string
 
     @IsNotEmpty()
+    @Length(0, 140)
     public email: string
 
     @IsNotEmpty()
+    @Length(0, 20)
     public phone: string
 
     @IsNotEmpty()
+    @Length(0, 140)
     public name: string
 
     public payable: Payable[]
