a3135a1322cb0f39844824d882021885c2f9fb76
Kevin Bezerra <kevinbzrra@gmail.com>
2024-02-07 02:31:01 -0300

    add PayableService methods

--- backend/src/payable/payable.service.ts
+++ backend/src/payable/payable.service.ts
@@ -1,6 +1,52 @@
 import { Injectable } from "@nestjs/common";
+import { Payable } from "@prisma/client";
+import { PrismaService } from "src/prisma.service";
 
 @Injectable()
 export class PayableService {
+    constructor(private readonly prisma: PrismaService) { }
+
+    async getAllPayables() {
+        return await this.prisma.payable.findMany({});
+    }
+
+    async getPayableByDocument(id: string) {
+        return await this.prisma.payable.findUnique({
+            where: {
+                id: id
+            }
+        });
+    }
+
+    async createPayable(payable: Payable) {
+        return await this.prisma.payable.create({
+            data: {
+                value: payable.value,
+                emissionDate: payable.emissionDate,
+                assignor: payable.assignor
+            }
+        })
+    }
+
+    async updatePayable(payable: Payable) {
+        return await this.prisma.payable.update({
+            data: {
+                value: payable.value,
+                emissionDate: payable.emissionDate,
+                assignor: payable.assignor
+            },
+            where: {
+                id: payable.id
+            }
+        })
+    }
+
+    async deletePayableByDocument(id: string) {
+        return await this.prisma.payable.delete({
+            where: {
+                id: id
+            }
+        });
+    }
 
 }
