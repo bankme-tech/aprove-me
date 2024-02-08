d863a12c9700e1050b6bb7db4e4228e2f35fec41
Kevin Bezerra <kevinbzrra@gmail.com>
2024-02-08 19:06:38 -0300

    add AuthController signIn test

--- backend/src/auth/auth.controller.spec.ts
+++ backend/src/auth/auth.controller.spec.ts
@@ -1,18 +1,42 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { User } from '../user/user.model';
+
+const user = new User();
+user.login = 'testuser';
+user.password = 'testuser';
+
+const jwtToken = '123';
 
 describe('AuthController', () => {
-  let controller: AuthController;
+  let authController: AuthController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
+      providers: [
+        {
+          provide: AuthService,
+          useValue: {
+            signIn: jest.fn().mockResolvedValue(jwtToken)
+          }
+        }
+      ]
     }).compile();
 
-    controller = module.get<AuthController>(AuthController);
+    authController = module.get<AuthController>(AuthController);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(authController).toBeDefined();
+  });
+
+  describe("signIn", () => {
+    it("should return a string token successfully", async () => {
+      const result = await authController.signIn(user);
+
+      expect(result).toBe(jwtToken);
+    });
   });
 });
