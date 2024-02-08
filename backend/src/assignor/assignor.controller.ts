e2837db572ec20c1fbb81b7ff85d064bfaa4635c
kevinbzrra <kevinbzrra@gmail.com>
2024-02-08 15:13:25 -0300

    add RoleGuard to AssignorController and PayableController

--- backend/src/assignor/assignor.controller.ts
+++ backend/src/assignor/assignor.controller.ts
@@ -1,25 +1,31 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, UseGuards } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Patch, Post, Request, UseGuards } from "@nestjs/common";
 import { AssignorService } from "./assignor.service";
 import { Assignor } from "./assignor.model";
 import { AuthGuard } from "src/auth/auth.guard";
+import { Roles } from "src/role/role.decorator";
+import { Role } from "src/role/role.enum";
+import { RoleGuard } from "src/role/role.guard";
 
 @Controller()
 export class AssignorController {
     constructor(private readonly assignorService: AssignorService) { }
 
-    @UseGuards(AuthGuard)
+    @UseGuards(AuthGuard, RoleGuard)
+    @Roles(Role.User)
     @Get()
     async getAssignors() {
         return await this.assignorService.getAllAssignors();
     }
 
-    @UseGuards(AuthGuard)
+    @UseGuards(AuthGuard, RoleGuard)
+    @Roles(Role.User)
     @Get(':id')
     async getAssignor(@Param('id') id: string) {
         return await this.assignorService.getAssignorById(id);
     }
 
-    @UseGuards(AuthGuard)
+    @UseGuards(AuthGuard, RoleGuard)
+    @Roles(Role.User)
     @Post()
     async createAssignor(
         @Body() assignor: Assignor
@@ -35,12 +41,13 @@ export class AssignorController {
         return await this.assignorService.createAssignor(newAssignor);
     }
 
-    @UseGuards(AuthGuard)
+    @UseGuards(AuthGuard, RoleGuard)
+    @Roles(Role.User)
     @Patch()
     async updateAssignor(
         @Body() assignor: Assignor
     ) {
-        const { id, document, email, phone, name} = assignor;
+        const { id, document, email, phone, name } = assignor;
 
         const newAssignor = new Assignor();
         newAssignor.id = id;
@@ -52,7 +59,8 @@ export class AssignorController {
         return await this.assignorService.updateAssignor(newAssignor);
     }
 
-    @UseGuards(AuthGuard)
+    @UseGuards(AuthGuard, RoleGuard)
+    @Roles(Role.User)
     @Delete(':id')
     async deleteAssignor(@Param('id') id: string) {
         return await this.assignorService.deleteAssignorById(id);
--- backend/src/payable/payable.controller.ts
+++ backend/src/payable/payable.controller.ts
@@ -2,24 +2,30 @@ import { Body, Controller, Delete, Get, Param, Patch, Post, UseGuards } from "@n
 import { PayableService } from "./payable.service";
 import { Payable } from "./payable.model";
 import { AuthGuard } from "src/auth/auth.guard";
+import { RoleGuard } from "src/role/role.guard";
+import { Roles } from "src/role/role.decorator";
+import { Role } from "src/role/role.enum";
 
 @Controller()
 export class PayableController {
     constructor(private readonly payableService: PayableService) { }
 
-    @UseGuards(AuthGuard)
+    @UseGuards(AuthGuard, RoleGuard)
+    @Roles(Role.User)
     @Get()
     async getPayables() {
         return await this.payableService.getAllPayables();
     }
 
-    @UseGuards(AuthGuard)
+    @UseGuards(AuthGuard, RoleGuard)
+    @Roles(Role.User)
     @Get(':id')
     async getPayable(@Param('id') id: string) {
         return await this.payableService.getPayableById(id);
     }
 
-    @UseGuards(AuthGuard)
+    @UseGuards(AuthGuard, RoleGuard)
+    @Roles(Role.User)
     @Post()
     async createPayable(
         @Body() payable: Payable
@@ -34,7 +40,8 @@ export class PayableController {
         return await this.payableService.createPayable(newPayable);
     }
 
-    @UseGuards(AuthGuard)
+    @UseGuards(AuthGuard, RoleGuard)
+    @Roles(Role.User)
     @Patch()
     async updatePayable(
         @Body() payable: Payable
@@ -50,7 +57,8 @@ export class PayableController {
         return await this.payableService.updatePayable(newPayable);
     }
 
-    @UseGuards(AuthGuard)
+    @UseGuards(AuthGuard, RoleGuard)
+    @Roles(Role.User)
     @Delete(':id')
     async deletePayable(@Param('id') id: string) {
         return await this.payableService.deletePayableById(id);
