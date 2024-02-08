51305f5f94c8ea1480f93da101be7b0821f8d410
kevinbzrra <kevinbzrra@gmail.com>
2024-02-08 15:08:54 -0300

    add RoleGuard

---
+++ backend/src/role/role.guard.ts
@@ -0,0 +1,23 @@
+import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Role } from './role.enum';
+import { ROLES_KEY } from './role.decorator';
+
+@Injectable()
+export class RoleGuard implements CanActivate {
+    constructor(private reflector: Reflector) { }
+
+    canActivate(context: ExecutionContext): boolean {
+        const requiredRoles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
+            context.getHandler(),
+            context.getClass(),
+        ]);
+        if (!requiredRoles) {
+            return true;
+        }
+
+        const { user } = context.switchToHttp().getRequest();
+
+        return requiredRoles.some((role) => user.roles?.includes(role));
+    }
+}
