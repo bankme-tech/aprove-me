e493cb2ab1342ca4efc9cfc0b37cba64d2edcf6e
Kevin Bezerra <kevinbzrra@gmail.com>
2024-02-08 20:39:07 -0300

    setup NextJS base

---
+++ frontend/app/(home)/page.tsx
@@ -0,0 +1,16 @@
+const Home = () => {
+    return (
+        <div className="w-full h-[100vh] flex items-center justify-center p-4">
+            <div className="grid grid-cols-2">
+                <div className="">
+
+                </div>
+                <form>
+                    <h1>Login</h1>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default Home;
---
+++ frontend/app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import { PrimeReactProvider } from 'primereact/api';
+import "primereact/resources/themes/lara-light-blue/theme.css";
+import 'primeicons/primeicons.css';
+
+export const metadata: Metadata = {
+  title: "Bankme",
+  description: "Quero ser Bankmer",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt">
+      <body>
+        <PrimeReactProvider>
+          {children}
+        </PrimeReactProvider>
+      </body>
+    </html>
+  );
+}
