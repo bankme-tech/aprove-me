5b8f9c500f6af9d1c50386ca257dd343782cad42
Kevin Bezerra <kevinbzrra@gmail.com>
2024-02-08 19:07:04 -0300

    add AuthService signIn test

--- backend/src/auth/auth.service.spec.ts
+++ backend/src/auth/auth.service.spec.ts
@@ -1,18 +1,49 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { UserService } from '../user/user.service';
+import { JwtService } from '@nestjs/jwt';
+import { User } from '../user/user.model';
+
+const user = new User();
+user.login = 'testuser';
+user.password = 'testuser';
+
+const jwtToken = '123';
 
 describe('AuthService', () => {
-  let service: AuthService;
+  let authService: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        {
+          provide: UserService,
+          useValue: {
+            findOne: jest.fn().mockResolvedValue(user)
+          }
+        },
+        {
+          provide: JwtService,
+          useValue: {
+            signAsync: jest.fn().mockResolvedValue(jwtToken)
+          }
+        }
+      ],
     }).compile();
 
-    service = module.get<AuthService>(AuthService);
+    authService = module.get<AuthService>(AuthService);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(authService).toBeDefined();
+  });
+
+  describe("signIn", () => {
+    it("should return an access_token successfully", async () => {
+      const result = await authService.signIn(user.login, user.password);
+
+      expect(result.access_token).toEqual(jwtToken);
+    });
   });
 });
