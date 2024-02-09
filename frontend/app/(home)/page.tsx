6211954d41fd1543f8d05775ab23d6955eccf9e0
Kevin Bezerra <kevinbzrra@gmail.com>
2024-02-08 21:38:16 -0300

    add signIn to Login page

--- frontend/app/(home)/page.tsx
+++ frontend/app/(home)/page.tsx
@@ -1,31 +1,64 @@
 'use client';
 
-import { FormEvent, useState } from "react";
+import { FormEvent, useRef, useState } from "react";
 import Input from "../_components/Input";
 import { Button } from "primereact/button";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
+import { auth } from "./_actions/auth";
+import { Toast } from "primereact/toast";
 
 const Home = () => {
+    const toastRef = useRef<any>(null);
 
     const router = useRouter();
 
-    const [usuario, setUsuario] = useState('');
-    const [senha, setSenha] = useState('');
+    const [login, setLogin] = useState('');
+    const [password, setPassword] = useState('');
     const [isLoading, setIsLoading] = useState(false);
+
+    const showError = (message: string) => {
+        toastRef.current.show({
+            severity: 'error', summary: 'Error!', detail: message
+        });
+    }
 
     const signIn = async (event: FormEvent<HTMLFormElement>) => {
         event.preventDefault();
 
-        setIsLoading(true);
+        try {
+            setIsLoading(true);
 
-        router.push('/assignors');
+            const result = await auth(login, password);
 
-        setIsLoading(false);
+            console.log(result)
+
+            if (result?.error) {
+                for (let i = 0; i < result.message.length; i++) {
+                    showError(result.message[i]);
+                }
+                return;
+            }
+
+            if (!result?.access_token) {
+                showError('Usuário não encontrado!');
+                return;
+            }
+
+            localStorage.setItem("token", result.access_token);
+
+            router.push('/assignors');
+        } catch (e: any) {
+            showError(e.message);
+        } finally {
+            setIsLoading(false);
+        }
     }
 
     return (
         <div className="w-full h-[100vh] flex items-center justify-center p-4">
+            <Toast ref={toastRef} />
+
             <div className="w-full max-w-[1280px] grid grid-cols-2">
                 <div className="flex items-center justify-center">
                     <Image
@@ -44,15 +77,15 @@ const Home = () => {
 
                     <Input
                         label="Usuário"
-                        onChange={e => setUsuario(e.target.value)}
-                        value={usuario}
+                        onChange={e => setLogin(e.target.value)}
+                        value={login}
                     />
 
                     <Input
                         type="password"
                         label="Senha"
-                        onChange={e => setSenha(e.target.value)}
-                        value={senha}
+                        onChange={e => setPassword(e.target.value)}
+                        value={password}
                     />
 
                     <Button
