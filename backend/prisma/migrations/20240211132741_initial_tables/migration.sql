70a2012d367fe3226bf46d98522d4f61fdb93d6f
Kevin Bezerra <kevinbzrra@gmail.com>
2024-02-11 13:36:03 -0300

    add migration

---
+++ backend/prisma/migrations/20240211132741_initial_tables/migration.sql
@@ -0,0 +1,19 @@
+-- CreateTable
+CREATE TABLE "Payable" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "value" REAL NOT NULL,
+    "emissionDate" DATETIME NOT NULL,
+    "assignorId" TEXT NOT NULL,
+    "deleted" BOOLEAN NOT NULL DEFAULT false,
+    CONSTRAINT "Payable_assignorId_fkey" FOREIGN KEY ("assignorId") REFERENCES "Assignor" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "Assignor" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "document" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "deleted" BOOLEAN NOT NULL DEFAULT false
+);
