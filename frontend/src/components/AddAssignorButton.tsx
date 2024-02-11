9df78d705e5d4d751cbe42d84fd1647ab0011a5b
Kevin Bezerra <kevinbzrra@gmail.com>
2024-02-11 19:34:09 -0300

    update AddAssignorButton component

--- frontend/src/components/AddAssignorButton.tsx
+++ frontend/src/components/AddAssignorButton.tsx
@@ -1,17 +1,28 @@
 'use client';
 
-import { useState } from "react";
-import CreateAssignorDialog from "./CreateAssignorDialog";
+import { useRef, useState } from "react";
 import { Button } from "primereact/button";
 import { Dialog } from "primereact/dialog";
 import AssignorForm from "./AssignorForm";
 
 const AddAssignorButton = () => {
+    const toastRef = useRef<any>();
 
     const [open, setOpen] = useState(false);
 
     const toggleDialog = () => {
         setOpen(value => !value);
+    }
+
+    const showToastSuccess = () => {
+        toastRef.current.show({
+            severity: 'success', summary: 'Sucesso!', detail: 'Informações armazenadas com sucesso!'
+        });
+    }
+
+    const handleOnSuccess = () => {
+        toggleDialog();
+        showToastSuccess();
     }
 
     return (
@@ -22,7 +33,7 @@ const AddAssignorButton = () => {
                 header="Criar novo cedente"
                 className="w-full max-w-md"
             >
-                <AssignorForm />
+                <AssignorForm onSuccess={handleOnSuccess} />
             </Dialog>
 
             <Button label="Novo" onClick={toggleDialog} />
