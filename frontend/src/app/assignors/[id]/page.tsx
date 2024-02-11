b3b288bdf8b3f59a9524079d713887db3c6f9038
Kevin Bezerra <kevinbzrra@gmail.com>
2024-02-11 14:16:17 -0300

    update assignor detail page

--- frontend/src/app/assignors/[id]/page.tsx
+++ frontend/src/app/assignors/[id]/page.tsx
@@ -1,7 +1,16 @@
+'use client';
+
+import AssignorInfo from "@/components/AssignorInfo";
 import Container from "@/components/Container";
 import Header from "@/components/Header";
+import { Suspense } from "react";
 
-const Assignor = () => {
+const Assignor = ({ params }: {
+    params: { id: string }
+}) => {
+
+    const token = localStorage.getItem('token');
+
     return (
         <div>
             <Header />
@@ -9,6 +18,9 @@ const Assignor = () => {
             <Container>
                 <h1 className="font-bold text-2xl">Cedente</h1>
 
+                <Suspense fallback={<div>Carregando...</div>}>
+                    <AssignorInfo token={token} id={params.id} />
+                </Suspense>
             </Container>
         </div>
     )
