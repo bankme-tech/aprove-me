33cc06ce4018b2b3791f4846b31c68c71b7cf5b4
Kevin Bezerra <kevinbzrra@gmail.com>
2024-02-11 13:36:20 -0300

    change server port and enabled cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,8 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  await app.listen(3001);
 }
 bootstrap();
