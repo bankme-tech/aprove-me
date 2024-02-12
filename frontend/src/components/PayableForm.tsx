4f8eccf415a3ff35f0aba66f325e6bd53617dda3
Kevin Bezerra <kevinbzrra@gmail.com>
2024-02-12 04:33:05 -0300

    add PayableForm component

---
+++ frontend/src/components/PayableForm.tsx
@@ -0,0 +1,135 @@
+'use client';
+
+import { useForm } from "react-hook-form";
+import { Button } from "primereact/button";
+import { Suspense, useRef, useState } from "react";
+import { Toast } from "primereact/toast";
+import { BASE_URL } from "@/contants";
+import InputNumber from "./InputNumber";
+import InputCalendar from "./InputCalendar";
+import AssignorsComboBox from "./AssignorsComboBox";
+import { Dropdown } from "primereact/dropdown";
+
+interface Payable {
+    id: string
+    value: number
+    emissionDate: Date
+    assignorId: string
+}
+
+const PayableForm = ({ payable, onSuccess }: {
+    payable?: Payable, onSuccess: () => void
+}) => {
+    const toastRef = useRef<any>();
+
+    let defaultValues;
+
+    if (payable) {
+        defaultValues = {
+            id: payable.id,
+            value: payable.value,
+            emissionDate: payable.emissionDate,
+            assignorId: payable.assignorId
+        }
+    }
+
+    const token = localStorage.getItem('token');
+
+    if(!token) return;
+
+    const [isSubmitLoading, setIsSubmitLoading] = useState(false);
+
+    const { control, handleSubmit, formState: { errors } } = useForm({
+        defaultValues
+    });
+
+    const showToastError = (message: string) => {
+        toastRef.current.show({
+            severity: 'error', summary: 'Erro!', detail: message
+        });
+    }
+
+    const onSubmit = async (data: any) => {
+        try {
+            setIsSubmitLoading(true);
+
+            let method = 'POST';
+
+            if (data.id) {
+                method = 'PATCH';
+            }
+
+            const res = await fetch(`${BASE_URL}/integrations/payable`, {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}`
+                },
+                method,
+                body: JSON.stringify(data)
+            });
+            const result = await res.json();
+
+            if (result?.error) {
+                for (let i = 0; i < result.message.length; i++) {
+                    showToastError(result.message[i]);
+                }
+                return;
+            }
+
+            onSuccess();
+        } catch (e: any) {
+            showToastError(e.message);
+        } finally {
+            setIsSubmitLoading(false);
+        }
+    }
+
+    return (
+        <form
+            className="w-full flex flex-col gap-4"
+            onSubmit={handleSubmit(onSubmit)}
+        >
+            <Toast ref={toastRef} />
+            <InputNumber
+                label="Valor"
+                name="value"
+                errors={errors}
+                control={control}
+                rules={{ required: "Valor obrigatório" }}
+                mode="currency"
+                currency="BRL"
+            />
+            <InputCalendar
+                label="Data de emissão"
+                name="emissionDate"
+                control={control}
+                errors={errors}
+                rules={{ required: "Data de emissão obrigatória" }}
+            />
+            <Suspense fallback={
+                <div className="flex flex-col gap-1">
+                    <label>Cedente</label>
+                    <Dropdown 
+                        disabled
+                        placeholder="Carregando..."
+                    />
+                </div>
+            }>
+                <AssignorsComboBox
+                    control={control}
+                    errors={errors}
+                    token={token}
+                />
+            </Suspense>
+            <div className="flex justify-end">
+                <Button
+                    label="Salvar"
+                    loading={isSubmitLoading}
+                    type="submit"
+                />
+            </div>
+        </form>
+    )
+}
+
+export default PayableForm;
