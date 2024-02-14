5dfad4482a1528f06746c635654a9dc1a9be0ed2
Kevin Bezerra <kevinbzrra@gmail.com>
2024-02-14 10:36:00 -0300

    add notFound page

---
+++ frontend/src/app/not-found.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import Image from "next/image";
+import Link from "next/link";
+
+const NotFound = () => {
+    return (
+        <div className="w-full h-[100vh] flex flex-col items-center justify-center gap-10">
+            <Image
+                src={'/logo-bankme.png'}
+                alt="Bankme"
+                className="max-w-[100px] h-auto"
+                width={913}
+                height={1080}
+            />
+            <p>Desculpe, não conseguimos encontrar esta página!</p>
+            <Link 
+            href="/"
+            className="underline text-blue-600"
+            >Ir para a tela de Login</Link>
+        </div>
+    )
+}
+
+export default NotFound;
