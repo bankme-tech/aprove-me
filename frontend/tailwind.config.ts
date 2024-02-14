59d0368d2e9f60e4282a099bde818492c49f8717
Kevin Bezerra <kevinbzrra@gmail.com>
2024-02-14 11:27:49 -0300

    fixed primereact + tailwind styles

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -5,6 +5,7 @@ const config: Config = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/primereact/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
