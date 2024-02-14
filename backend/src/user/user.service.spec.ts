ba5bffc4ea35324b04b2c1aa7828b304206481c4
Kevin Bezerra <kevinbzrra@gmail.com>
2024-02-14 09:16:58 -0300

    updated UserService tests

--- backend/src/user/user.service.spec.ts
+++ backend/src/user/user.service.spec.ts
@@ -1,18 +1,42 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
+import { mockDeep } from 'jest-mock-extended';
+import { PrismaClient } from '@prisma/client';
+import { PrismaService } from '../prisma.service';
+import { User } from './user.model';
+
+const user = new User();
+user.login = 'aprovame';
+user.password = 'arovame';
 
 describe('UserService', () => {
-  let service: UserService;
+  let userService: UserService;
+  let prismaService: PrismaService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
-    }).compile();
+      providers: [UserService, PrismaService],
+    })
+      .overrideProvider(PrismaService)
+      .useValue(mockDeep(PrismaClient))
+      .compile();
 
-    service = module.get<UserService>(UserService);
+    userService = module.get(UserService);
+    prismaService = module.get(PrismaService);
+
+    prismaService.user.findFirst = jest.fn().mockResolvedValue(user)
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(userService).toBeDefined();
+    expect(prismaService).toBeDefined();
+  });
+
+  describe("getUserByLogin", () => {
+    it("should return a user successfully with the same login sent", async () => {
+      const result = await userService.getUserByLogin(user.login);
+
+      expect(result.login).toBe(user.login);
+    })
   });
 });
