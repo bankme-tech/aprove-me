2461192dbaadc91f35d29de822c85b2bfafe1f9a
k1nha <lucascmpus@gmail.com>
2024-02-14 19:03:37 -0300

    init backend

---
+++ backend/src/main.ts
@@ -0,0 +1,13 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  app.setGlobalPrefix('/api');
+  app.useGlobalPipes(new ValidationPipe());
+  await app.listen(3000);
+}
+
+bootstrap();
