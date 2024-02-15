fa54fe5f39f49e7cff9a0482435922c1c4640589
Viiict0r <victorh.cepil@hotmail.com>
2024-02-15 19:44:00 -0300

    setup: jest config, validation libs & ts-paths

---
+++ apps/api/jest.config.ts
@@ -0,0 +1,20 @@
+import { Config } from 'jest';
+import { pathsToModuleNameMapper } from 'ts-jest';
+import { compilerOptions } from './tsconfig.json';
+
+const config: Config = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  setupFiles: ['dotenv/config'],
+  coverageDirectory: '../coverage',
+  testEnvironment: 'node',
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>/',
+  }),
+};
+
+export default config;
