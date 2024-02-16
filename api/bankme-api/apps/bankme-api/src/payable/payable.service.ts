7e565af7ec67d6b6c88df2e1662e4c60f868bab9
Jonathan Vale <creativeweb.de@gmail.com>
2024-02-16 01:28:52 -0300

    Create payable crud

---
+++ api/bankme-api/apps/bankme-api/src/payable/payable.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@nestjs/common';
+import { CreatePayableDto } from './dto/create-payable.dto';
+import { UpdatePayableDto } from './dto/update-payable.dto';
+
+@Injectable()
+export class PayableService {
+  create(createPayableDto: CreatePayableDto) {
+    return 'This action adds a new payable';
+  }
+
+  findAll() {
+    return `This action returns all payable`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} payable`;
+  }
+
+  update(id: number, updatePayableDto: UpdatePayableDto) {
+    return `This action updates a #${id} payable`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} payable`;
+  }
+}
