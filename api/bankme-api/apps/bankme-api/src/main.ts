1a7a22c02237469597a074cda5e472f24c3bdc33
Jonathan Vale <creativeweb.de@gmail.com>
2024-02-17 17:18:30 -0300

    creating prisma configuration

--- api/bankme-api/apps/bankme-api/src/main.ts
+++ api/bankme-api/apps/bankme-api/src/main.ts
@@ -16,6 +16,9 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('swagger', app, document);
 
-  await app.listen(3000);
+  await app.listen(
+    process.env.PORT || 3000,
+    process.env.ENV === 'PRODUCTION' ? '0.0.0.0' : undefined,
+  );
 }
 bootstrap();
