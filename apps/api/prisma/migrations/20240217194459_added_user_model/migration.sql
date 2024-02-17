408031ae09c707cdb6a256f190e21a20797ab8d9
Viiict0r <victorh.cepil@hotmail.com>
2024-02-17 16:47:36 -0300

    feat(user): added user model to prisma scheme

---
+++ apps/api/prisma/migrations/20240217194459_added_user_model/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
