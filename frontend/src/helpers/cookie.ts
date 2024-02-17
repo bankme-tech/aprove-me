3d0b06d710e647c3aef680945772a4dd6cecd75d
Kevin Bezerra <kevinbzrra@gmail.com>
2024-02-17 12:11:38 -0300

    add cookie helpers

---
+++ frontend/src/helpers/cookie.ts
@@ -0,0 +1,10 @@
+export const readCookie = (name: string) => {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
