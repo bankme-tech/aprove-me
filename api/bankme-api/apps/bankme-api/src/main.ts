f6755c3732c0a4d02c2c936a55cedd544dc30360
Jonathan Vale <creativeweb.de@gmail.com>
2024-02-17 16:12:05 -0300

    add swagger to project

--- api/bankme-api/apps/bankme-api/src/main.ts
+++ api/bankme-api/apps/bankme-api/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Bankme aprove-me')
+    .setDescription(
+      'Api for application test to try get a chance to join in bankme',
+    )
+    .setVersion('1.0')
+    .addTag('Bankme')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
