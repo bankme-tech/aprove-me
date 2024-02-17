35b80e23d7ccb26c8e08bec5ab08b22bd091fd29
Jonathan Vale <creativeweb.de@gmail.com>
2024-02-17 16:12:41 -0300

    add swagger to auth controller

--- api/bankme-api/apps/bankme-api/src/app.controller.ts
+++ api/bankme-api/apps/bankme-api/src/app.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Redirect } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiExcludeController } from '@nestjs/swagger';
 
+@ApiExcludeController()
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor() {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+  @Redirect('/swagger')
+  goToSwagger() {}
 }
--- api/bankme-api/apps/bankme-api/src/auth/auth.controller.ts
+++ api/bankme-api/apps/bankme-api/src/auth/auth.controller.ts
@@ -1,33 +1,70 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateAuthDto } from './dto/create-auth.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { ListAuthDto } from './dto/list-auth.dto';
 
 @Controller('auth')
+@ApiTags('Authentication')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  @ApiOperation({ summary: 'Autentication' })
+    constructor(private readonly authService: AuthService) { }
 
   @Post()
+  @ApiResponse({
+    status: 200,
+    description: 'Create a user',
+    type: CreateAuthDto
+  })
   create(@Body() createAuthDto: CreateAuthDto) {
     return this.authService.create(createAuthDto);
   }
 
   @Get()
+  @ApiResponse({
+    status: 200,
+    description: 'Get user list',
+    type: ListAuthDto
+  })
   findAll() {
     return this.authService.findAll();
   }
 
   @Get(':id')
+  @ApiResponse({
+    status: 200,
+    description: 'Get user By Id',
+    type: CreateAuthDto
+  })
   findOne(@Param('id') id: string) {
     return this.authService.findOne(+id);
   }
 
   @Patch(':id')
+  @ApiResponse({
+    status: 200,
+    description: 'Change user By Id',
+    type: UpdateAuthDto
+  })
   update(@Param('id') id: string, @Body() updateAuthDto: UpdateAuthDto) {
     return this.authService.update(+id, updateAuthDto);
   }
 
   @Delete(':id')
+  @ApiResponse({
+    status: 200,
+    description: 'Remove user By Id',
+    type: UpdateAuthDto
+  })
   remove(@Param('id') id: string) {
     return this.authService.remove(+id);
   }
