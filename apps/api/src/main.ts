dbd3772c32b4271b5727870793ac5f1e5555c2f1
Viiict0r <victorh.cepil@hotmail.com>
2024-02-18 00:19:14 -0300

    fix: cors

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,10 +3,12 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+  });
 
   app.useGlobalPipes(new ValidationPipe());
 
-  await app.listen(3000);
+  await app.listen(3001);
 }
 bootstrap();
