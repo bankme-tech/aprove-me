6176a2a8ea0c0f4fcc9a4fb2ec13bab47cc05d38
Viiict0r <victorh.cepil@hotmail.com>
2024-02-19 19:44:21 -0300

    chore: added pages generic title

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <title>Tech Test | bankme.tech</title>
       <body
         className={cn(
           'min-h-screen bg-background font-sans antialiased bg-slate-50',
