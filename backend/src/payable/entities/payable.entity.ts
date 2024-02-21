f4c8729bac2e8cfb4a6a170607246b130d1756b0
ricardo <ricardorbxx1@gmail.com>
2024-02-21 19:55:47 -0300

    feat: add a constructor to payable entity

--- backend/src/payable/entities/payable.entity.ts
+++ backend/src/payable/entities/payable.entity.ts
@@ -6,4 +6,11 @@ export class Payable {
   emissionDate: Date;
   assignorId: string;
   assignor?: Assignor;
+
+  constructor(id: string, value: number, emissionDate: Date, assignorId: string) {
+    this.id = id;
+    this.value = value;
+    this.emissionDate = emissionDate;
+    this.assignorId = assignorId;
+  }
 }
