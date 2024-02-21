58dfbfe419d817ccd92c7973a301178e8cc6b2fb
ricardo <ricardorbxx1@gmail.com>
2024-02-21 14:08:17 -0300

    feat: add Assignor entity

--- backend/src/assignor/entities/assignor.entity.ts
+++ backend/src/assignor/entities/assignor.entity.ts
@@ -1,1 +1,10 @@
-export class Assignor {}
+import { Payable } from '@prisma/client';
+
+export class Assignor {
+  id: string;
+  document: string;
+  email: string;
+  phone: string;
+  name: string;
+  Payables?: Payable[];
+}
