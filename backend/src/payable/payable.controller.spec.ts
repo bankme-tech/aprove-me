abb947c030ea110d1d7b9344d4bfd64cde54377d
ricardo <ricardorbxx1@gmail.com>
2024-02-21 20:16:03 -0300

    test: add unit tests for payable controller layer

--- backend/src/payable/payable.controller.spec.ts
+++ backend/src/payable/payable.controller.spec.ts
@@ -3,9 +3,11 @@ import { PayableController } from './payable.controller';
 import { PayableService } from './payable.service';
 import PayableRepository from './repositories/payableRepository';
 import AssignorRepository from '../assignor/repositories/assignorRepository';
+import { payableMock } from './payable.service.spec';
 
 describe('PayableController', () => {
   let controller: PayableController;
+  let service: PayableService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -24,9 +26,34 @@ describe('PayableController', () => {
     }).compile();
 
     controller = module.get<PayableController>(PayableController);
+    service = module.get<PayableService>(PayableService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should return a payable', async () => {
+    jest.spyOn(service, 'findOne').mockImplementation(async () => payableMock);
+    const payable = await controller.findOne('1');
+    expect(payable).toEqual(payableMock);
+  });
+
+  it('should return a list of  payables', async () => {
+    jest.spyOn(service, 'findAll').mockImplementation(async () => [payableMock]);
+    const payable = await controller.findAll();
+    expect(payable).toEqual([payableMock]);
+  });
+
+  it('should return a payable after updating it', async () => {
+    jest.spyOn(service, 'update').mockImplementation(async () => payableMock);
+    const payable = await controller.update('1', payableMock);
+    expect(payable).toEqual(payableMock);
+  });
+
+  it('should return a message after deleting a payable', async () => {
+    jest.spyOn(service, 'remove').mockImplementation(async () => undefined);
+    const payable = await controller.remove('1');
+    expect(payable).toEqual({ message: 'Payable deleted' });
   });
 });
