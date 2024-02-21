fd2724f08dbc1d913f07d4714da49596002bc01f
ricardo <ricardorbxx1@gmail.com>
2024-02-21 13:53:08 -0300

    feat: generate Assignor resource using nest cli

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { PrismaService } from './database/prisma.service';
 import { HealthController } from './app.controller';
 import { PayableModule } from './payable/payable.module';
+import { AssignorModule } from './assignor/assignor.module';
 
 @Module({
-  imports: [PayableModule],
+  imports: [PayableModule, AssignorModule],
   controllers: [HealthController],
   providers: [PrismaService],
 })
