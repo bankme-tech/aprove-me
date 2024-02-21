c115c0de0ac44d9da2cc0eb7826ce292346238cb
ricardo <ricardorbxx1@gmail.com>
2024-02-21 17:09:14 -0300

    feat: added UpdateAssignorDTO

--- backend/src/assignor/dto/update-assignor.dto.ts
+++ backend/src/assignor/dto/update-assignor.dto.ts
@@ -1,4 +1,21 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateAssignorDto } from './create-assignor.dto';
+import { IsString, MaxLength } from 'class-validator';
 
-export class UpdateAssignorDto extends PartialType(CreateAssignorDto) {}
+export class UpdateAssignorDto extends PartialType(CreateAssignorDto) {
+  @IsString()
+  @MaxLength(30)
+  document: string;
+
+  @IsString()
+  @MaxLength(140)
+  email: string;
+
+  @IsString()
+  @MaxLength(20)
+  phone: string;
+
+  @IsString()
+  @MaxLength(140)
+  name: string;
+}
