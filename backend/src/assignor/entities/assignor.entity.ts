f48ec87a58b9ad9bb2125799e5aa4a438a1d7a6c
ricardo <ricardorbxx1@gmail.com>
2024-02-21 19:55:34 -0300

    feat: add a constructor to assignor entity

--- backend/src/assignor/entities/assignor.entity.ts
+++ backend/src/assignor/entities/assignor.entity.ts
@@ -7,4 +7,20 @@ export class Assignor {
   phone: string;
   name: string;
   Payables?: Payable[];
+
+  constructor(
+    id: string,
+    document: string,
+    email: string,
+    phone: string,
+    name: string,
+    Payables?: Payable[],
+  ) {
+    this.id = id;
+    this.document = document;
+    this.email = email;
+    this.phone = phone;
+    this.name = name;
+    this.Payables = Payables;
+  }
 }
