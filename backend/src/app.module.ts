f577562b01ed963d9890d65e2061c1394157585d
ricardo <ricardorbxx1@gmail.com>
2024-02-21 20:42:03 -0300

    feat: implemented Router Module to manage routes

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,28 @@ import { PrismaService } from './database/prisma.service';
 import { HealthController } from './app.controller';
 import { PayableModule } from './payable/payable.module';
 import { AssignorModule } from './assignor/assignor.module';
+import { RouterModule } from '@nestjs/core';
 
 @Module({
-  imports: [PayableModule, AssignorModule],
+  imports: [
+    PayableModule,
+    AssignorModule,
+    RouterModule.register([
+      {
+        path: 'integrations',
+        children: [
+          {
+            path: 'payable',
+            module: PayableModule,
+          },
+          {
+            path: 'assignor',
+            module: AssignorModule,
+          },
+        ],
+      },
+    ]),
+  ],
   controllers: [HealthController],
   providers: [PrismaService],
 })
