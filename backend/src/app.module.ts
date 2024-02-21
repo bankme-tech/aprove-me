9150cf6f55b163ca9d98e5fdb5081245ba4557fa
ricardo <ricardorbxx1@gmail.com>
2024-02-21 12:51:17 -0300

    chore: cleaned root folder and added db connection health check

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { PrismaService } from './database/prisma.service';
+import { HealthController } from './app.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [HealthController],
+  providers: [PrismaService],
 })
 export class AppModule {}
