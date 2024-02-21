bec2b8a439d7c68d126f44979b886b7547618ea0
ricardo <ricardorbxx1@gmail.com>
2024-02-21 15:52:23 -0300

    feat: add UpdatePayableDTO

--- backend/src/payable/dto/update-payable.dto.ts
+++ backend/src/payable/dto/update-payable.dto.ts
@@ -1,4 +1,13 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreatePayableDto } from './create-payable.dto';
+import { IsNotEmpty, IsString, IsUUID } from 'class-validator';
 
-export class UpdatePayableDto extends PartialType(CreatePayableDto) {}
+export class UpdatePayableDto extends PartialType(CreatePayableDto) {
+  @IsNotEmpty()
+  @IsString()
+  value: number;
+
+  @IsNotEmpty()
+  @IsString()
+  assignorId: string;
+}
