3cdeb691e7ed7bed3ce4a9f1ef5b65672b7f1d95
ricardo <ricardorbxx1@gmail.com>
2024-02-21 14:08:44 -0300

    feat: add repository layer to assignor module

--- backend/src/assignor/assignor.module.ts
+++ backend/src/assignor/assignor.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AssignorService } from './assignor.service';
 import { AssignorController } from './assignor.controller';
+import { PrismaService } from 'src/database/prisma.service';
+import AssignorRepository from './repositories/assignorRepository';
+import PrismaAssignorRepository from './repositories/prismaAssignorRepository';
 
 @Module({
   controllers: [AssignorController],
-  providers: [AssignorService],
+  providers: [
+    AssignorService,
+    PrismaService,
+    {
+      provide: AssignorRepository,
+      useClass: PrismaAssignorRepository,
+    },
+  ],
 })
 export class AssignorModule {}
---
+++ backend/src/assignor/repositories/prismaAssignorRepository.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/database/prisma.service';
+import { CreateAssignorDto } from '../dto/create-assignor.dto';
+import { Assignor } from '../entities/assignor.entity';
+import AssignorRepository from './assignorRepository';
+
+@Injectable()
+export default class PrismaAssignorRepository extends AssignorRepository {
+  constructor(private prisma: PrismaService) {
+    super();
+  }
+
+  async create(createAssignorDto: CreateAssignorDto): Promise<Assignor> {
+    const { document, name, email, phone } = createAssignorDto;
+    return this.prisma.assignor.create({
+      data: {
+        document,
+        name,
+        email,
+        phone,
+      },
+    });
+  }
+}
