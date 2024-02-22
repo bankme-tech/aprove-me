57a7c9f88cc003ebe9480fe5d1386bb00c32eea4
ricardo <ricardorbxx1@gmail.com>
2024-02-22 14:42:58 -0300

    feat: add and implement Create method to user layer

--- backend/src/user/repositories/prismaUserRepository.ts
+++ backend/src/user/repositories/prismaUserRepository.ts
@@ -2,6 +2,8 @@ import { User } from '@prisma/client';
 import UserRepository from './userRepository';
 import { PrismaService } from 'src/database/prisma.service';
 import { Injectable } from '@nestjs/common';
+import { CreateUserDTO } from '../dto/create-user.dto';
+import bcrypt from 'bcrypt';
 
 @Injectable()
 export default class PrismaUserRepository extends UserRepository {
@@ -11,5 +13,19 @@ export default class PrismaUserRepository extends UserRepository {
 
   findByLogin(login: string): Promise<User> {
     return this.prisma.user.findUnique({ where: { login } });
+  }
+
+  create(createUserDTO: CreateUserDTO): Promise<User> {
+    const { login, password } = createUserDTO;
+
+    const salt = bcrypt.genSaltSync();
+    const hashedPassword = bcrypt.hashSync(password, salt);
+
+    return this.prisma.user.create({
+      data: {
+        login,
+        password: hashedPassword,
+      },
+    });
   }
 }
--- backend/src/user/repositories/userRepository.ts
+++ backend/src/user/repositories/userRepository.ts
@@ -1,5 +1,7 @@
 import { User } from '@prisma/client';
+import { CreateUserDTO } from '../dto/create-user.dto';
 
 export default abstract class UserRepository {
   abstract findByLogin(login: string): Promise<User>;
+  abstract create(createUserDTO: CreateUserDTO): Promise<User>;
 }
