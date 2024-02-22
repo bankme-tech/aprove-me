296c982ece40fde061dc18ffadd5ac4a13fbda4f
ricardo <ricardorbxx1@gmail.com>
2024-02-22 14:44:08 -0300

    feat: add global filters to take the PrismaExceptionFilter

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { PrismaExceptionFilter } from './exception-filters/prisma.exception-filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalFilters(new PrismaExceptionFilter());
   await app.listen(3000);
 }
 bootstrap();
