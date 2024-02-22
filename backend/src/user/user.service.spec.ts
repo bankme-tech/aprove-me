b34545e3b948246f62425d832c4af7bd8a5161cc
ricardo <ricardorbxx1@gmail.com>
2024-02-22 08:31:20 -0300

    test: add providers to mock the user repository layer

--- backend/src/user/user.service.spec.ts
+++ backend/src/user/user.service.spec.ts
@@ -1,12 +1,19 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
+import UserRepository from './repositories/userRepository';
 
 describe('UserService', () => {
   let service: UserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+      providers: [
+        UserService,
+        {
+          provide: UserRepository,
+          useValue: {},
+        },
+      ],
     }).compile();
 
     service = module.get<UserService>(UserService);
