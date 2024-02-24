b002c31024594673443a49f8b8a50968ac2efe59
ricardo <ricardorbxx1@gmail.com>
2024-02-23 21:10:44 -0300

    feat: configured micro service that consumes rabbitMQ queues

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,11 +2,25 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { PrismaExceptionFilter } from './exception-filters/prisma.exception-filter';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new PrismaExceptionFilter());
-  await app.listen(3000);
+
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.RMQ,
+    options: {
+      urls: ['amqp://admin:admin@rabbitmq:5672/'],
+      queue: 'payable-queue',
+      queueOptions: {
+        durable: true,
+      },
+    },
+  });
+
+  await app.startAllMicroservices();
+  await app.listen(3333);
 }
 bootstrap();
