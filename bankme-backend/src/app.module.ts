711fb7da41237b45d45fe817ebe9a74ed65d8ca6
jjribeiro1 <jvsr99@hotmail.com>
2024-05-17 20:34:03 -0300

    [backend] prisma setup

--- bankme-backend/src/app.module.ts
+++ bankme-backend/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { IntegrationsModule } from './integrations/integrations.module';
+import { DbModule } from './db/db.module';
 
 @Module({
-  imports: [IntegrationsModule],
+  imports: [IntegrationsModule, DbModule],
   controllers: [AppController],
   providers: [AppService],
 })
