56f9fa2bae24fa77eee48ea585b96392ffa3105e
hxsggsz <vt.hugo.2021@gmail.com>
2024-05-17 19:12:11 -0300

    feat: create assignor entity

---
+++ src/app/entities/assignor.ts
@@ -0,0 +1,36 @@
+import { Replace } from '@/utils/replace';
+import { randomUUID } from 'crypto';
+
+interface PayableProps {
+  document: string;
+  email: string;
+  phone: string;
+  name: string;
+  createdAt: Date;
+  updatedAt: Date;
+  deletedAt?: Date;
+}
+
+interface PayablePropsReplaceble {
+  deletedAt?: Date;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+export class Payable {
+  public _id: string;
+  public props: PayableProps;
+
+  constructor(
+    props: Replace<PayablePropsReplaceble, PayablePropsReplaceble>,
+    _id?: string,
+  ) {
+    this._id = _id ? _id : randomUUID();
+    this.props = {
+      ...this.props,
+      createdAt: props.createdAt ?? new Date(),
+      updatedAt: props.updatedAt ?? new Date(),
+      deletedAt: props.deletedAt ?? new Date(),
+    };
+  }
+}
