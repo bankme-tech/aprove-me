490147e046ac02c9b4f89843bd0ffd6fce2e5333
Emerson <emerson.vv75248@gmail.com>
2024-05-17 19:24:19 -0300

    chore: Add Assignor and Receivable services

---
+++ api/src/services/prisma.service.ts
@@ -0,0 +1,10 @@
+
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  async onModuleInit() {
+    await this.$connect();
+  }
+}
