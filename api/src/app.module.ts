4dd4d41d821d4be914a9e9d58aeab80d3fca5775
Emerson <emerson.vv75248@gmail.com>
2024-05-18 00:00:17 -0300

    chore: Remove unused app controller and service

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { PrismaService } from './services/prisma.service';
 import { ReceivableService } from './services/receivable.service';
 import { AssignorService } from './services/assignor.service';
@@ -9,7 +7,7 @@ import { AssignorController } from './controllers/assignor.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, ReceivableController, AssignorController],
-  providers: [AppService, PrismaService, ReceivableService, AssignorService],
+  controllers: [ReceivableController, AssignorController],
+  providers: [PrismaService, ReceivableService, AssignorService],
 })
 export class AppModule {}
