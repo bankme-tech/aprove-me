b5aeb36b3b5502b47e5810a1baee6c4d05eac1bd
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-05-18 16:38:16 -0300

    feat(payable): criando serviço para payable & método createPayableRegister

---
+++ bankme/src/integrations/payable/payable.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+import Payable from '../entity/Payable';
+import PayableRepository from './payable.repository';
+import { IPayable } from '../types/IPayables';
+
+@Injectable()
+export class PayableService {
+  constructor(private payableRepository: PayableRepository) {}
+
+  async createPayableRegister(payable: Payable): Promise<IPayable> {
+    const createdPayable =
+      await this.payableRepository.createPayableRegister(payable);
+
+    return createdPayable;
+  }
+}
