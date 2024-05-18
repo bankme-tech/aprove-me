75dc668041956c375fc007e417811e500d5e7a8f
Caio Fernando <caiofsr13@gmail.com>
2024-05-18 14:25:49 -0300

    configure jest to improve test dx

---
+++ packages/api/jest.config.ts
@@ -0,0 +1,21 @@
+import { Config } from 'jest';
+import { pathsToModuleNameMapper } from 'ts-jest';
+import { compilerOptions } from './tsconfig.json';
+
+const config: Config = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>/',
+  }),
+  collectCoverageFrom: ['src/**/*.(t|j)s'],
+  coverageDirectory: './coverage',
+  testEnvironment: 'node',
+  resetMocks: true,
+  clearMocks: true,
+};
+
+export default config;
