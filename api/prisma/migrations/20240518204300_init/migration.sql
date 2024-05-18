7cc129ef68795c1c5613a8d325815e6ca198900b
Emerson <emerson.vv75248@gmail.com>
2024-05-18 17:43:43 -0300

    chore: Update .gitignore to include prisma migrations

---
+++ api/prisma/migrations/20240518204300_init/migration.sql
@@ -0,0 +1,30 @@
+-- CreateTable
+CREATE TABLE "receivables" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "value" REAL NOT NULL,
+    "emissionDate" DATETIME NOT NULL,
+    "assignor" TEXT NOT NULL,
+    CONSTRAINT "receivables_assignor_fkey" FOREIGN KEY ("assignor") REFERENCES "assignors" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "assignors" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "document" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+    "name" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "permissions" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "login" TEXT NOT NULL,
+    "password" TEXT NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "assignors_document_key" ON "assignors"("document");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "permissions_login_key" ON "permissions"("login");
