eaacb6776f1a44671165ea7b58e7b4457da654a8
Caio Fernando <caiofsr13@gmail.com>
2024-05-18 18:13:52 -0300

    add payables endpoints

--- packages/api/src/payable/payable.controller.spec.ts
+++ packages/api/src/payable/payable.controller.spec.ts
@@ -3,6 +3,7 @@ import { Payable } from './entities/payable.entity';
 import { payableMock } from './payable.service.spec';
 import { Test, TestingModule } from '@nestjs/testing';
 import { PayableController } from './payable.controller';
+import { assignorMock } from '@assignor/assignor.service.spec';
 import { PayableRepository } from './repositories/payable-repository';
 import { AssignorRepository } from '../assignor/repositories/assignor-repository';
 
@@ -35,11 +36,34 @@ describe('PayableController', () => {
   });
 
   it('should create a payable and return it', async () => {
-    jest.spyOn(service, 'create').mockImplementation(async () => payableMock);
+    jest.spyOn(service, 'create').mockResolvedValue(payableMock);
 
     const payable = await controller.create(payableMock);
 
     expect(payable).toEqual(payableMock);
     expect(payable).toBeInstanceOf(Payable);
+  });
+
+  it('should return all payables', async () => {
+    jest.spyOn(service, 'getAll').mockResolvedValue([payableMock]);
+
+    const payables = await controller.getAll();
+
+    expect(payables).toEqual([payableMock]);
+  });
+
+  it('should return the payable when updated', async () => {
+    const newPayable = new Payable('1', 50, assignorMock.id, new Date());
+    jest.spyOn(service, 'update').mockResolvedValue(newPayable);
+
+    const payable = await controller.update('1', newPayable);
+
+    expect(payable).toEqual(newPayable);
+  });
+
+  it('should not return nothing when deled a payable', async () => {
+    jest.spyOn(service, 'delete').mockResolvedValue(undefined);
+
+    expect(await controller.delete('1')).toBeUndefined();
   });
 });
--- packages/api/src/payable/payable.controller.ts
+++ packages/api/src/payable/payable.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Post } from '@nestjs/common';
 import { PayableService } from './payable.service';
 import { CreatePayableDto } from './dto/create-payable.dto';
+import { UpdatePayableDto } from './dto/update-payable.dto';
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Patch, Post } from '@nestjs/common';
 
 @Controller()
 export class PayableController {
@@ -9,5 +10,26 @@ export class PayableController {
   @Post()
   async create(@Body() createPayableDto: CreatePayableDto) {
     return this.payableService.create(createPayableDto);
+  }
+
+  @Get()
+  async getAll() {
+    return this.payableService.getAll();
+  }
+
+  @Get(':id')
+  async findById(@Param('id') id: string) {
+    return this.payableService.findById(id);
+  }
+
+  @Patch(':id')
+  async update(@Param('id') id: string, @Body() updatePayableDto: UpdatePayableDto) {
+    return this.payableService.update(id, updatePayableDto);
+  }
+
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async delete(@Param('id') id: string) {
+    return this.payableService.delete(id);
   }
 }
