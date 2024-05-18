be336248737f1771f0ed47318b6ad3ea59e8c9c6
Samuel Deotti <samuel_deotti@hotmail.com>
2024-05-18 20:11:08 -0300

    adding user model and auth

--- assignor/assignor.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Controller } from '@nestjs/common';
-import { AssignorService } from './assignor.service';
-
-@Controller('assignor')
-export class AssignorController {
-  constructor(private readonly assignorService: AssignorService) {}
-}
