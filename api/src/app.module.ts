71429e32de0c5f5d44e71655eb1a1c134371fc0c
Emerson <emerson.vv75248@gmail.com>
2024-05-17 23:56:21 -0300

    chore: Add controllers and services for Receivable and Assignor

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaService } from './services/prisma.service';
+import { ReceivableService } from './services/receivable.service';
+import { AssignorService } from './services/assignor.service';
+import { ReceivableController } from './controllers/receivable.controller';
+import { AssignorController } from './controllers/assignor.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, ReceivableController, AssignorController],
+  providers: [AppService, PrismaService, ReceivableService, AssignorService],
 })
 export class AppModule {}
