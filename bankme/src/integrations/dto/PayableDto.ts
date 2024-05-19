c48d54c9f48a9e395315b893cf7afa7c37255f85
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-05-19 10:46:11 -0300

    refactor(payable): refatorando método toEntity, criando método fromEntity

--- bankme/src/integrations/dto/PayableDto.ts
+++ bankme/src/integrations/dto/PayableDto.ts
@@ -1,13 +1,34 @@
 import Payable from '../entity/Payable';
-import { IPayableCreation } from '../types';
+import { IPayable } from '../types/IPayables';
 
 export default class PayableDto extends Payable {
-  static toEntity(payable: IPayableCreation): Payable {
+  toEntity(): Payable {
     const payableEntity = new Payable();
 
-    payableEntity.value = payable.value;
-    payableEntity.emissionDate = payable.emissionDate;
+    payableEntity.value = this.value;
+    payableEntity.emissionDate = this.emissionDate;
+    payableEntity.assignorId = this.assignorId;
 
     return payableEntity;
+  }
+
+  static fromEntity(payable: IPayable): PayableDto {
+    const payableDto = new PayableDto();
+
+    payableDto.id = payable.id;
+    payableDto.value = payable.value;
+    payableDto.emissionDate = payable.emissionDate;
+    payableDto.assignorId = payable.assignorId;
+
+    return payableDto;
+  }
+
+  toJSON() {
+    return {
+      id: this.id,
+      value: this.value,
+      emissionDate: this.emissionDate,
+      assignorId: this.assignorId,
+    };
   }
 }
