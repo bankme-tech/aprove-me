3571d4ca409ef8272684033b5bab4449003ab355
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-05-19 10:49:40 -0300

    refactor(payable): removendo propriedade assignorId de IPayable

--- bankme/src/integrations/types/IPayables.ts
+++ bankme/src/integrations/types/IPayables.ts
@@ -3,8 +3,7 @@ import { ID } from '.';
 export interface IPayableValues {
   value: number;
   emissionDate: Date;
-}
-
-export interface IPayable extends ID, IPayableValues {
   assignorId: string;
 }
+
+export interface IPayable extends ID, IPayableValues {}
