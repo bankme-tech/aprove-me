f340f29abbbca71129e1e674f36e750270d9c226
Caio Fernando <caiofsr13@gmail.com>
2024-05-19 13:05:47 -0300

    improve prisma exception filter

--- packages/api/src/exception-filters/prisma.exception-filter.ts
+++ packages/api/src/exception-filters/prisma.exception-filter.ts
@@ -8,16 +8,20 @@ export class PrismaExceptionFilter implements ExceptionFilter {
     const res = ctx.getResponse();
 
     if (exception.code === 'P2025') {
+      const model = exception.meta.modelName as string;
+
       return res.status(404).json({
         statusCode: 404,
-        message: `entity ${exception.meta.modelName} not found`,
+        message: `${model.toLocaleLowerCase()} not found`,
       });
     }
 
     if (exception.code === 'P2002') {
+      const model = exception.meta.modelName as string;
+
       return res.status(400).json({
         statusCode: 400,
-        message: 'user already exists',
+        message: `${model.toLocaleLowerCase()} already exists`,
       });
     }
 
