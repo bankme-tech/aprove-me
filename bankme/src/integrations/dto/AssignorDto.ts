d4a7170d3490c77af3c62cc6cd680b888500bf5e
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-05-19 10:45:39 -0300

    refactor(assignor): mudando método toEntity, criando método fromEntity

--- bankme/src/integrations/dto/AssignorDto.ts
+++ bankme/src/integrations/dto/AssignorDto.ts
@@ -1,15 +1,37 @@
 import Assignor from '../entity/Assignor';
-import { IPayableCreation } from '../types';
+import { IAssignor } from '../types/IAssignor';
 
 export default class AssignorDto extends Assignor {
-  static toEntity(payable: IPayableCreation): Assignor {
+  toEntity(): Assignor {
     const payableEntity = new Assignor();
 
-    payableEntity.document = payable.assignor.document;
-    payableEntity.email = payable.assignor.email;
-    payableEntity.name = payable.assignor.name;
-    payableEntity.phone = payable.assignor.phone;
+    payableEntity.document = this.document;
+    payableEntity.email = this.email;
+    payableEntity.name = this.name;
+    payableEntity.phone = this.phone;
 
     return payableEntity;
+  }
+
+  static fromEntity(assignor: IAssignor): AssignorDto {
+    const assignorDto = new AssignorDto();
+
+    assignorDto.id = assignor.id;
+    assignorDto.document = assignor.document;
+    assignorDto.email = assignor.email;
+    assignorDto.name = assignor.name;
+    assignorDto.phone = assignor.phone;
+
+    return assignorDto;
+  }
+
+  toJSON() {
+    return {
+      id: this.id,
+      document: this.document,
+      email: this.email,
+      phone: this.phone,
+      name: this.name,
+    };
   }
 }
