46cf03a004b04519098209fc568e103aa75f1b70
jjribeiro1 <jvsr99@hotmail.com>
2024-05-19 20:11:10 -0300

    [backend] CryptoService test

--- bankme-backend/src/crypto/crypto.service.spec.ts
+++ bankme-backend/src/crypto/crypto.service.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CryptoService } from './crypto.service';
+import * as bcrypt from 'bcrypt';
 
 describe('CryptoService', () => {
   let service: CryptoService;
@@ -12,7 +13,58 @@ describe('CryptoService', () => {
     service = module.get<CryptoService>(CryptoService);
   });
 
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('hash', () => {
+    it('should return a hashed string', async () => {
+      const value = 'testValue';
+      const salt = 10;
+      const hashedValue = 'hashedValue';
+
+      jest
+        .spyOn(bcrypt, 'hash')
+        .mockImplementationOnce(() => Promise.resolve(hashedValue));
+
+      const result = await service.hash(value, salt);
+
+      expect(result).toBe(hashedValue);
+      expect(bcrypt.hash).toHaveBeenCalledWith(value, salt);
+    });
+  });
+
+  describe('compare', () => {
+    it('should return true for a valid comparison', async () => {
+      const value = 'testValue';
+      const hash = 'hashedValue';
+
+      jest
+        .spyOn(bcrypt, 'compare')
+        .mockImplementationOnce(() => Promise.resolve(true));
+
+      const result = await service.compare(value, hash);
+
+      expect(result).toBe(true);
+      expect(bcrypt.compare).toHaveBeenCalledWith(value, hash);
+    });
+
+    it('should return false for an invalid comparison', async () => {
+      const value = 'testValue';
+      const hash = 'hashedValue';
+
+      jest
+        .spyOn(bcrypt, 'compare')
+        .mockImplementationOnce(() => Promise.resolve(false));
+
+      const result = await service.compare(value, hash);
+
+      expect(result).toBe(false);
+      expect(bcrypt.compare).toHaveBeenCalledWith(value, hash);
+    });
   });
 });
