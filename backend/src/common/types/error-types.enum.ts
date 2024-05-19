9fcb02e4e28a1874916ede7a7ee5fdda8cee7a1e
felipejhordan <felipejordan.alves@gmail.com>
2024-05-19 16:53:48 -0300

    feat(backend-1): set  logger and global pipes/filter/prefix in main file

---
+++ backend/src/common/types/error-types.enum.ts
@@ -0,0 +1,6 @@
+export enum ErrorType {
+  NOT_FOUND_ERROR,
+  SYNC_SERVICE_ERROR,
+  DATABASE_ERROR,
+  BAD_REQUEST_ERROR,
+}
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger, ValidationPipe } from '@nestjs/common';
+
 import { AppModule } from './app.module';
+import { BASE_PATH } from './common/constants/paths';
+import { GeneralErrorFilter } from './common/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const logger = new Logger('Main');
+
+  app.useGlobalFilters(new GeneralErrorFilter(logger));
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  app.setGlobalPrefix(BASE_PATH);
+
   await app.listen(3000);
 }
 bootstrap();
