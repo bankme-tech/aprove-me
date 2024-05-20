5c09d39b7c9e701d0dbb58059d04c72544f0d2e6
jjribeiro1 <jvsr99@hotmail.com>
2024-05-20 16:04:23 -0300

    [backend] swagger setup

--- bankme-backend/src/main.ts
+++ bankme-backend/src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -11,6 +12,18 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Bankme Api')
+    .setVersion('1.0')
+    .addTag('auth')
+    .addTag('payable')
+    .addTag('assignor')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
