f499720983f108154547a04a64dadd1bc5f0dfa6
Caio Fernando <caiofsr13@gmail.com>
2024-05-20 16:43:40 -0300

    add modal to login

--- packages/web/src/components/header.tsx
+++ packages/web/src/components/header.tsx
@@ -1,5 +1,16 @@
 import Image from "next/image";
-import { Button } from "./ui/button";
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
 export default function Header() {
   return (
@@ -15,7 +26,33 @@ export default function Header() {
       <div className="flex items-center space-x-5 text-gray-500">
         <Button variant="outline">Pagáveis</Button>
         <Button variant="outline">Cedentes</Button>
-        <Button>Login</Button>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button>Login</Button>
+          </DialogTrigger>
+          <DialogContent className="sm:max-w-[425px]">
+            <DialogHeader className="mt-4">
+              <DialogDescription>Faça o seu login para poder fazer alterações e adicionar pagáveis e cedentes</DialogDescription>
+            </DialogHeader>
+            <div className="grid gap-4 py-4">
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="login" className="text-right">
+                  Login
+                </Label>
+                <Input id="login" className="col-span-3" />
+              </div>
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="password" className="text-right">
+                  Password
+                </Label>
+                <Input id="password" className="col-span-3" type="password" />
+              </div>
+            </div>
+            <DialogFooter>
+              <Button type="submit">Login</Button>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
       </div>
     </header>
   );
