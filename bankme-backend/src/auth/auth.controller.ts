1cdd6d4f60b2f6cefcfe9c3215f186abf934e11a
jjribeiro1 <jvsr99@hotmail.com>
2024-05-19 23:02:31 -0300

    [backend] add Public decorator to mark a route as public

--- bankme-backend/src/auth/auth.controller.ts
+++ bankme-backend/src/auth/auth.controller.ts
@@ -1,11 +1,13 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { CredentialsDto } from './dto/auth.dto';
 import { AuthService } from './auth.service';
+import { Public } from 'src/utils/isPublic.decorator';
 
 @Controller('integrations')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @Public()
   @Post('auth')
   async signIn(@Body() dto: CredentialsDto) {
     return this.authService.signIn(dto)
