e6eca0dce8dfe73d63e5fc4c90d2a0c2fe9075c9
jjribeiro1 <jvsr99@hotmail.com>
2024-05-20 15:44:40 -0300

    [backend] remove unnecessary files

--- bankme-backend/src/app.module.ts
+++ bankme-backend/src/app.module.ts
@@ -1,20 +1,18 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { PayableModule } from './payable/payable.module';
-import { AssignorModule } from './assignor/assignor.module';
-import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { DbModule } from './db/db.module';
+import { PayableModule } from './payable/payable.module';
+import { AssignorModule } from './assignor/assignor.module';
 import { UserModule } from './user/user.module';
 import { CryptoModule } from './crypto/crypto.module';
 import { AuthModule } from './auth/auth.module';
-import { validate } from './config/env.validation'
+import { validate } from './config/env.validation';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      validate
+      validate,
     }),
     DbModule,
     PayableModule,
@@ -23,7 +21,5 @@ import { validate } from './config/env.validation'
     CryptoModule,
     AuthModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
