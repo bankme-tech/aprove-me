6b3ef220b158648bd2aa07acd3f815201285853d
jjribeiro1 <jvsr99@hotmail.com>
2024-05-20 15:42:16 -0300

    [backend] ensure that the application only runs with all environment variables defined

--- bankme-backend/src/app.module.ts
+++ bankme-backend/src/app.module.ts
@@ -3,13 +3,26 @@ import { AppController } from './app.controller';
 import { PayableModule } from './payable/payable.module';
 import { AssignorModule } from './assignor/assignor.module';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 import { DbModule } from './db/db.module';
 import { UserModule } from './user/user.module';
 import { CryptoModule } from './crypto/crypto.module';
 import { AuthModule } from './auth/auth.module';
+import { validate } from './config/env.validation'
 
 @Module({
-  imports: [DbModule, PayableModule, AssignorModule, UserModule, CryptoModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validate
+    }),
+    DbModule,
+    PayableModule,
+    AssignorModule,
+    UserModule,
+    CryptoModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ bankme-backend/src/config/env.validation.ts
@@ -0,0 +1,30 @@
+import { plainToInstance } from 'class-transformer';
+import { IsNotEmpty, IsString, validateSync } from 'class-validator';
+
+class EnvironmentVariables {
+  @IsString()
+  @IsNotEmpty()
+  DATABASE_URL: string;
+
+  @IsString()
+  @IsNotEmpty()
+  JWT_SECRET: string;
+
+  @IsString()
+  @IsNotEmpty()
+  JWT_EXPIRES_IN: string;
+}
+
+export function validate(config: Record<string, unknown>) {
+  const validatedConfig = plainToInstance(EnvironmentVariables, config, {
+    enableImplicitConversion: true,
+  });
+  const errors = validateSync(validatedConfig, {
+    skipMissingProperties: false,
+  });
+
+  if (errors.length > 0) {
+    throw new Error(errors.toString());
+  }
+  return validatedConfig;
+}
