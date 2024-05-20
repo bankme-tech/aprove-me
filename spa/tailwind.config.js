543bcd1a0098c13bcfc5fb6f73ec5b1605dd9045
Emerson Vieira <emersondont@gmail.com>
2024-05-20 02:25:10 -0300

    chore: Update fonts and colors in SPA

--- spa/tailwind.config.js
+++ spa/tailwind.config.js
@@ -4,7 +4,20 @@ module.exports = {
     './src/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'Nunito': ['Nunito Sans', 'sans-serif']
+      },
+      colors: {
+        bgColor: '#fdfefe',
+        HeaderBgColor: '#ffffff',
+        textColor: '#333333',
+        themeColor: '#0a36b0',
+        borderColor: '#d1d5db',
+        cardBgColorFrom: '#EAF1FC',
+        cardBgColorto: '#F1F5F9'
+      }
+    },
   },
   plugins: [],
 }
