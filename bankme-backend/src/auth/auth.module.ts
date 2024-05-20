c166eee063fa669429faeb9bf582725a1cd58bc0
jjribeiro1 <jvsr99@hotmail.com>
2024-05-20 15:43:15 -0300

    [backend] provide jwt options with config service

--- bankme-backend/src/auth/auth.module.ts
+++ bankme-backend/src/auth/auth.module.ts
@@ -6,14 +6,20 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { AuthGuard } from './guards/auth.guard';
 import { APP_GUARD } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     UserModule,
     CryptoModule,
-    JwtModule.register({
-      secret: process.env.JWT_SECRET,
-      signOptions: { expiresIn: '60s' },
+    JwtModule.registerAsync({
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET'),
+        signOptions: {
+          expiresIn: configService.get('JWT_EXPIRES_IN'),
+        },
+      }),
+      inject: [ConfigService],
     }),
   ],
   controllers: [AuthController],
