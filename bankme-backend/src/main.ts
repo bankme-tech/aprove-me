ee47ac7e2f690bdedf3df59b533e172834f64ca7
jjribeiro1 <jvsr99@hotmail.com>
2024-05-20 19:25:14 -0300

    [backend] change port

--- bankme-backend/src/main.ts
+++ bankme-backend/src/main.ts
@@ -27,6 +27,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  await app.listen(3001);
 }
 bootstrap();
