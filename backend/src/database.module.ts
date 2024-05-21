2a12c23aa058e3c4ce46bf159984abf06efb447a
Mário Santos <mariodev7@gmail.com>
2024-05-21 18:02:51 -0300

    fix(backend): exports account provider in database module

--- backend/src/database.module.ts
+++ backend/src/database.module.ts
@@ -4,13 +4,16 @@ import { PrismaAssignorRepository } from './modules/assignor/repositories/prisma
 import { IAssignorRepository } from './modules/assignor/repositories/interfaces/assignor.repository-interface';
 import { IPayableRepository } from './modules/payable/repositories/interfaces/payable.repository-interface';
 import { PrismaPayableRepository } from './modules/payable/repositories/prisma/payable.repository';
+import { PrismaAccountRepository } from './modules/auth/repositories/prisma/account.repository';
+import { IAccountRepository } from './modules/auth/repositories/interfaces/account.repository-interface';
 
 @Module({
   providers: [
     PrismaService,
     { useClass: PrismaAssignorRepository, provide: IAssignorRepository },
     { useClass: PrismaPayableRepository, provide: IPayableRepository },
+    { useClass: PrismaAccountRepository, provide: IAccountRepository },
   ],
-  exports: [IAssignorRepository, IPayableRepository],
+  exports: [IAssignorRepository, IPayableRepository, IAccountRepository],
 })
 export class DatabaseModule {}
