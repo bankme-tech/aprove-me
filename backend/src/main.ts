7320f9c4e9a7259a8daee7df17e564a7c564f76b
Arthur Almeida <arthursa.dev@gmail.com>
2024-05-21 12:41:34 -0300

    feat: register validation pipe as global pipe

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('integrations');
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
