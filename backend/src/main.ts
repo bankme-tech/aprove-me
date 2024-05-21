9dd231d84a60bf789513964baa977a2a3096681d
Mário Santos <mariodev7@gmail.com>
2024-05-20 22:14:40 -0300

    feat(backend): initialize nest-app

---
+++ backend/src/main.ts
@@ -0,0 +1,10 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({ errorHttpStatusCode: 422 }));
+  await app.listen(3000);
+}
+bootstrap();
