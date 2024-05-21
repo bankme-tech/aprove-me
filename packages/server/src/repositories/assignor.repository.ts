ee7c4e2aeac5c227a6d150def47b856fa90d46ce
Yazalde Filimone <yazaldefilimon@gmail.com>
2024-05-21 19:56:18 +0200

    feat:  get methods

--- packages/server/src/repositories/assignor.repository.ts
+++ packages/server/src/repositories/assignor.repository.ts
@@ -1,12 +1,21 @@
 import { Result } from 'src/types/either';
-import { assignorResponseDto, assignorBodyDto } from 'src/dtos/assignor.dto';
+import { assignorResponseDto, assignorBodyDto, assignorUniqueResponseType } from 'src/dtos/assignor.dto';
 
 export abstract class AssignorRepository {
   abstract create_assignor(assignor: AssignorRepository.bodyType): AssignorRepository.IdResponseType;
   abstract get_assignor(id: string): AssignorRepository.responseType;
   abstract get_list_assignor(): AssignorRepository.listResponseType;
   abstract delete_assignor(id: string): Promise<Result<Error, void>>;
-  abstract update_assignor(id: string, assignor: AssignorRepository.bodyType): AssignorRepository.IdResponseType;
+  abstract get_assignor_by_document(document: string): AssignorRepository.responseType;
+  abstract get_assignor_by_email(email: string): AssignorRepository.responseType;
+  abstract get_assignor_by_phone(phone: string): AssignorRepository.responseType;
+  abstract get_unique_assignor(
+    assignor: Partial<AssignorRepository.bodyType>,
+  ): Promise<Result<Error, assignorUniqueResponseType>>;
+  abstract update_assignor(
+    id: string,
+    assignor: Partial<AssignorRepository.bodyType>,
+  ): AssignorRepository.IdResponseType;
 }
 
 export namespace AssignorRepository {
