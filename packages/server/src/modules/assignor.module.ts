80030a64b3db217008cd82c1d105b8a4c9df79dc
Yazalde Filimone <yazaldefilimon@gmail.com>
2024-05-21 18:26:04 +0200

    fix: remove imports and add providers

--- packages/server/src/modules/assignor.module.ts
+++ packages/server/src/modules/assignor.module.ts
@@ -1,14 +1,16 @@
 import { Module } from '@nestjs/common';
 import { ReceivableController } from 'src/controllers/receivable.controller';
+import { PrismaService } from 'src/database/prisma.config';
 import { PrismaReceivableRepository } from 'src/repositories/prisma/prisma.receivable.repository';
 import { ReceivableRepository } from 'src/repositories/receivable.repository';
 
 import { ReceivableService } from 'src/services/receivable.service';
 
 @Module({
-  imports: [PrismaReceivableRepository],
+  imports: [],
   controllers: [ReceivableController],
   providers: [
+    PrismaService,
     ReceivableService,
     {
       provide: ReceivableRepository,
--- packages/server/src/modules/receivable.module.ts
+++ packages/server/src/modules/receivable.module.ts
@@ -1,14 +1,16 @@
 import { Module } from '@nestjs/common';
 
 import { AssignorController } from 'src/controllers/assignor.controller';
+import { PrismaService } from 'src/database/prisma.config';
 import { AssignorRepository } from 'src/repositories/assignor.repository';
 import { PrismaAssignorRepository } from 'src/repositories/prisma/prisma.assignor.repository';
 import { AssignorService } from 'src/services/assignor.service';
 
 @Module({
-  imports: [PrismaAssignorRepository],
+  imports: [],
   controllers: [AssignorController],
   providers: [
+    PrismaService,
     AssignorService,
     {
       provide: AssignorRepository,
