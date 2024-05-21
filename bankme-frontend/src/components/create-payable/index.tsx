936cb190bb5fd1cd5a38f807256aacf1532d160d
jjribeiro1 <jvsr99@hotmail.com>
2024-05-21 10:51:43 -0300

    [frontend] add query invalidation after createPayableMutation

--- bankme-frontend/src/components/create-payable/index.tsx
+++ bankme-frontend/src/components/create-payable/index.tsx
@@ -1,8 +1,9 @@
 "use client";
+import { useState } from "react";
 import { z } from "zod";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Button } from "@/components/ui/button";
+import { Button, buttonVariants } from "@/components/ui/button";
 import {
   Form,
   FormControl,
@@ -12,7 +13,8 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { useMutation } from "@tanstack/react-query";
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { createPayable } from "@/services/payable";
 
 const dateSchema = z.coerce.date();
@@ -30,6 +32,8 @@ const formSchema = z.object({
 });
 
 export default function CreatePayableForm() {
+  const [open, setOpen] = useState(false);
+
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -39,60 +43,75 @@ export default function CreatePayableForm() {
     },
   });
 
-  const mutation = useMutation({
+  const queryClient = useQueryClient();
+  const { mutate: createPayableMutation } = useMutation({
     mutationFn: createPayable,
+    onSuccess() {
+      form.reset();
+      setOpen(false);
+      queryClient.invalidateQueries({ queryKey: ["get-all-payable"] });
+    },
   });
 
   function onSubmit(data: z.infer<typeof formSchema>) {
     const { assignorId, emissionDate, value } = data;
-    mutation.mutate({ assignorId, emissionDate, value });
+    createPayableMutation({ assignorId, emissionDate, value });
   }
 
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
-        <FormField
-          control={form.control}
-          name="value"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Value</FormLabel>
-              <FormControl>
-                <Input type="number" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="emissionDate"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Emission date</FormLabel>
-              <FormControl>
-                <Input type="date" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="assignorId"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Assignor ID</FormLabel>
-              <FormControl>
-                <Input {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+    <>
+      <Dialog open={open} onOpenChange={setOpen}>
+        <DialogTrigger className={buttonVariants()}>
+          Create new payable
+        </DialogTrigger>
+        <DialogContent>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+              <FormField
+                control={form.control}
+                name="value"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Value</FormLabel>
+                    <FormControl>
+                      <Input type="number" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="emissionDate"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Emission date</FormLabel>
+                    <FormControl>
+                      <Input type="date" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="assignorId"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Assignor ID</FormLabel>
+                    <FormControl>
+                      <Input {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
 
-        <Button type="submit">Submit</Button>
-      </form>
-    </Form>
+              <Button type="submit">Submit</Button>
+            </form>
+          </Form>
+        </DialogContent>
+      </Dialog>
+    </>
   );
 }
