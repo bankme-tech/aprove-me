836bd013a4ae835ccc24b5f018010ba02922914b
Caique Torres <caiquetorres@Caiques-MacBook-Air.local>
2024-05-21 18:33:07 -0300

    feat: created the project resposible for saving many payables

---
+++ apps/bulk-payable-service/.eslintrc.js
@@ -0,0 +1,77 @@
+module.exports = {
+  extends: ['../../.eslintrc.js'],
+  plugins: ['import-helpers'],
+  overrides: [
+    {
+      files: ['*.decorator.ts'],
+      rules: {
+        '@typescript-eslint/naming-convention': [
+          'error',
+          {
+            selector: 'function',
+            format: ['PascalCase', 'camelCase'],
+            leadingUnderscore: 'allow',
+          },
+        ],
+      },
+    },
+    {
+      files: ['env.ts'],
+      rules: {
+        '@typescript-eslint/naming-convention': [
+          'error',
+          {
+            selector: 'classProperty',
+            modifiers: ['private'],
+            format: ['camelCase', 'UPPER_CASE'],
+            leadingUnderscore: 'require',
+          },
+          {
+            selector: 'classProperty',
+            modifiers: ['public'],
+            format: ['camelCase', 'UPPER_CASE'],
+            leadingUnderscore: 'forbid',
+          },
+        ],
+      },
+    },
+    {
+      files: ['*.document.ts'],
+      rules: {
+        '@typescript-eslint/naming-convention': [
+          'error',
+          {
+            selector: 'classProperty',
+            modifiers: ['private'],
+            format: ['camelCase'],
+          },
+          {
+            selector: ['variable'],
+            format: ['camelCase', 'UPPER_CASE', 'PascalCase'],
+            leadingUnderscore: 'allow',
+          },
+        ],
+      },
+    },
+  ],
+  rules: {
+    'import-helpers/order-imports': [
+      'warn',
+      {
+        newlinesBetween: 'always',
+        groups: [
+          '/@nestjs|^mongoose|@automock|vitest|supertest/',
+          '/@bankme/',
+          '/@domain/',
+          '/@infra/',
+          '/@application/',
+          '/@presentation/',
+          ['index'],
+        ],
+        alphabetize: {
+          ignoreCase: true,
+        },
+      },
+    ],
+  },
+};
