e642f0d989edad7ae6172e4a1550010b69d34824
Mário Santos <mariodev7@gmail.com>
2024-05-21 18:10:17 -0300

    feat(backend): create auth guard

---
+++ backend/src/modules/auth/guars/auth.guard.ts
@@ -0,0 +1,43 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { Request } from 'express';
+
+type TokenPayload = {
+  sub: string;
+  login: string;
+};
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private jwtService: JwtService) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+
+    const token = this.extractTokenFromHeader(request);
+
+    if (!token) throw new UnauthorizedException('Não autorizado.');
+
+    try {
+      const payload = await this.jwtService.verifyAsync<TokenPayload>(token, {
+        secret: process.env.SECRET,
+      });
+
+      request['account'] = payload;
+    } catch {
+      throw new UnauthorizedException('Expired token.');
+    }
+
+    return true;
+  }
+
+  private extractTokenFromHeader(request: Request): string | undefined {
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
+  }
+}
