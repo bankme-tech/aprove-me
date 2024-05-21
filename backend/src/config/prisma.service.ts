ffea12a48a313d2d0302d525f2afc2ab08af2e4c
Igor Soares <igor.soares.developer@gmail.com>
2024-05-21 16:06:10 -0300

    feat(backend): add method to close connection to prisma service

--- backend/src/config/prisma.service.ts
+++ backend/src/config/prisma.service.ts
@@ -1,9 +1,16 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
   async onModuleInit() {
     await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
   }
 }
