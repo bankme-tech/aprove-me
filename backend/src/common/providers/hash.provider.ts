99048fb4d9b89e0004ed19d2a5c030c96614a353
Mário Santos <mariodev7@gmail.com>
2024-05-21 17:37:45 -0300

    feat(backend): create common hash provider

---
+++ backend/src/common/providers/hash.provider.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+import { hash, compare } from 'bcrypt';
+
+@Injectable()
+export class HashProvider {
+  private HASH_SALT_LENGTH = 8;
+
+  hash(plain: string): Promise<string> {
+    return hash(plain, this.HASH_SALT_LENGTH);
+  }
+
+  compare(plain: string, hash: string): Promise<boolean> {
+    return compare(plain, hash);
+  }
+}
