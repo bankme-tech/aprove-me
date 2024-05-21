a1e6e41287ef4e70c2465d5aa25eb91269824bed
Renato Vicente <rntvicente@gmail.com>
2024-05-21 15:30:50 -0300

    feat: objeto de valor phone

--- src/domain/common/value-object/index.ts
+++ src/domain/common/value-object/index.ts
@@ -2,3 +2,4 @@ export * from './unique-entity-id.vo';
 export * from './email.vo';
 export * from './cpf.vo';
 export * from './cnpj.vo';
+export * from './phone.vo';
---
+++ src/domain/common/value-object/phone.vo.ts
@@ -0,0 +1,20 @@
+import { isValidMobilePhone } from '@brazilian-utils/brazilian-utils';
+import { ValueObject } from './value-object';
+import { InvalidFieldError } from '../exception';
+
+export class PhoneVO extends ValueObject<string> {
+  constructor(number: string) {
+    super(number.trim().replace(/\D/g, ''));
+    this.validate();
+  }
+
+  private validate() {
+    const isValid = isValidMobilePhone(this._value);
+
+    if (!isValid) throw new InvalidFieldError('phone');
+  }
+
+  toString() {
+    return this._value.replace(/(\d{2})(\d{5})(\d{4})/, '($1) $2-$3');
+  }
+}
