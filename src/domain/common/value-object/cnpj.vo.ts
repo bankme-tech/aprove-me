662a22807441564b7a623b1fd2b75e1ef922fcbd
Renato Vicente <rntvicente@gmail.com>
2024-05-21 15:05:20 -0300

    feat: objeto de valor CNPJ

---
+++ src/domain/common/value-object/cnpj.vo.ts
@@ -0,0 +1,21 @@
+import { isValidCNPJ, formatCNPJ } from '@brazilian-utils/brazilian-utils';
+
+import { InvalidFieldError } from '../exception';
+import { ValueObject } from "./value-object";
+
+export class CnpjVO extends ValueObject<string> {
+  constructor(cnpj: string) {
+    super(cnpj.replace(/\D/g, ''));
+    this.validate();
+  }
+
+  private validate() {
+    const isValid = isValidCNPJ(this._value);
+
+    if (!isValid) throw new InvalidFieldError('cnpj');
+  }
+
+  toString () {
+    return formatCNPJ(this._value, { pad: true });
+  };
+};
--- src/domain/common/value-object/index.ts
+++ src/domain/common/value-object/index.ts
@@ -1,3 +1,4 @@
 export * from './unique-entity-id.vo';
 export * from './email.vo';
 export * from './cpf.vo';
+export * from './cnpj.vo';
