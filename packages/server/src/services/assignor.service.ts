01404a4efe303c82d3e0d6df9914182443aa412a
Yazalde Filimone <yazaldefilimon@gmail.com>
2024-05-21 19:55:19 +0200

    feat: validations

--- packages/server/src/services/assignor.service.ts
+++ packages/server/src/services/assignor.service.ts
@@ -1,26 +1,66 @@
 import { Injectable } from '@nestjs/common';
 import { AssignorRepository } from 'src/repositories/assignor.repository';
-import { Result } from 'src/types/either';
+import { Err, Ok, Result } from 'src/types/either';
+import { ValidationService } from './validations.service';
+import { CustomError } from 'src/validations/errors';
 
 @Injectable()
 export class AssignorService {
   private assignor_repository: AssignorRepository;
-  constructor(assignor_repository: AssignorRepository) {
+  private validation_service: ValidationService;
+  constructor(assignor_repository: AssignorRepository, validation_service: ValidationService) {
     this.assignor_repository = assignor_repository;
+    this.validation_service = validation_service;
   }
   async create_assignor(assignor: AssignorRepository.bodyType): Promise<AssignorRepository.IdResponseType> {
-    return this.assignor_repository.create_assignor(assignor);
+    try {
+      const data = this.validation_service.validateAssignor(assignor);
+      const try_assignor = await this.assignor_repository.get_unique_assignor(data);
+      if (try_assignor.isError()) {
+        return try_assignor;
+      }
+      if (try_assignor.value.id) {
+        return Err(new CustomError(`${try_assignor.value.field} is already registered.`));
+      }
+      const result = await this.assignor_repository.create_assignor(data);
+      return result;
+    } catch (error) {
+      return Err(error);
+    }
   }
   async get_assignor(id: string): Promise<AssignorRepository.responseType> {
-    return this.assignor_repository.get_assignor(id);
+    try {
+      const id_result = this.validation_service.validateUuid(id);
+      const is_error = await this.assignor_repository.get_assignor(id_result);
+      if (is_error.isError()) {
+        return is_error;
+      }
+      const assignor = is_error.value;
+      return Ok(assignor);
+    } catch (error) {
+      return Err(error);
+    }
   }
   async get_list_assignor(): Promise<AssignorRepository.listResponseType> {
-    return this.assignor_repository.get_list_assignor();
+    const assignors = await this.assignor_repository.get_list_assignor();
+    return assignors;
   }
   async delete_assignor(id: string): Promise<Result<Error, void>> {
-    return this.assignor_repository.delete_assignor(id);
+    try {
+      const id_result = this.validation_service.validateUuid(id);
+      await this.assignor_repository.delete_assignor(id_result);
+    } catch (error) {
+      return Err(error);
+    }
   }
   async update_assignor(id: string, assignor: AssignorRepository.bodyType): Promise<AssignorRepository.IdResponseType> {
-    return this.assignor_repository.update_assignor(id, assignor);
+    try {
+      const id_result = this.validation_service.validateUuid(id);
+      const data = this.validation_service.validateUpdateAssignor(assignor);
+      const result = await this.assignor_repository.update_assignor(id_result, data);
+      return result;
+    } catch (error) {
+      return Err(error);
+    }
   }
 }
