f093c2cb30cffa881697e694afd566b480ae8ed2
Yazalde Filimone <yazaldefilimon@gmail.com>
2024-05-21 17:24:17 +0200

    feat: group all module

--- packages/server/src/modules/app.module.ts
+++ packages/server/src/modules/app.module.ts
@@ -5,12 +5,16 @@ import { AppController } from '../controllers/app.controller';
 import { AppService } from '../services/app.service';
 
 import config from '../shared/config';
+import { AssignorModule } from './assignor.module';
+import { ReceivableModule } from './receivable.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       load: [config],
     }),
+    AssignorModule,
+    ReceivableModule,
   ],
   controllers: [AppController],
   providers: [AppService],
