3011c337289e0b39e8c592162444bb0aae9cda47
Renato Vicente <rntvicente@gmail.com>
2024-05-20 22:30:29 -0300

    feat: definindo contrato value object

---
+++ src/domain/common/value-object/value-object.ts
@@ -0,0 +1,63 @@
+import isEqual from 'lodash/isEqual';
+
+export abstract class ValueObject<Value = any> {
+  protected readonly _value: Value;
+
+  constructor(value: Value) {
+    this._value = deepFreeze(value);
+  }
+
+  get value(): Value {
+    return this._value;
+  }
+
+  public equals(obj: this): boolean {
+    if (obj === null || obj === undefined) {
+      return false;
+    }
+
+    if (obj.value === undefined) {
+      return false;
+    }
+
+    if (obj.constructor.name !== this.constructor.name) {
+      return false;
+    }
+
+    return isEqual(this.value, obj.value);
+  }
+
+  toString = () => {
+    if (typeof this.value !== 'object' || this.value === null) {
+      try {
+        return this.value.toString();
+      } catch (e) {
+        return this.value + '';
+      }
+    }
+
+    const valueStr = this.value.toString();
+    
+    return valueStr === '[object Object]'
+      ? JSON.stringify(this.value)
+      : valueStr;
+  };
+}
+
+export function deepFreeze<T>(obj: T) {
+  try {
+    const propNames = Object.getOwnPropertyNames(obj);
+
+    for (const name of propNames) {
+      const value = obj[name as keyof T];
+
+      if (value && typeof value === 'object') {
+        deepFreeze(value);
+      }
+    }
+
+    return Object.freeze(obj);
+  } catch (e) {
+    return obj;
+  }
+}
