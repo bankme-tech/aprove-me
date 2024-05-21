956973057eff0a0889190909126dd09076670e13
Breno Godoy <godoyy25z@gmail.com>
2024-05-21 18:24:57 -0300

    :wrench: fix: Corrige contextAPI que não funcionava da maneira esperada e adiciona CORS ao back

--- bankme-api/src/main.ts
+++ bankme-api/src/main.ts
@@ -4,6 +4,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept', 
+  });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
