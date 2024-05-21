8e6fc2724e809a375e2b3f781bda78d3fec09cc9
Renato Vicente <rntvicente@gmail.com>
2024-05-21 12:44:22 -0300

    feat: definindo dominiuo de recebiveis

---
+++ src/domain/receivable.entity.ts
@@ -0,0 +1,36 @@
+import { UniqueEntityIdVO } from "./common/value-object";
+import { Entity } from "./entity";
+
+export type ReceivableProps = {
+  id?: string;
+  value: number;
+  emissionDate: Date | string;
+  assignor: string;
+}
+
+export class ReceivableEntity extends Entity {
+  readonly id: UniqueEntityIdVO;
+  readonly value: number;
+  readonly emissonDate: Date;
+  readonly assignor: UniqueEntityIdVO;
+
+  constructor(props: ReceivableProps) {
+    super();
+    this.id = props.id ? new UniqueEntityIdVO(props.id) : new UniqueEntityIdVO();
+    this.value = props.value;
+    this.emissonDate = new Date(props.emissionDate);
+    this.assignor = new UniqueEntityIdVO(props.assignor);
+  }
+
+  static create(input: Omit<ReceivableProps, 'id'>): ReceivableEntity {
+    return new ReceivableEntity(input);
+  }
+
+  toJSON() {
+    return {
+      ...this,
+      id: this.id.value,
+      assignor: this.assignor.value
+    };
+  }
+};
