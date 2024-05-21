2f5e14df979f27e93dc9b8373f4c81904342eb96
Yazalde Filimone <yazaldefilimon@gmail.com>
2024-05-21 17:44:05 +0200

    feat: register repository in assignor

--- packages/server/src/modules/assignor.module.ts
+++ packages/server/src/modules/assignor.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
 import { ReceivableController } from 'src/controllers/receivable.controller';
+import { PrismaReceivableRepository } from 'src/repositories/prisma/prisma.receivable.repository';
+import { ReceivableRepository } from 'src/repositories/receivable.repository';
 
-import { AssignorService } from 'src/services/assignor.service';
+import { ReceivableService } from 'src/services/receivable.service';
 
 @Module({
+  imports: [PrismaReceivableRepository],
   controllers: [ReceivableController],
-  providers: [AssignorService],
+  providers: [
+    ReceivableService,
+    {
+      provide: ReceivableRepository,
+      useClass: PrismaReceivableRepository,
+    },
+  ],
 })
 export class AssignorModule {}
