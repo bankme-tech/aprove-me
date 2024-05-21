4f41e531b35660e124537dfcc13a6412e08acba0
Renato Vicente <rntvicente@gmail.com>
2024-05-20 22:30:01 -0300

    feat: definindo contrato entity

---
+++ src/domain/entity.ts
@@ -0,0 +1,23 @@
+import { UniqueEntityIdVO } from "./common/value-object";
+
+export abstract class Entity {
+  readonly id: UniqueEntityIdVO;
+
+  abstract toJSON(): any;
+
+  equals(obj: this) {
+    if (obj === null || obj === undefined) {
+      return false;
+    }
+
+    if (obj.id === undefined) {
+      return false;
+    }
+
+    if (obj.constructor.name !== this.constructor.name) {
+      return false;
+    }
+
+    return obj.id.equals(this.id);
+  }
+}
