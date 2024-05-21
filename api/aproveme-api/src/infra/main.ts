c4cd1f04862e2d3c373122f383295d44819d2ccd
Lamenkazu <333.erick@gmail.com>
2024-05-21 10:02:02 -0300

    ✏️ Env Port on Main

--- api/aproveme-api/src/infra/main.ts
+++ api/aproveme-api/src/infra/main.ts
@@ -1,9 +1,14 @@
-import { NestFactory } from '@nestjs/core'
+import { NestFactory } from "@nestjs/core";
 
-import { AppModule } from './app.module'
+import { AppModule } from "./app.module";
+import { EnvService } from "./env/env.service";
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
-  await app.listen(3000)
-}
-bootstrap()
+const bootstrap = async () => {
+  const app = await NestFactory.create(AppModule);
+
+  const envService = app.get(EnvService);
+  const port = envService.get("PORT");
+
+  await app.listen(port);
+};
+bootstrap();
