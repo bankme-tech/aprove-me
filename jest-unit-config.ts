deb9d19927a02d0829c4eeacc1df2151e5a47e8c
Renato Vicente <rntvicente@gmail.com>
2024-05-20 22:29:37 -0300

    chore: install jest

---
+++ jest-unit-config.ts
@@ -0,0 +1,3 @@
+import config from './jest.config';
+config['testMatch'] = ['/**/*.spec.ts'];
+export default config;
---
+++ jest.config.ts
@@ -0,0 +1,19 @@
+export default {
+  bail: 1,
+  clearMocks: true,
+  detectOpenHandles: true,
+  testEnvironment: 'node',
+  collectCoverageFrom: ['<rootDir>/src/**', '!<rootDir>/src/main.ts'],
+  coverageDirectory: 'coverage',
+  coveragePathIgnorePatterns: [
+    '/node_modules/'
+  ],
+  coverageReporters: ['json', 'text', 'lcov', 'clover'],
+  roots: ['<rootDir>/test', '<rootDir>/src'],
+  transform: { '.+\\.ts$': '@swc/jest' },
+  watchPathIgnorePatterns: ['globalConfig'],
+  setupFiles: [],
+  testEnvironmentOptions: {
+    timeZone: 'America/Sao_Paulo',
+  },
+};
