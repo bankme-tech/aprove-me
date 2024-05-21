e3a17407a15b8c8f33163b0573ad62ace97753c1
Nyllaah <nylla.g@hotmail.com>
2024-05-20 21:52:29 -0300

    feat: payable class

--- backend-app/src/payable/entities/payable.entity.ts
+++ backend-app/src/payable/entities/payable.entity.ts
@@ -1,1 +1,18 @@
-export class Payable {}
+export class Payable {
+  id: string;
+  value: number;
+  emissionDate: Date;
+  assignorId: string;
+
+  constructor(
+    id: string,
+    value: number,
+    emissionDate: Date,
+    assignorId: string,
+  ) {
+    this.id = id;
+    this.value = value;
+    this.emissionDate = emissionDate;
+    this.assignorId = assignorId;
+  }
+}
