f0374b0a99fcbd6c2f9cd511ad697db3b0ba38f1
hxsggsz <vt.hugo.2021@gmail.com>
2024-05-21 16:09:38 -0300

    feat: create find all assignors use case

---
+++ backend/src/app/use-cases/assignor/find-all.ts
@@ -0,0 +1,18 @@
+import { Assignor } from '@/app/entities/assignor';
+import { AssignorRepository } from '@/app/repositories/assignor.repository';
+import { Injectable } from '@nestjs/common';
+
+interface Output {
+  assignors: Assignor[];
+}
+
+@Injectable()
+export class FindAll {
+  constructor(private assignorRepository: AssignorRepository) { }
+
+  async execute(): Promise<Output> {
+    const assignors = await this.assignorRepository.findAll();
+
+    return { assignors };
+  }
+}
