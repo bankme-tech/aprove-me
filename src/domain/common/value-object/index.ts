b795ef20db0601699e3e51cab3476c84a31ba960
Renato Vicente <rntvicente@gmail.com>
2024-05-20 22:31:21 -0300

    feat: unique entity id

---
+++ src/domain/common/value-object/index.ts
@@ -0,0 +1,1 @@
+export * from './unique-entity-id.vo';
---
+++ src/domain/common/value-object/unique-entity-id.vo.ts
@@ -0,0 +1,21 @@
+import * as crypto from 'crypto';
+
+import { ValueObject } from './value-object';
+import { InvalidUuidError } from '../exception';
+
+export class UniqueEntityIdVO extends ValueObject<string> {
+  private readonly _uuidV4Regex = /^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i; 
+
+  constructor(id?: string) {
+    super(id || crypto.randomUUID());
+    this.validate();
+  }
+
+  private validate() {
+    const isValid = this._uuidV4Regex.test(this._value);
+
+    if (!isValid) {
+      throw new InvalidUuidError(this.value);
+    }
+  }
+}
