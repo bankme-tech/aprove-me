303ed8944d74a802d14ef8c4eda1bb909d2b29a3
Mário Santos <mariodev7@gmail.com>
2024-05-21 01:38:03 -0300

    feat(backend): create assignor mapper
    this mapper will be used to transform entity from domain to persisntece and vice versa

---
+++ backend/src/modules/assignor/mappers/assignor.mapper.ts
@@ -0,0 +1,28 @@
+import { AssignorPersistence } from '~/common/types/assignor.types';
+import { AssignorEntity } from '../entities/assignor.entity';
+
+export class AssignorMapper {
+  static toDomain(raw: AssignorPersistence): AssignorEntity {
+    const entity = AssignorEntity.create({
+      name: raw.name,
+      document: raw.document,
+      email: raw.email,
+      phone: raw.phone,
+    });
+
+    if (entity.isLeft())
+      throw new Error(`Can't transform "${raw.id}" assignor to domain layer.`);
+
+    return entity.value;
+  }
+
+  static toPersistence(raw: AssignorEntity): AssignorPersistence {
+    return {
+      id: raw.id,
+      name: raw.name,
+      document: raw.document,
+      email: raw.email,
+      phone: raw.phone,
+    };
+  }
+}
