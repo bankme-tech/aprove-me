03b2fd4f207df3b4838e24faa075228e1b2fd114
jjribeiro1 <jvsr99@hotmail.com>
2024-05-22 14:38:05 -0300

    [backend] enable cors

--- bankme-backend/src/main.ts
+++ bankme-backend/src/main.ts
@@ -6,6 +6,7 @@ import { PrismaClientExceptionFilter } from './filters/prisma.exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
