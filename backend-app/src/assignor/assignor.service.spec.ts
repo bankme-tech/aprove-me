4fa3989430c56fee5a95284b71004c89ce0c1730
Nyllaah <nylla.g@hotmail.com>
2024-05-22 20:52:08 -0300

    feat: assignor and payable unit tests

--- backend-app/src/assignor/assignor.service.spec.ts
+++ backend-app/src/assignor/assignor.service.spec.ts
@@ -1,18 +1,102 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AssignorService } from './assignor.service';
+import { PrismaService } from '../prisma.service';
+import { CreateAssignorDto } from './dto/create-assignor.dto';
+import { UpdateAssignorDto } from './dto/update-assignor.dto';
 
 describe('AssignorService', () => {
   let service: AssignorService;
+  let prismaService: PrismaService;
+  const mock = {
+    document: '111.222.333-44',
+    email: 'carlos.santos@example.com',
+    phone: '(31) 99876-5432',
+    name: 'Carlos Santos',
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AssignorService],
+      providers: [
+        AssignorService,
+        {
+          provide: PrismaService,
+          useValue: {
+            assignor: {
+              create: jest.fn(),
+              findMany: jest.fn(),
+              findUnique: jest.fn(),
+              update: jest.fn(),
+              delete: jest.fn(),
+            },
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<AssignorService>(AssignorService);
+    prismaService = module.get<PrismaService>(PrismaService);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('create', () => {
+    it('should call prisma.assignor.create with correct data', async () => {
+      const dto: CreateAssignorDto = mock;
+      await service.create(dto);
+      expect(prismaService.assignor.create).toHaveBeenCalledWith({
+        data: {
+          document: dto.document,
+          email: dto.email,
+          phone: dto.phone,
+          name: dto.name,
+        },
+      });
+    });
+  });
+
+  describe('findAll', () => {
+    it('should call prisma.assignor.findMany', async () => {
+      await service.findAll();
+      expect(prismaService.assignor.findMany).toHaveBeenCalled();
+    });
+  });
+
+  describe('findOne', () => {
+    it('should call prisma.assignor.findUnique with correct id', async () => {
+      const id = 'someId';
+      await service.findOne(id);
+      expect(prismaService.assignor.findUnique).toHaveBeenCalledWith({
+        where: { id },
+      });
+    });
+  });
+
+  describe('update', () => {
+    it('should call prisma.assignor.update with correct data', async () => {
+      const id = 'someId';
+      const dto: UpdateAssignorDto = mock;
+      await service.update(id, dto);
+      expect(prismaService.assignor.update).toHaveBeenCalledWith({
+        where: { id },
+        data: {
+          document: dto.document,
+          email: dto.email,
+          phone: dto.phone,
+          name: dto.name,
+        },
+      });
+    });
+  });
+
+  describe('remove', () => {
+    it('should call prisma.assignor.delete with correct id', async () => {
+      const id = 'someId';
+      await service.remove(id);
+      expect(prismaService.assignor.delete).toHaveBeenCalledWith({
+        where: { id },
+      });
+    });
   });
 });
--- backend-app/src/payable/payable.service.spec.ts
+++ backend-app/src/payable/payable.service.spec.ts
@@ -1,18 +1,97 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PayableService } from './payable.service';
+import { PrismaService } from '../prisma.service';
+import { CreatePayableDto } from './dto/create-payable.dto';
+import { UpdatePayableDto } from './dto/update-payable.dto';
 
 describe('PayableService', () => {
   let service: PayableService;
+  let prismaService: PrismaService;
+
+  const mock = {
+    value: 1000,
+    assignor: '09258e88-e727-4d4c-9373-36ea98b61d29',
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PayableService],
+      providers: [
+        PayableService,
+        {
+          provide: PrismaService,
+          useValue: {
+            payable: {
+              create: jest.fn(),
+              findMany: jest.fn(),
+              findUnique: jest.fn(),
+              update: jest.fn(),
+              delete: jest.fn(),
+            },
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<PayableService>(PayableService);
+    prismaService = module.get<PrismaService>(PrismaService);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('create', () => {
+    it('should call prisma.payable.create with correct data', async () => {
+      const dto: CreatePayableDto = mock;
+      await service.create(dto);
+      expect(prismaService.payable.create).toHaveBeenCalledWith({
+        data: {
+          value: dto.value,
+          assignorId: dto.assignor,
+        },
+      });
+    });
+  });
+
+  describe('findAll', () => {
+    it('should call prisma.Payable.findMany', async () => {
+      await service.findAll();
+      expect(prismaService.payable.findMany).toHaveBeenCalled();
+    });
+  });
+
+  describe('findOne', () => {
+    it('should call prisma.Payable.findUnique with correct id', async () => {
+      const id = 'someId';
+      await service.findOne(id);
+      expect(prismaService.payable.findUnique).toHaveBeenCalledWith({
+        where: { id },
+      });
+    });
+  });
+
+  describe('update', () => {
+    it('should call prisma.Payable.update with correct data', async () => {
+      const id = 'someId';
+      const dto: UpdatePayableDto = mock;
+      await service.update(id, dto);
+      expect(prismaService.payable.update).toHaveBeenCalledWith({
+        where: { id },
+        data: {
+          value: dto.value,
+          assignorId: dto.assignor,
+        },
+      });
+    });
+  });
+
+  describe('remove', () => {
+    it('should call prisma.Payable.delete with correct id', async () => {
+      const id = 'someId';
+      await service.remove(id);
+      expect(prismaService.payable.delete).toHaveBeenCalledWith({
+        where: { id },
+      });
+    });
   });
 });
