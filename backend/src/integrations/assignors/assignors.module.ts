61dac29ee1fa5ed95d25708de05950ed09ce531e
Yanzaum <yandavid80@gmail.com>
2024-05-22 18:39:15 -0300

    feat: add AuthGuard to AssignorsModule and PayablesModule

--- backend/src/integrations/assignors/assignors.module.ts
+++ backend/src/integrations/assignors/assignors.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common';
 import { AssignorsService } from './assignors.service';
 import { AssignorsController } from './assignors.controller';
 import { PrismaService } from 'src/prisma.service';
+import { AuthGuard } from '../auth/auth.guard';
 
 @Module({
   controllers: [AssignorsController],
-  providers: [AssignorsService, PrismaService],
+  providers: [
+    AssignorsService,
+    PrismaService,
+    {
+      provide: 'APP_GUARD',
+      useClass: AuthGuard,
+    },
+  ],
 })
 export class AssignorsModule {}
--- backend/src/integrations/payables/payables.module.ts
+++ backend/src/integrations/payables/payables.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common';
 import { PayablesService } from './payables.service';
 import { PayablesController } from './payables.controller';
 import { PrismaService } from 'src/prisma.service';
+import { AuthGuard } from '../auth/auth.guard';
 
 @Module({
   controllers: [PayablesController],
-  providers: [PayablesService, PrismaService],
+  providers: [
+    PayablesService,
+    PrismaService,
+    {
+      provide: 'APP_GUARD',
+      useClass: AuthGuard,
+    },
+  ],
 })
 export class PayablesModule {}
