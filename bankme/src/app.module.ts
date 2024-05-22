01cac4783320709e76ca07b776345624680b2f94
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-05-22 11:44:17 -0300

    feat: adicionando prisma ao appModule

--- bankme/src/app.module.ts
+++ bankme/src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PayableModule } from './integrations/payable/payable.module';
+import { AssignorModule } from './integrations/assignor/assignor.module';
+import { PrismaService } from './prisma/prisma.service';
 
 @Module({
-  imports: [],
+  imports: [PayableModule, AssignorModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
