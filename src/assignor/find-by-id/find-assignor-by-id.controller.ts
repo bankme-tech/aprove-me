688d6f05d69be8c46841a21ad337d0d8585fadcb
Matheus Lopes <inolopesm@gmail.com>
2024-05-22 14:48:35 -0300

    remove unused constructors

--- src/assignor/find-by-id/find-assignor-by-id.controller.ts
+++ src/assignor/find-by-id/find-assignor-by-id.controller.ts
@@ -1,14 +1,11 @@
 import { Controller, Get, Param, ParseUUIDPipe } from "@nestjs/common";
 import type { Assignor } from "@prisma/client";
 
-import { PrismaProvider } from "../../providers/prisma.provider";
 import { FindAssignorByIdPipe } from "./find-assignor-by-id.pipe";
 import { FindAssignorByIdOutputDTO } from "./find-assignor-by-id-output.dto";
 
 @Controller()
 export class FindAssignorByIdController {
-  constructor(private readonly prisma: PrismaProvider) {}
-
   @Get("/integrations/assignor/:id")
   async handle(
     @Param("id", ParseUUIDPipe, FindAssignorByIdPipe) assignor: Assignor,
--- src/payable/find-by-id/find-payable-by-id.controller.ts
+++ src/payable/find-by-id/find-payable-by-id.controller.ts
@@ -1,14 +1,11 @@
 import { Controller, Get, Param, ParseUUIDPipe } from "@nestjs/common";
 import type { Payable } from "@prisma/client";
 
-import { PrismaProvider } from "../../providers/prisma.provider";
 import { FindPayableByIdPipe } from "./find-payable-by-id.pipe";
 import { FindPayableByIdOutputDTO } from "./find-payable-by-id-output.dto";
 
 @Controller()
 export class FindPayableByIdController {
-  constructor(private readonly prisma: PrismaProvider) {}
-
   @Get("/integrations/payable/:id")
   async handle(
     @Param("id", ParseUUIDPipe, FindPayableByIdPipe) payable: Payable,
