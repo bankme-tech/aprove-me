738634b9d8efccf29ca494c1b778f4ec0c9b8160
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2024-05-21 22:51:52 -0300

    feat: add Assignor entity

---
+++ backend/src/assignor/entities/assignor.entity.ts
@@ -0,0 +1,23 @@
+import { UUID } from 'crypto';
+
+export class Assignor {
+  public id: UUID;
+  public document: string;
+  public email: string;
+  public phone: string;
+  public name: string;
+
+  constructor(
+    id: UUID,
+    document: string,
+    email: string,
+    phone: string,
+    name: string,
+  ) {
+    this.id = id;
+    this.document = document;
+    this.email = email;
+    this.phone = phone;
+    this.name = name;
+  }
+}
