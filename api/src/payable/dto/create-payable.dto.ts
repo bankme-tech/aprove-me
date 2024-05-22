e312f3cef8308bc3c883acf0393f8abd2ac2e98e
arthcc <arthurdaconceicaocunha@gmail.com>
2024-05-22 20:55:38 -0300

    :sparkles: feat: add create payable dto

---
+++ api/src/payable/dto/create-payable.dto.ts
@@ -0,0 +1,21 @@
+import { Type } from "class-transformer";
+import { IsDate, IsNotEmpty, IsNumber } from "class-validator";
+
+export class CreatePayableDto {
+    @IsNotEmpty({
+        message: 'O valor do recebível é obrigatório',
+      })
+      @IsNumber({}, {
+        message: 'O valor do recebível deve ser um número válido',
+      })
+      value: number;
+    
+      @IsNotEmpty({
+        message: 'A data de emissão do recebível é obrigatória',
+      })
+      @IsDate({
+        message: 'A data de emissão do recebível deve ser uma data válida',
+      })
+      @Type(() => Date)
+      emissionDate: Date;
+    }    
