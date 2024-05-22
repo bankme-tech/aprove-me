54c0060f908ac059ecf20f9b444e29858244ca5e
Felipe Rodrigues <neath.felipe@gmail.com>
2024-05-22 11:53:48 -0300

    feat: modified payable entity

--- api/src/payable/entities/payable.entity.ts
+++ api/src/payable/entities/payable.entity.ts
@@ -1,1 +1,13 @@
-export class Payable {}
+export class Payable {
+  id: string;
+  value: number;
+  emissionDate: Date;
+  assignor: string;
+
+  constructor(payable: Payable) {
+    this.id = payable.id;
+    this.value = payable.value;
+    this.emissionDate = payable.emissionDate;
+    this.assignor = payable.assignor;
+  }
+}
