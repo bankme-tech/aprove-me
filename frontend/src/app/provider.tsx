826bac9f4f3c5a3be87f5853acd7542785d41108
Yanzaum <yandavid80@gmail.com>
2024-05-22 20:53:23 -0300

    chore: install React Query and configure provider

--- frontend/src/app/provider.tsx
+++ frontend/src/app/provider.tsx
@@ -1,4 +1,8 @@
 import { NextUIProvider } from "@nextui-org/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+export const queryClient = new QueryClient()
 
 export default function Provider({
   children,
@@ -6,8 +10,11 @@ export default function Provider({
   children: React.ReactNode;
 }) {
   return (
-    <NextUIProvider>
-      {children}
-    </NextUIProvider>
+    <QueryClientProvider client={queryClient}>
+      <NextUIProvider>
+        {children}
+        <ReactQueryDevtools initialIsOpen={false} />
+      </NextUIProvider>
+    </QueryClientProvider>
   );
 }
