4f3b10cf9abe2155accb2ca69eb04780a0381991
jjribeiro1 <jvsr99@hotmail.com>
2024-05-22 14:37:39 -0300

    [frontend] axios interceptors

--- bankme-frontend/src/lib/axios.ts
+++ bankme-frontend/src/lib/axios.ts
@@ -3,3 +3,21 @@ import axios from "axios";
 export const api = axios.create({
   baseURL: "http://localhost:3001/integrations",
 });
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  config.headers["Authorization"] = `Bearer ${token}`;
+
+  return config;
+});
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      localStorage.clear();
+      window.location.href = "/auth";
+    }
+    return Promise.reject(error);
+  }
+);
