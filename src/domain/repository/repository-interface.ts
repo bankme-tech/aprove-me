9d2c872953451ea79c06bf03864b753221fb78bd
Renato Vicente <rntvicente@gmail.com>
2024-05-21 23:34:31 -0300

    refactor: renomeando

--- src/domain/repository/repository-interface.ts
+++ src/domain/repository/repository-interface.ts
@@ -2,7 +2,5 @@ import { Entity } from '../entity';
 
 export interface IRepository<E extends Entity> {
   add(entity: E): Promise<void>;
-  findById(id: any): Promise<E | null>;
-  findAll(): Promise<E[]>;
-  delete(entity: E): Promise<void>;
+  findAll(): Promise<E[] | null>;
 }
