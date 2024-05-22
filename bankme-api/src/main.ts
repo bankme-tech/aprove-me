2b789ddd04fa9d8bb8123941aae6a6d5a9a0f800
Breno Godoy <godoyy25z@gmail.com>
2024-05-22 02:27:07 -0300

    :sparkles: feat: Adiciona API na tela de Adicionar Recebível

--- bankme-api/src/main.ts
+++ bankme-api/src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   app.enableCors({
     origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: 'Content-Type, Accept', 
+    allowedHeaders: 'Content-Type, Accept, Authorization', 
   });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
