6e15f4e31c2de1066130dcf3811c19d318464d29
Yanzaum <yandavid80@gmail.com>
2024-05-22 16:52:14 -0300

    refactor: update UpdatePayableDto to extend PartialType(CreatePayableDto)

--- backend/src/integrations/payables/dto/update-payable.dto.ts
+++ backend/src/integrations/payables/dto/update-payable.dto.ts
@@ -1,1 +1,4 @@
-export class UpdatePayableDto {}
+import { PartialType } from '@nestjs/swagger';
+import { CreatePayableDto } from './create-payable.dto';
+
+export class UpdatePayableDto extends PartialType(CreatePayableDto) {}
