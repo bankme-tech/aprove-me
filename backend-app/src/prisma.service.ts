582a19e830eaee058ad82308188e34a3e4cfaa0e
Nyllaah <nylla.g@hotmail.com>
2024-05-22 17:40:43 -0300

    feat: prisma service created

---
+++ backend-app/src/prisma.service.ts
@@ -0,0 +1,10 @@
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  async onModuleInit() {
+    // Note: this is optional
+    await this.$connect();
+  }
+}
