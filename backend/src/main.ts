3b992168a25cd59646a191eb8a4bc3c8d1bfd661
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2024-05-21 22:56:29 -0300

    feat: add validation global pipe

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('integrations');
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
