8f1a6773e59da88cb363bd56445ead0e022e82e9
Renato Vicente <rntvicente@gmail.com>
2024-05-21 23:34:49 -0300

    feat; contrato para tabela assignor

---
+++ src/domain/repository/assignor-repository.interface.ts
@@ -0,0 +1,6 @@
+import { IRepository } from "./repository-interface";
+import { AssignorEntity } from "../entity/assignor.entity";
+
+export interface IAssignorRepository extends IRepository<AssignorEntity> {
+  findByDocument(document: string): Promise<AssignorEntity | null>;
+}
