f4031f69d93fd9a0b7de7ca6b9ce37fe796f4caf
Yazalde Filimone <yazaldefilimon@gmail.com>
2024-05-22 23:20:20 +0200

    feat: disconnect database

--- packages/server/src/database/prisma.config.ts
+++ packages/server/src/database/prisma.config.ts
@@ -1,9 +1,12 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
   async onModuleInit() {
     await this.$connect();
+  }
+  async onModuleDestroy() {
+    await this.$disconnect();
   }
 }
