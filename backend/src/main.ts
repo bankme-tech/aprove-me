eb4bc69daefc2a4e72bbb929847f3e06b4eed3c4
Yanzaum <yandavid80@gmail.com>
2024-05-22 15:49:37 -0300

    feat: add Swagger documentation to the Payables API

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Aprove Me API')
+    .setDescription('API to manage payables and assignors')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
