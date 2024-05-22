3c84a729abdbd8c26191fc54c8742e911cfe6f9a
Mário Santos <mariodev7@gmail.com>
2024-05-22 19:35:28 -0300

    feat(fronend): initialize next project

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "@/styles/globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "AproveMe",
+  description: "AproveMe é uma plataforma de gerenciamento de pagamentos.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-br">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
