6ee7884cba31fbdf83b71bc5837b15d37cb40dd8
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2024-05-22 18:34:41 -0300

    feat: add PrismaAssignorMapper

---
+++ backend/src/assignor/mappers/prisma-assignor.mapper.ts
@@ -0,0 +1,26 @@
+import { UUID } from 'crypto';
+import { AssignorMapper } from './assignor.mapper.interface';
+import { Assignor } from '@prisma/client';
+import { AssignorEntity } from '../entities/assignor.entity';
+
+export class PrismaAssignorMapper extends AssignorMapper<Assignor> {
+  toDomainEntity(data: Assignor): AssignorEntity {
+    return {
+      id: data.id as UUID,
+      document: data.document,
+      email: data.email,
+      phone: data.phone,
+      name: data.name,
+    };
+  }
+
+  toPersistenceModel(data: AssignorEntity): Assignor {
+    return {
+      id: data.id as string,
+      document: data.document,
+      email: data.email,
+      phone: data.phone,
+      name: data.name,
+    };
+  }
+}
