b8fc3bc38693d27a571aacd85cfa7fe7be418bd4
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-05-22 11:37:20 -0300

    refactor(assignor): refatorando retorno da camada de serviço de assignor

--- bankme/src/integrations/assignor/assignor.service.ts
+++ bankme/src/integrations/assignor/assignor.service.ts
@@ -1,48 +1,50 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import Assignor from '../entity/Assignor';
 import AssignorRepository from './assignor.repository';
-import { IAssignor } from '../types/IAssignor';
 import AssignorDto from '../dto/AssignorDto';
 
 @Injectable()
 export class AssignorService {
   constructor(private assignorRepository: AssignorRepository) {}
 
-  async createAssignorRegister(assignor: Assignor): Promise<IAssignor> {
+  async createAssignorRegister(assignor: Assignor): Promise<AssignorDto> {
     const createdAssignor =
       await this.assignorRepository.createAssignorRegister(assignor);
 
     return AssignorDto.fromEntity(createdAssignor);
   }
 
-  async findAssignorById(id: string) {
+  async findAssignorById(id: string): Promise<AssignorDto> {
     const assignor = await this.assignorRepository.findAssignorById(id);
 
     if (!assignor) {
-      throw new HttpException('Assignor not found', HttpStatus.NOT_FOUND);
+      throw new HttpException('Assignor not found.', HttpStatus.NOT_FOUND);
     }
 
     return AssignorDto.fromEntity(assignor);
   }
 
-  async updateAssignorById(id: string, assignor: Assignor) {
+  async updateAssignorById(
+    id: string,
+    assignor: Assignor,
+  ): Promise<AssignorDto> {
     const updatedAssignor = await this.assignorRepository.updateAssignorById(
       id,
       assignor,
     );
 
     if (!updatedAssignor) {
-      throw new HttpException('Assignor not found', HttpStatus.NOT_FOUND);
+      throw new HttpException('Assignor not found.', HttpStatus.NOT_FOUND);
     }
 
     return AssignorDto.fromEntity(updatedAssignor);
   }
 
-  async deleteAssignorById(id: string) {
+  async deleteAssignorById(id: string): Promise<void> {
     const assignor = await this.assignorRepository.deleteAssignorById(id);
 
     if (!assignor) {
-      throw new HttpException('Assignor not found', HttpStatus.NOT_FOUND);
+      throw new HttpException('Assignor not found.', HttpStatus.NOT_FOUND);
     }
 
     return;
