f9630fc439ed4afbc9b6eb3f2b5557e9e7aa604a
arthcc <arthurdaconceicaocunha@gmail.com>
2024-05-22 20:52:34 -0300

    :sparkles: feat: add assignor service

---
+++ api/src/assignor/assignor.service.ts
@@ -0,0 +1,70 @@
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
+import { CreateAssignorDto } from './dto/create-assignor.dto';
+import { UpdateAssignorDto } from './dto/update-assignor.dto';
+import { prisma } from '../prisma/prisma.client';
+import { find } from 'rxjs';
+
+@Injectable()
+export class AssignorService {
+
+  async create(createAssignorDto: CreateAssignorDto) {
+    try{
+     // if (typeof createAssignorDto.document !== 'string') {
+       // throw new Error('Favor informar um documento válido');
+      //}
+      //if (typeof createAssignorDto.email !== 'string') {
+       // throw new Error('Favor informar um email válido');
+     // }
+     // if (typeof createAssignorDto.phone !== 'string') {
+       /// throw new Error('Favor informar um telefone válido');
+      //}
+     // if (typeof createAssignorDto.name !== 'string') {
+       // throw new Error('Formato de nome inválido');
+      //}
+
+      return await prisma.assignor.create({
+        data: createAssignorDto,
+      });
+    } catch (error) {
+      if( error instanceof BadRequestException){
+        throw BadRequestException;
+      }
+
+    }
+
+}
+
+async findOne(id: string) {
+  const assignor = await prisma.assignor.findUnique({
+    where: { id },
+  });
+  if (!assignor) {
+    throw new NotFoundException('Cedente não encontrado.');
+  }
+  return assignor;
+}
+async deleteAssignor(id: string) {
+  const assignor = await prisma.assignor.delete({
+    where: { id },
+  });
+  if (!assignor) {
+    throw new NotFoundException('Cedente não encontrado.');
+  }
+  return {message: 'Cedente deletado com sucesso.'};
+}
+
+async updateAssignor(id: string, updateAssignorDto: UpdateAssignorDto) {
+  try {
+    const assignor = await prisma.assignor.update({
+      where: { id },
+      data: updateAssignorDto,
+    });
+    return assignor;
+  } catch (error) {
+    if (error instanceof BadRequestException || error instanceof NotFoundException) {
+      throw error;
+    }
+    throw new BadRequestException('Ocorreu um erro ao atualizar o cedente.');
+  }
+}
+}
