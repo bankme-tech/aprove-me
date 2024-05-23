a1f9ce296427f25d9dcaf1c48b537987e65956ba
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2024-05-23 02:45:27 -0300

    feat: Add CreatePayableUseCase implementation and related dependencies

--- backend/src/payable/payable.module.ts
+++ backend/src/payable/payable.module.ts
@@ -1,8 +1,29 @@
 import { Module } from '@nestjs/common';
 import { PayableController } from './payable.controller';
+import { CreatePayableUseCase } from './usecases/create-payable.usecase';
+import { PrismaPayableRepository } from './repositories/prisma-payable.repository';
+import { IPayableRepository } from './repositories/payable.repository.interface';
+import { PayableMapper } from './mappers/payable.mapper.interface';
+import { PrismaPayableMapper } from './mappers/prisma-payable.mapper';
+import { ICreatePayableUseCase } from './usecases/create-payable.usecase.interface';
+import { AssignorModule } from 'src/assignor/assignor.module';
 
 @Module({
   controllers: [PayableController],
-  providers: [],
+  providers: [
+    {
+      provide: ICreatePayableUseCase,
+      useClass: CreatePayableUseCase,
+    },
+    {
+      provide: IPayableRepository,
+      useClass: PrismaPayableRepository,
+    },
+    {
+      provide: PayableMapper,
+      useClass: PrismaPayableMapper,
+    },
+  ],
+  imports: [AssignorModule],
 })
 export class PayableModule {}
