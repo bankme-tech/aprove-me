93a8c52568aae4d52724a85e514a096ba1017b94
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2024-05-23 00:49:52 -0300

    feat: add AssignorModule to AppModule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,12 +3,14 @@ import { AppController } from './app.controller';
 import { ConfigModule } from '@nestjs/config';
 import { PayableModule } from './payable/payable.module';
 import { PrismaModule } from './persistence/prisma.module';
+import { AssignorModule } from './assignor/assignor.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     PrismaModule,
     PayableModule,
+    AssignorModule,
   ],
   controllers: [AppController],
   providers: [],
---
+++ backend/src/assignor/assignor.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { AssignorController } from './assignor.controller';
+import { CreateAssignorUseCase } from './usecases/create-assignor.usecase';
+import { PrismaAssignorRepository } from './repositories/prisma-assignor.repository';
+import { IAssignorRepository } from './repositories/assignor.repository.interface';
+import { ICreateAssignorUseCase } from './usecases/create-assignor.usecase.interface';
+import { AssignorMapper } from './mappers/assignor.mapper.interface';
+import { PrismaAssignorMapper } from './mappers/prisma-assignor.mapper';
+
+@Module({
+  controllers: [AssignorController],
+  providers: [
+    {
+      provide: AssignorMapper,
+      useClass: PrismaAssignorMapper,
+    },
+    {
+      provide: ICreateAssignorUseCase,
+      useClass: CreateAssignorUseCase,
+    },
+    {
+      provide: IAssignorRepository,
+      useClass: PrismaAssignorRepository,
+    },
+  ],
+})
+export class AssignorModule {}
