c19f998d01c39e7563555ebc8a6c33e3be8635a2
Yanzaum <yandavid80@gmail.com>
2024-05-22 21:55:17 -0300

    feat: add Header component and BankmeLogo to layout

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Provider from "./provider";
+import Header from "./components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Header />
         <Provider>
           {children}
         </Provider>
