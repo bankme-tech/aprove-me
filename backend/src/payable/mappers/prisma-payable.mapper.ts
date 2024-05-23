5dc4f85f02ce68cd9c94788f252628218c989d04
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2024-05-23 02:38:18 -0300

    feat: add PrismaPayableMapper

---
+++ backend/src/payable/mappers/prisma-payable.mapper.ts
@@ -0,0 +1,29 @@
+import { Payable } from '@prisma/client';
+import { PayableMapper } from './payable.mapper.interface';
+import { PayableEntity } from '../entities/payable.entity';
+import { UUID } from 'crypto';
+
+export class PrismaPayableMapper extends PayableMapper<Payable> {
+  toDomainEntity(data: Payable): PayableEntity {
+    const year = data.emissionDate.getFullYear();
+    const month = data.emissionDate.getMonth() + 1;
+    const day = data.emissionDate.getDate();
+    const emissionDate = new Date(year, month - 1, day);
+
+    return {
+      id: data.id as UUID,
+      value: data.value,
+      assignorId: data.assignorId as UUID,
+      emissionDate: emissionDate,
+    };
+  }
+
+  toPersistenceModel(data: PayableEntity): Payable {
+    return {
+      id: data.id as string,
+      value: data.value,
+      assignorId: data.assignorId as string,
+      emissionDate: new Date(data.emissionDate),
+    };
+  }
+}
