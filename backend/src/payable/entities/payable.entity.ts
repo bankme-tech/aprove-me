34f7a3d949ec43aa68f147c91230dcd3bb1e06b1
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2024-05-23 02:31:12 -0300

    feat: make Payable entity id property optional and rename assignor property

--- backend/src/payable/entities/payable.entity.ts
+++ backend/src/payable/entities/payable.entity.ts
@@ -1,15 +1,15 @@
 import { UUID } from 'crypto';
 
-export class Payable {
-  public id: UUID;
+export class PayableEntity {
+  public id?: UUID;
   public value: number;
   public emissionDate: Date;
-  public assignor: UUID;
+  public assignorId: UUID;
 
   constructor(id: UUID, value: number, emissionDate: Date, assignor: UUID) {
     this.id = id;
     this.value = value;
     this.emissionDate = emissionDate;
-    this.assignor = assignor;
+    this.assignorId = assignor;
   }
 }
