8c6fa17373db8f1c3deff18d72a294b96a7f209a
Yanzaum <yandavid80@gmail.com>
2024-05-22 21:53:28 -0300

    feat: add Header component for navigation bar

---
+++ frontend/src/app/components/Header.tsx
@@ -0,0 +1,20 @@
+import { Button, Navbar, NavbarBrand, NavbarContent, NavbarItem } from "@nextui-org/react";
+import BankmeLogo from "./BankmeLogo";
+import Link from "next/link";
+
+export default function Header() {
+    return (
+        <Navbar shouldHideOnScroll>
+          <NavbarBrand>
+            <BankmeLogo />
+          </NavbarBrand>
+          <NavbarContent justify="end">
+            <NavbarItem>
+              <Button as={Link} color="primary" href="#" variant="flat">
+                Entrar
+              </Button>
+            </NavbarItem>
+          </NavbarContent>
+        </Navbar>
+    )
+}
