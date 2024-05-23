3069b43786839ba4cf26eddaad19f3ef49f97e7c
Renato Vicente <rntvicente@gmail.com>
2024-05-23 13:30:18 -0300

    feat: caso de uso para buscar recebivel por id

---
+++ src/application/get-payable.usecase.ts
@@ -0,0 +1,15 @@
+import { ReceivableEntity } from '../domain/entity';
+import { IReceivableRepository } from '../domain/repository';
+import { ReceivableNotFoundException } from '../domain/common/exception';
+
+export class GetPayableUsecase {
+  constructor(private readonly receivableRepo: IReceivableRepository) {}
+
+  async execute(id: string): Promise<ReceivableEntity> {
+    const receivable = await this.receivableRepo.findById(id);
+
+    if(!receivable) throw new ReceivableNotFoundException(id);
+
+    return receivable;
+  }
+}
