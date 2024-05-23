b70dcad8c53fcd3bf5848e11c80860365706e0c0
Mário Santos <mariodev7@gmail.com>
2024-05-23 15:47:08 -0300

    fix(frontend): validate is on client side

--- frontend/src/lib/utils/token.ts
+++ frontend/src/lib/utils/token.ts
@@ -1,11 +1,17 @@
 export function getToken(){
+    if(typeof window === 'undefined') return null
+
     return localStorage.getItem('token')
 }
 
 export function saveToken(token: string){
+    if(typeof window === 'undefined') return null
+
     return localStorage.setItem('token', token)
 }
 
 export function removeToken(){
+    if(typeof window === 'undefined') return
+
     localStorage.removeItem('token')
 }
