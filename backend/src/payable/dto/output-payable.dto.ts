4915c6ce13be816d2d7d8f182cfc5684816e3a3e
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2024-05-23 02:44:16 -0300

    refactor: remove unused DTO classes

--- backend/src/payable/dto/output-payable.dto.ts
+++
@@ -1,7 +0,0 @@
-import { InputAssignorDTO } from 'src/assignor/dto/input-assignor.dto';
-import { InputPayableDTO } from './input-payable.dto';
-
-export type OutputPayableDTO = {
-  payable: InputPayableDTO;
-  assignor: InputAssignorDTO;
-};
