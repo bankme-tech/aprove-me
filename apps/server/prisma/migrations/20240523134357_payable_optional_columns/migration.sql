24714a2f1f526d426d8f5bb8bd3bbe8d50a8ea09
Gabriel Duarte <jds.gabrielduarte@gmail.com>
2024-05-23 12:06:13 -0300

    chore: add optional columns in payable table

---
+++ apps/server/prisma/migrations/20240523134357_payable_optional_columns/migration.sql
@@ -0,0 +1,19 @@
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_payables" (
+    "id" TEXT NOT NULL,
+    "value" REAL NOT NULL,
+    "emission_date" DATETIME NOT NULL,
+    "assignor_id" TEXT NOT NULL,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "payed_at" DATETIME,
+    "canceled_at" DATETIME,
+    CONSTRAINT "payables_assignor_id_fkey" FOREIGN KEY ("assignor_id") REFERENCES "assignors" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+INSERT INTO "new_payables" ("assignor_id", "canceled_at", "created_at", "emission_date", "id", "payed_at", "value") SELECT "assignor_id", "canceled_at", "created_at", "emission_date", "id", "payed_at", "value" FROM "payables";
+DROP TABLE "payables";
+ALTER TABLE "new_payables" RENAME TO "payables";
+CREATE UNIQUE INDEX "payables_id_key" ON "payables"("id");
+CREATE INDEX "payables_assignor_id_idx" ON "payables"("assignor_id");
+PRAGMA foreign_key_check("payables");
+PRAGMA foreign_keys=ON;
