67288dc1fd86bb89c9ed00e21df43a174a7e6b05
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-05-23 16:51:14 -0300

    feat(payable): adicionando o guard para validar o jwt as rotas de payable

--- bankme/src/integrations/payable/payable.controller.ts
+++ bankme/src/integrations/payable/payable.controller.ts
@@ -6,16 +6,19 @@ import {
   Param,
   Post,
   Put,
+  UseGuards,
 } from '@nestjs/common';
 import { PayableService } from './payable.service';
 import Payable from '../entity/Payable';
 import PayableCreationDto from '../dto/PayableCreationDto';
+import { AuthGuard } from '../../auth/auth.guard';
 
 @Controller('integrations/payable')
 export class PayableController {
   constructor(private payableService: PayableService) {}
 
   @Post('/')
+  @UseGuards(AuthGuard)
   async createPayableRegister(@Body() payableBody: PayableCreationDto) {
     const payable: Payable = payableBody.toEntity();
     const responsePayable =
@@ -25,6 +28,7 @@ export class PayableController {
   }
 
   @Get('/:id')
+  @UseGuards(AuthGuard)
   async findPayableById(@Param('id') id: string) {
     const responsePayable = await this.payableService.findPayableById(id);
 
@@ -32,6 +36,7 @@ export class PayableController {
   }
 
   @Put('/:id')
+  @UseGuards(AuthGuard)
   async updatePayableById(
     @Param('id') id: string,
     @Body() payableBody: PayableCreationDto,
@@ -46,6 +51,7 @@ export class PayableController {
   }
 
   @Delete('/:id')
+  @UseGuards(AuthGuard)
   async deletePayableById(@Param('id') id: string) {
     await this.payableService.deletePayableById(id);
 
