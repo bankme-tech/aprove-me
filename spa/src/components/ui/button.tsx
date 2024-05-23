54f182616187fea4ea2c007b47627fdf37edeb70
Emerson Vieira <emersondont@gmail.com>
2024-05-23 19:46:02 -0300

    chore: Update button and deleteButton components

--- spa/src/components/ui/button.tsx
+++ spa/src/components/ui/button.tsx
@@ -9,7 +9,7 @@ interface Props {
 export default function Button(props: Props) {
   return (
     <button
-      className={'w-full py-2 px-3 rounded-2xl font-bold text-text duration-200 ease-out flex justify-center items-center gap-1 text-HeaderBgColor ' +
+      className={'w-full py-2 px-3 rounded-2xl font-bold duration-200 ease-out flex justify-center items-center gap-1 text-HeaderBgColor ' +
         (props.disabled ? 'bg-opacity-40' : 'hover:bg-opacity-80') +
         (props.delete ? ' bg-red-600' : ' bg-themeColor')
       }
