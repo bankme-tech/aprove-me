e153c54a7697748530bcae35678875a88c40bbba
Gabriel Duarte <jds.gabrielduarte@gmail.com>
2024-05-23 12:06:38 -0300

    feat: add payable module in app

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common'
 import { AssignorsModule } from './assignors/assignors.module'
 import { PrismaModule } from './database/prisma.module'
 import { HealthzModule } from './healthz/healthz.module'
+import { PayableModule } from './payable/payable.module'
 
 @Module({
-  imports: [HealthzModule, PrismaModule, AssignorsModule],
+  imports: [HealthzModule, PrismaModule, AssignorsModule, PayableModule],
 })
 export class AppModule {}
