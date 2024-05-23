61a7d5d33f4762c6359767679ffffcb247699c36
arthcc <arthurdaconceicaocunha@gmail.com>
2024-05-23 20:31:34 -0300

    fix: fix router after login

--- client/src/app/sign-in/page.tsx
+++ client/src/app/sign-in/page.tsx
@@ -1,71 +1,72 @@
-'use client';
+"use client";
 
-import axios from 'axios';
-import { useState } from 'react';
-import Link from 'next/link';
-import { Button } from '@/components/ui/button';
-import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
-import { toast } from 'sonner';
+import { useState } from "react";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { toast } from "sonner";
 import { useRouter } from 'next/navigation';
-import { cn } from '@/lib/utils';
-import { ChevronLeft } from 'lucide-react';
-import { jwtDecode } from 'jwt-decode';
-import { api, setAuthToken } from '@/services/api';
+import { cn } from "@/lib/utils";
+import { ChevronLeft } from "lucide-react";
+import { jwtDecode } from "jwt-decode";
+import { api, setAuthToken } from "@/services/api";
 
 export default function SignIn() {
+  const router = useRouter();
   const [formData, setFormData] = useState({
-    email: '',
-    password: '',
+    email: "",
+    password: "",
   });
   const [isLoading, setIsLoading] = useState(false);
 
-  const handleChange = (e: { target: { name: any; value: any; }; }) => {
+  const handleChange = (e: { target: { name: any; value: any } }) => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
   };
 
-  const router = useRouter();
 
-  const handleSubmit = async (e: { preventDefault: () => void; }) => {
+
+  const handleSubmit = async (e: { preventDefault: () => void }) => {
     e.preventDefault();
     const { email, password } = formData;
     setIsLoading(true);
 
     try {
-        const response = await api.post('/integrations/auth', { email, password });
-        const {
-            accessToken : token
-        }  = response.data;
-        localStorage.setItem('authToken', token);
-        const decodedToken = jwtDecode(token);
-       
-    
-      toast.success('Login realizado com sucesso!', {
-        description: 'Você foi autenticado com sucesso!',
+      const response = await api.post("/integrations/auth", {
+        email,
+        password,
+      });
+      const { accessToken: token } = response.data;
+      localStorage.setItem("authToken", token);
+      const decodedToken = jwtDecode(token);
+      router.push("/payable-assignor-create");
+      toast.success("Login realizado com sucesso!", {
+        description: "Você foi autenticado com sucesso!",
         action: {
-          label: 'Fechar',
-          onClick: () => ('Fechar'),
+          label: "Fechar",
+          onClick: () => "Fechar",
         },
       });
-
     } catch (error) {
-        if ((error as any).response && (error as any).response.data && (error as any).response.data.message) {
+      if (
+        (error as any).response &&
+        (error as any).response.data &&
+        (error as any).response.data.message
+      ) {
         toast.error("Erro ao realizar login!", {
-        description: (error as any).response.data.message,
+          description: (error as any).response.data.message,
           action: {
             label: "Fechar",
-            onClick: () => ("Fechar"),
-          }
-       
+            onClick: () => "Fechar",
+          },
         });
-     
       } else {
-        toast.error('Erro ao realizar login!', {
-          description: 'Erro desconhecido',
+        toast.error("Erro ao realizar login!", {
+          description: "Erro desconhecido",
           action: {
-            label: 'Fechar',
-            onClick: () => ('Fechar'),
+            label: "Fechar",
+            onClick: () => "Fechar",
           },
         });
       }
@@ -75,7 +76,7 @@ export default function SignIn() {
   };
 
   const handleClick = () => {
-    window.location.href = 'http://localhost:3000';
+    window.location.href = "http://localhost:3000";
   };
 
   return (
@@ -116,7 +117,11 @@ export default function SignIn() {
               />
             </div>
             <Button type="submit" className="w-full">
-              {isLoading ? <LoadingSpinner className="w-6 h-6 mx-auto" /> : 'Login'}
+              {isLoading ? (
+                <LoadingSpinner className="w-6 h-6 mx-auto" />
+              ) : (
+                "Login"
+              )}
             </Button>
           </div>
         </form>
@@ -131,18 +136,18 @@ export default function SignIn() {
 }
 
 const LoadingSpinner = ({ className }: { className: string }) => (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      className={cn("animate-spin", className)}
-    >
-      <path d="M21 12a9 9 0 1 1-6.219-8.56" />
-    </svg>
-  );
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    className={cn("animate-spin", className)}
+  >
+    <path d="M21 12a9 9 0 1 1-6.219-8.56" />
+  </svg>
+);
