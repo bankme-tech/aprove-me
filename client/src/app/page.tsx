99f0d7706812443cec9811d95d652b964e3df3c2
arthcc <arthurdaconceicaocunha@gmail.com>
2024-05-23 20:16:17 -0300

    feat: adding home page

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,5 +1,34 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import Link from "next/link"; 
 
-export default function Home() {
-  //return ();
+export default function HomePage() {
+  return (
+    <>
+      <div className="relative">
+        <div className={cn("flex justify-end py-4 px-8")}>
+          <nav className={cn("space-x-4")}>
+
+            <Link href="/sign-in">
+              <Button variant="secondary">Login</Button>
+            </Link>
+            <Link href="/sign-up">
+              <Button>Cadastre-se</Button>
+            </Link>
+          </nav>
+        </div>
+        
+        <div className="text-center mx-auto px-10 flex flex-col justify-center items-center h-screen">
+          <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-5">
+          Cdastro de Recebíveis e Cedentes
+            <br></br><strong>AproveMe</strong>
+          </h1>
+          <h2 className="text-xl text-muted-foreground">
+          </h2>
+        </div>
+     
+      </div>
+
+    </>
+  );
 }
