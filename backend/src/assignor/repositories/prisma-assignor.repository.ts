c788e78c8009b02d1ead7394f3145495daa68174
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2024-05-23 00:48:49 -0300

    feat: add PrismaAssingnorRepository

---
+++ backend/src/assignor/repositories/prisma-assignor.repository.ts
@@ -0,0 +1,31 @@
+import { PrismaService } from 'src/persistence/prisma.service';
+import { CreateAssignorInputDTO } from '../dto/create-assignor.input.dto';
+import { AssignorEntity } from '../entities/assignor.entity';
+import { IAssignorRepository } from './assignor.repository.interface';
+import { Injectable } from '@nestjs/common';
+import { AssignorMapper } from '../mappers/assignor.mapper.interface';
+import { Assignor } from '@prisma/client';
+
+@Injectable()
+export class PrismaAssignorRepository implements IAssignorRepository {
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly mapper: AssignorMapper<Assignor>,
+  ) {}
+
+  async save(
+    createAssignorDTO: CreateAssignorInputDTO,
+  ): Promise<AssignorEntity> {
+    const assignor = this.mapper.toPersistenceModel(createAssignorDTO);
+    const response = await this.prisma.assignor.create({
+      data: {
+        document: assignor.document,
+        email: assignor.email,
+        phone: assignor.phone,
+        name: assignor.name,
+      },
+    });
+
+    return this.mapper.toDomainEntity(response);
+  }
+}
