a30b05cc252155bbe043e2e541fc745be48f40cd
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2024-05-23 02:39:06 -0300

    feat: add PrismaPayableRepository

---
+++ backend/src/payable/repositories/prisma-payable.repository.ts
@@ -0,0 +1,26 @@
+import { PrismaService } from 'src/persistence/prisma.service';
+import { CreatePayableInputDTO } from '../dto/create-payable.input.dto';
+import { PayableEntity } from '../entities/payable.entity';
+import { IPayableRepository } from './payable.repository.interface';
+import { Injectable } from '@nestjs/common';
+import { PayableMapper } from '../mappers/payable.mapper.interface';
+import { Payable } from '@prisma/client';
+
+@Injectable()
+export class PrismaPayableRepository implements IPayableRepository {
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly mapper: PayableMapper<Payable>,
+  ) {}
+
+  async save(createPayableDTO: CreatePayableInputDTO): Promise<PayableEntity> {
+    const payable = this.mapper.toPersistenceModel(createPayableDTO);
+    const response = await this.prisma.payable.create({
+      data: {
+        ...payable,
+      },
+    });
+
+    return this.mapper.toDomainEntity(response);
+  }
+}
