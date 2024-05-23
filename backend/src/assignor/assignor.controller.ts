d265c698bc0fe5591caa7338a9cd060c2138507d
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2024-05-23 08:50:50 -0300

    feat: add findAll method to AssignorController

--- backend/src/assignor/assignor.controller.ts
+++ backend/src/assignor/assignor.controller.ts
@@ -1,16 +1,26 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CreateAssignorInputDTO } from './dto/create-assignor.input.dto';
 import { ICreateAssignorUseCase } from './usecases/create-assignor.usecase.interface';
 import { CreateAssignorOutputDTO } from './dto/create-assignor.ouput.dto';
+import { IFindAllAssignorsUseCase } from './usecases/find-all-assignors.usecase.interface';
+import { FindAssignorOutputDTO } from './dto/find-assignor.output.dto';
 
 @Controller('assignor')
 export class AssignorController {
-  constructor(private readonly createAssignorUseCase: ICreateAssignorUseCase) {}
+  constructor(
+    private readonly createAssignorUseCase: ICreateAssignorUseCase,
+    private readonly findAllAssignorsUseCase: IFindAllAssignorsUseCase,
+  ) {}
 
   @Post()
   async create(
     @Body() createAssignorDTO: CreateAssignorInputDTO,
   ): Promise<CreateAssignorOutputDTO> {
     return await this.createAssignorUseCase.execute(createAssignorDTO);
+  }
+
+  @Get()
+  async findAll(): Promise<FindAssignorOutputDTO[]> {
+    return await this.findAllAssignorsUseCase.execute();
   }
 }
--- backend/src/assignor/assignor.module.ts
+++ backend/src/assignor/assignor.module.ts
@@ -6,6 +6,8 @@ import { IAssignorRepository } from './repositories/assignor.repository.interfac
 import { ICreateAssignorUseCase } from './usecases/create-assignor.usecase.interface';
 import { AssignorMapper } from './mappers/assignor.mapper.interface';
 import { PrismaAssignorMapper } from './mappers/prisma-assignor.mapper';
+import { IFindAllAssignorsUseCase } from './usecases/find-all-assignors.usecase.interface';
+import { FindAllAssignorsUseCase } from './usecases/find-all-assignors.usecase';
 
 @Module({
   controllers: [AssignorController],
@@ -17,6 +19,10 @@ import { PrismaAssignorMapper } from './mappers/prisma-assignor.mapper';
     {
       provide: ICreateAssignorUseCase,
       useClass: CreateAssignorUseCase,
+    },
+    {
+      provide: IFindAllAssignorsUseCase,
+      useClass: FindAllAssignorsUseCase,
     },
     {
       provide: IAssignorRepository,
