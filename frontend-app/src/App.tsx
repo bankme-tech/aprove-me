8aaf558e96214de6041ff51a6764da9b45335219
Nyllaah <nylla.g@hotmail.com>
2024-05-24 17:26:39 -0300

    feat: login page created

--- frontend-app/src/App.tsx
+++ frontend-app/src/App.tsx
@@ -2,12 +2,14 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import CreatePayable from './pages/CreatePayable';
 import CreateAssignor from './pages/CreateAssignor';
+import Login from './pages/Login';
 
 function App() {
   return (
     <Routes>
       <Route path='/integrations/payable' element={ <CreatePayable /> } />
       <Route path='/integrations/assignor' element={ <CreateAssignor /> } />
+      <Route path='/integrations/auth' element={ <Login /> } />
     </Routes>
   )
 }
---
+++ frontend-app/src/pages/Login.tsx
@@ -0,0 +1,13 @@
+function Login() {
+  return(
+    <main>
+      <h1>Efetue o Login</h1>
+      <form action="submit">
+        <input type="text" placeholder="Usuário"/>
+        <input type="text" placeholder="Senha"/>
+      </form>
+    </main>
+  )
+}
+
+export default Login;
