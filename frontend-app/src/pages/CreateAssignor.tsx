35e126d01fa00d42dbb7e83e543d36c6c09ac0af
Nyllaah <nylla.g@hotmail.com>
2024-05-24 15:11:43 -0300

    feat: input control implemented

--- frontend-app/src/pages/CreateAssignor.tsx
+++ frontend-app/src/pages/CreateAssignor.tsx
@@ -1,24 +1,50 @@
+import { useState } from "react";
+
 function CreateAssignor() {
+  const [formData, setFormData] = useState({
+    name: '',
+    document: '',
+    email: '',
+    phone: '',
+  })
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const {name, value} = e.target;
+    
+    setFormData({...formData, [name]: value})
+  }
   return (
     <main>
       <h1>Cadastro de Cedentes</h1>
         <form action="submit">
-          <label htmlFor="">
-            Nome:
-            <input type="text" />
-          </label>
-          <label htmlFor="">
-            Documento:
-            <input type="text" />
-          </label>
-          <label htmlFor="">
-            Email:
-            <input type="text" />
-          </label>
-          <label htmlFor="">
-            Telefone:
-            <input type="text" />
-          </label>
+          <input
+            type="text"
+            placeholder="Nome do cedente"
+            name="name"
+            value={ formData.name }
+            onChange={handleChange}
+            />
+          <input
+            type="text"
+            placeholder="Número do documento"
+            name="document"
+            value={ formData.document }
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            placeholder="Email"
+            name="email"
+            value={ formData.email }
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            name="phone"
+            placeholder="Telefone"
+            value={ formData.phone }
+            onChange={handleChange}
+          />
           <button>Cadastrar</button>
         </form>
     </main>
