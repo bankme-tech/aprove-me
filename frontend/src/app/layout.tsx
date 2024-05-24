54c187d7dc3851d1494936535317282edb37caca
Yanzaum <yandavid80@gmail.com>
2024-05-24 13:28:56 -0300

    feat: Update language attribute to pt-BR in RootLayout component

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="pt-BR">
       <body className={inter.className}>
         <Header />
         <Provider>
