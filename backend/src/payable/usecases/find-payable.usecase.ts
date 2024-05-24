f4859ac3e046ec2883d32d99dac78981e120124c
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2024-05-24 00:36:08 -0300

    feat: add FindPayableUseCase implementation

---
+++ backend/src/payable/usecases/find-payable.usecase.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@nestjs/common';
+import { IPayableRepository } from '../repositories/payable.repository.interface';
+import { PayableEntity } from '../entities/payable.entity';
+import { IFindPayableUseCase } from './find-payable.usecase.interface';
+import { FindPayableInputDTO } from '../dto/find-payable.input.dto';
+
+@Injectable()
+export class FindPayableUseCase implements IFindPayableUseCase {
+  constructor(private readonly assignorRepository: IPayableRepository) {}
+
+  async execute(findPayableDTO: FindPayableInputDTO): Promise<PayableEntity> {
+    return await this.assignorRepository.findOne(findPayableDTO);
+  }
+}
