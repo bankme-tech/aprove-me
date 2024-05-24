3a1624f2e94b8898d7c8f64717d90745441dca0b
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2024-05-24 03:52:59 -0300

    feat: handle record not found error in RemovePayableUseCase

--- backend/src/payable/usecases/remove-payable-usecase.ts
+++ backend/src/payable/usecases/remove-payable-usecase.ts
@@ -2,12 +2,17 @@ import { Injectable } from '@nestjs/common';
 import { IRemovePayableUseCase } from './remove-payable.usecase.interface';
 import { IPayableRepository } from '../repositories/payable.repository.interface';
 import { RemovePayableInputDTO } from '../dto/remove-payable.input.dto';
+import { RecordNotFoundError } from 'src/persistence/errors/record-not-found.error';
 
 @Injectable()
 export class RemovePayableUseCase implements IRemovePayableUseCase {
   constructor(private readonly payableRepository: IPayableRepository) {}
 
-  execute(removePayableDTO: RemovePayableInputDTO): Promise<void> {
+  async execute(removePayableDTO: RemovePayableInputDTO): Promise<void> {
+    const payable = await this.payableRepository.findById(removePayableDTO);
+    if (!payable) {
+      throw new RecordNotFoundError('Payable');
+    }
     return this.payableRepository.remove(removePayableDTO);
   }
 }
--- backend/test/usecases/payable/remove-payable.usecase.spec.ts
+++ backend/test/usecases/payable/remove-payable.usecase.spec.ts
@@ -4,6 +4,7 @@ import { IPayableRepository } from 'src/payable/repositories/payable.repository.
 import { makePayableDTO } from 'test/mocks/dtos.mock';
 import { RemovePayableUseCase } from 'src/payable/usecases/remove-payable-usecase';
 import { RemovePayableInputDTO } from 'src/payable/dto/remove-payable.input.dto';
+import { RecordNotFoundError } from 'src/persistence/errors/record-not-found.error';
 
 describe('RemovePayableUseCase', () => {
   let sut: RemovePayableUseCase;
@@ -36,5 +37,13 @@ describe('RemovePayableUseCase', () => {
     await sut.execute(dto);
 
     expect(removeSpy).toHaveBeenCalledTimes(1);
+  });
+
+  test('should throw if no assignor is found', async () => {
+    jest.spyOn(payableRepositoryStub, 'findById').mockResolvedValue(null);
+
+    const promise = sut.execute(dto);
+
+    await expect(promise).rejects.toThrow(new RecordNotFoundError('Payable'));
   });
 });
