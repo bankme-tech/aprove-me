c333495a53fc6ef902d9c0a52b956688cf095781
Yanzaum <yandavid80@gmail.com>
2024-05-24 13:28:38 -0300

    feat: Add login page component

---
+++ frontend/src/app/login/page.tsx
@@ -0,0 +1,60 @@
+import { Button } from "@nextui-org/react";
+
+export default function Login() {
+    return (
+        <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+          <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+            <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+              Acesse sua conta
+            </h2>
+          </div>
+  
+          <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+            <form className="space-y-6" action="#" method="POST">
+              <div className="flex flex-col">
+                <label htmlFor="login" className="block text-sm font-medium leading-6 text-gray-900">
+                  Login
+                </label>
+                <div className="mt-2">
+                    <input
+                        id="login"
+                        name="login"
+                        type="text"
+                        autoComplete="login"
+                        required
+                        className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                </div>
+              </div>
+  
+              <div className="flex flex-col">
+                <div className="flex items-center justify-between">
+                    <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                        Password
+                    </label>
+                </div>
+                <div className="mt-2">
+                    <input
+                        id="password"
+                        name="password"
+                        type="password"
+                        autoComplete="current-password"
+                        required
+                        className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                </div>
+            </div>
+  
+            <Button
+                type="submit"
+                className="w-full"
+                color="primary"
+            >
+                Logar
+            </Button>
+            </form>
+          </div>
+        </div>
+    )
+  }
+  
