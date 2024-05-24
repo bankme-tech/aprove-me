cfb87a69ddd11f0252aad96742e6c53b96de637b
Yanzaum <yandavid80@gmail.com>
2024-05-24 14:52:42 -0300

    feat: Enable CORS in main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -14,6 +14,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
