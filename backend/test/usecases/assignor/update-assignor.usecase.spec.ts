c6e6dc408bebdaa823db042f5c4796a43fa36824
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2024-05-24 01:52:57 -0300

    test: add tests for UpdateAssignorUseCase

---
+++ backend/test/usecases/assignor/update-assignor.usecase.spec.ts
@@ -0,0 +1,49 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AssignorRepositoryStub } from '../../mocks/repositories/assignor.repository.mock';
+import { IAssignorRepository } from 'src/assignor/repositories/assignor.repository.interface';
+import { UpdateAssignorUseCase } from 'src/assignor/usecases/update-assignor.usecase';
+import { UpdateAssignorInputDTO } from 'src/assignor/dto/update-assignor.input.dto';
+import { makeAssignorEntity } from 'test/mocks/entities/assignor.entity.mock';
+
+describe('UpdateAssignorUseCase', () => {
+  let sut: UpdateAssignorUseCase;
+  let assignorRepositoryStub: AssignorRepositoryStub;
+  let dto: UpdateAssignorInputDTO;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        UpdateAssignorUseCase,
+        {
+          provide: IAssignorRepository,
+          useClass: AssignorRepositoryStub,
+        },
+      ],
+    }).compile();
+
+    sut = module.get<UpdateAssignorUseCase>(UpdateAssignorUseCase);
+    assignorRepositoryStub =
+      module.get<AssignorRepositoryStub>(IAssignorRepository);
+
+    const { id, document, email, name, phone } = makeAssignorEntity();
+    dto = {
+      id,
+      document,
+      email,
+      name,
+      phone,
+    };
+  });
+
+  test('should call assignorRepository with correct values', async () => {
+    await sut.execute(dto);
+
+    expect(assignorRepositoryStub.data).toEqual(dto);
+  });
+
+  test('should return a new assignor', async () => {
+    const response = await sut.execute(dto);
+
+    expect(response).toEqual(assignorRepositoryStub.response);
+  });
+});
