fdbac98b013c45bb947472b8d72a125df1bf1166
Lamenkazu <333.erick@gmail.com>
2024-05-24 12:51:13 -0300

    ✨Not Found Page Style

--- web/aproveme-web/src/pages/404.tsx
+++ web/aproveme-web/src/pages/404.tsx
@@ -2,11 +2,14 @@ import { Link } from 'react-router-dom'
 
 export const NotFound = () => {
   return (
-    <div>
-      <h1>404.</h1>
-      <h2>Página não encontrada.</h2>
-
-      <Link to="/">Voltar para o Dashboard</Link>
+    <div className="flex h-screen flex-col items-center justify-center gap-2">
+      <h1 className="text-6xl font-bold">404.</h1>
+      <h2 className="text-3xl font-bold">Página não encontrada.</h2>
+      <p className="text-accent-foreground">
+        <Link to="/" className="text-sky-600 dark:text-sky-400">
+          Voltar para o painel.
+        </Link>
+      </p>
     </div>
   )
 }
