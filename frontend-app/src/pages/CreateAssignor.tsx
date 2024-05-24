2a3216591e4976607a4842b68ce7711f7d0e5282
Nyllaah <nylla.g@hotmail.com>
2024-05-24 16:29:59 -0300

    feat: createAssignor implemented

--- frontend-app/src/pages/CreateAssignor.tsx
+++ frontend-app/src/pages/CreateAssignor.tsx
@@ -1,22 +1,45 @@
 import { useState } from "react";
+import { axiosInstance as axios } from '../api';
+
+const initialState = {
+  name: '',
+  document: '',
+  email: '',
+  phone: '',
+}
 
 function CreateAssignor() {
-  const [formData, setFormData] = useState({
-    name: '',
-    document: '',
-    email: '',
-    phone: '',
-  })
+  const [formData, setFormData] = useState(initialState);
+  let message;
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const {name, value} = e.target;
-    
     setFormData({...formData, [name]: value})
+  };
+
+  const createAssignor = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      const token = localStorage.getItem('token');
+      const config = {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        }
+      }
+      const response = await axios.post('/assignor', formData, config);
+      setFormData(initialState);
+      console.log(response.data);
+      message = "Cadastrado com sucesso."
+    } catch (err) {
+      message = "Algo deu errado."
+      console.log(err);
+    }
   }
+  
   return (
     <main>
       <h1>Cadastro de Cedentes</h1>
-        <form action="submit">
+        <form action="submit" onSubmit={ createAssignor }>
           <input
             type="text"
             placeholder="Nome do cedente"
@@ -47,6 +70,7 @@ function CreateAssignor() {
           />
           <button>Cadastrar</button>
         </form>
+        <p>{message}</p>
     </main>
   )
 }
