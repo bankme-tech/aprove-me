7da25c87e6b276a61521f0e39f46d27dd38eff91
Yanzaum <yandavid80@gmail.com>
2024-05-24 11:40:20 -0300

    chore: add BullModule to app.module.ts for handling background jobs with NestJS Bull

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,9 +5,21 @@ import { PayablesModule } from './integrations/payables/payables.module';
 import { AssignorsModule } from './integrations/assignors/assignors.module';
 import { AuthModule } from './integrations/auth/auth.module';
 import { UsersModule } from './integrations/users/users.module';
+import { BullModule } from '@nestjs/bull';
 
 @Module({
-  imports: [PayablesModule, AssignorsModule, AuthModule, UsersModule],
+  imports: [
+    PayablesModule,
+    AssignorsModule,
+    AuthModule,
+    UsersModule,
+    BullModule.forRoot({
+      redis: {
+        host: 'localhost',
+        port: 6379,
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
