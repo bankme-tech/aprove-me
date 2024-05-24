29324ac1c3611f35039764fc5847dfcbdd21c643
Lamenkazu <333.erick@gmail.com>
2024-05-24 12:43:23 -0300

    ⚙️✏️ Styled Components To Shadcn/ui with tailwindcss

--- web/aproveme-web/src/App.tsx
+++ web/aproveme-web/src/App.tsx
@@ -1,25 +1,20 @@
+import './styles/globals.css'
+
 import { QueryClientProvider } from '@tanstack/react-query'
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
-import { ThemeProvider } from 'styled-components'
 
 import { queryClient } from './lib/react-query'
 import { router } from './routes'
-import { GlobalStyle } from './styles/global'
-import { defaultTheme } from './styles/themes/default'
 
 export const App = () => {
   return (
     <HelmetProvider>
-      <ThemeProvider theme={defaultTheme}>
-        <GlobalStyle />
+      <Helmet titleTemplate="%s | aprove.me" />
 
-        <Helmet titleTemplate="%s | aprove.me" />
-
-        <QueryClientProvider client={queryClient}>
-          <RouterProvider router={router} />
-        </QueryClientProvider>
-      </ThemeProvider>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </HelmetProvider>
   )
 }
