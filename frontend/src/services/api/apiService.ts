5e3ba01be2988e1108a7347f467a0ee390a9feba
Yanzaum <yandavid80@gmail.com>
2024-05-24 15:02:18 -0300

    feat: Update API base URL and add authorization header

--- frontend/src/services/api/apiService.ts
+++ frontend/src/services/api/apiService.ts
@@ -2,5 +2,13 @@ import { getServerURL } from "@/utils/getServerURL";
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: getServerURL().concat("/api"),
+    baseURL: getServerURL(),
+});
+
+api.interceptors.request.use((config) => {
+    const token = localStorage.getItem("access_token");
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
 });
