eaadc02e4b68ed79a2e942a71f30840ba2f52cb9
Felipe Rodrigues <neath.felipe@gmail.com>
2024-05-24 18:06:06 -0300

    create new route from auth

--- api/src/auth/auth.controller.ts
+++ api/src/auth/auth.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { SignInDto } from './dto/signIn.dto';
 
 @Controller('integrations/auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authSerivce: AuthService) {}
+
+  @HttpCode(HttpStatus.OK)
+  @Post()
+  async signIn(@Body() signInDto: SignInDto) {
+    return await this.authSerivce.signIn(signInDto.login, signInDto.password);
+  }
+}
