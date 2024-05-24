c36363f2c1b0f7c4df0018b87a53fa7dff321f08
Gabriel Duarte <jds.gabrielduarte@gmail.com>
2024-05-23 22:36:26 -0300

    chore: add status in payable model database

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common'
 import { AssignorsModule } from './assignors/assignors.module'
 import { AuthModule } from './auth/auth.module'
+import { DataProcessModule } from './data-process/data-process.module'
 import { PrismaModule } from './database/prisma.module'
 import { HealthzModule } from './healthz/healthz.module'
 import { PayableModule } from './payable/payable.module'
@@ -12,6 +13,7 @@ import { PayableModule } from './payable/payable.module'
     AssignorsModule,
     PayableModule,
     AuthModule,
+    DataProcessModule,
   ],
 })
 export class AppModule {}
--- apps/server/src/environments.ts
+++ apps/server/src/environments.ts
@@ -5,6 +5,8 @@ export const envSchema = z.object({
   JWT_SECRET: z.string(),
   JWT_ISSUER: z.string(),
   JWT_AUDIENCE: z.string(),
+  REDIS_HOST: z.string(),
+  REDIS_PORT: z.number({ coerce: true }),
 })
 
 export const env = envSchema.parse(process.env)
