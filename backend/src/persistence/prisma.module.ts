6404d859d28e118590c3442ed84ed588ab812288
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2024-05-24 19:32:16 -0300

    refactor: add forTest method to PrismaModule for testing purposes

--- backend/src/persistence/prisma.module.ts
+++ backend/src/persistence/prisma.module.ts
@@ -1,9 +1,23 @@
-import { Global, Module } from '@nestjs/common';
+import { DynamicModule, Global, Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
+import { PrismaClient } from '@prisma/client';
 
 @Global()
 @Module({
   providers: [PrismaService],
   exports: [PrismaService],
 })
-export class PrismaModule {}
+export class PrismaModule {
+  static forTest(prismaClient: PrismaClient): DynamicModule {
+    return {
+      module: PrismaModule,
+      providers: [
+        {
+          provide: PrismaService,
+          useFactory: () => prismaClient as PrismaService,
+        },
+      ],
+      exports: [PrismaService],
+    };
+  }
+}
