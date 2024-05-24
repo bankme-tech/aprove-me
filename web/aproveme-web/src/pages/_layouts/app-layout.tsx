4951ea67846795fbca1bb2e003fde92e8495699b
Lamenkazu <333.erick@gmail.com>
2024-05-24 13:23:07 -0300

    ✨ App Layout Style

--- web/aproveme-web/src/pages/_layouts/app-layout.tsx
+++ web/aproveme-web/src/pages/_layouts/app-layout.tsx
@@ -4,10 +4,10 @@ import { Header } from '@/components/header'
 
 export const AppLayout = () => {
   return (
-    <div>
+    <div className="flex min-h-screen flex-col antialiased">
       <Header />
 
-      <div>
+      <div className="flex flex-1 flex-col gap-4 p-8 pt-6">
         <Outlet />
       </div>
     </div>
