2daa402eb6440ed33c1441e497597e4008fdc3cb
Felipe Rodrigues <neath.felipe@gmail.com>
2024-05-24 03:14:01 -0300

    feat: start service assignor

--- api/src/assignor/assignor.service.spec.ts
+++ api/src/assignor/assignor.service.spec.ts
@@ -1,18 +1,53 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AssignorService } from './assignor.service';
+import { PrismaService } from '../db/prisma.service';
+import { Assignor } from './entities/assignor.entity';
+
+const assignorEntityList = [
+  new Assignor(
+    '1',
+    '12345678901',
+    'teste@teste.com',
+    '31999999999',
+    'Assignor 1',
+  ),
+  new Assignor(
+    '2',
+    '98765432109',
+    'teste2@teste.com',
+    '31999999998',
+    'Assignor 2',
+  ),
+];
 
 describe('AssignorService', () => {
-  let service: AssignorService;
+  let assignorService: AssignorService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AssignorService],
+      providers: [
+        AssignorService,
+        {
+          provide: PrismaService,
+          useValue: {
+            assignor: {
+              findMany: jest.fn().mockResolvedValue(assignorEntityList),
+              create: jest.fn().mockResolvedValue(assignorEntityList[0]),
+              findUniqueOrThrow: jest
+                .fn()
+                .mockResolvedValue(assignorEntityList[1]),
+              update: jest.fn().mockResolvedValue(assignorEntityList[1]),
+              delete: jest.fn().mockResolvedValue({}),
+            },
+          },
+        },
+      ],
     }).compile();
 
-    service = module.get<AssignorService>(AssignorService);
+    assignorService = module.get<AssignorService>(AssignorService);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(assignorService).toBeDefined();
   });
 });
