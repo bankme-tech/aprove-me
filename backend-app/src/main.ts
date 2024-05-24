849eb81a8c864d4c095faad18be353e6aba211b5
Nyllaah <nylla.g@hotmail.com>
2024-05-24 17:20:24 -0300

    fix: enabling CORS

--- backend-app/src/main.ts
+++ backend-app/src/main.ts
@@ -4,7 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe()).enableCors();
   await app.listen(3001);
 }
 bootstrap();
