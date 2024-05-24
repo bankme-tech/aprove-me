caa8b9d29c03544f725445d7e2d0f717785a58fb
Nyllaah <nylla.g@hotmail.com>
2024-05-24 09:22:17 -0300

    fix: docker ports

--- backend-app/src/main.ts
+++ backend-app/src/main.ts
@@ -5,6 +5,6 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  await app.listen(3001);
 }
 bootstrap();
--- frontend-app/vite.config.ts
+++ frontend-app/vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+ base: "/",
+ plugins: [react()],
+ preview: {
+  port: 3000,
+  strictPort: true,
+ },
+ server: {
+  port: 3000,
+  strictPort: true,
+  host: true,
+  origin: "http://0.0.0.0:3000",
+ },
+});
