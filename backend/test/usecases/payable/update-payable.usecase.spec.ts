45b34e89d15984242e31ae536f667eb12a9cfc59
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2024-05-24 02:46:23 -0300

    test: add tests for UpdatePayableUseCase

---
+++ backend/test/usecases/payable/update-payable.usecase.spec.ts
@@ -0,0 +1,46 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { PayableRepositoryStub } from '../../mocks/repositories/payable.repository.mock';
+import { IPayableRepository } from 'src/payable/repositories/payable.repository.interface';
+import { makePayableDTO } from 'test/mocks/dtos.mock';
+import { UpdatePayableUseCase } from 'src/payable/usecases/update-payable.usecase';
+import { UpdatePayableInputDTO } from 'src/payable/dto/update-payable.input.dto';
+
+describe('UpdatePayableUseCase', () => {
+  let sut: UpdatePayableUseCase;
+  let payableRepositoryStub: PayableRepositoryStub;
+  let dto: UpdatePayableInputDTO;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        UpdatePayableUseCase,
+        {
+          provide: IPayableRepository,
+          useClass: PayableRepositoryStub,
+        },
+      ],
+    }).compile();
+
+    sut = module.get<UpdatePayableUseCase>(UpdatePayableUseCase);
+
+    payableRepositoryStub =
+      module.get<PayableRepositoryStub>(IPayableRepository);
+
+    const { id } = makePayableDTO();
+    dto = { id };
+  });
+
+  test('should call payableRepository with correct values', async () => {
+    const updateSpy = jest.spyOn(payableRepositoryStub, 'update');
+
+    await sut.execute(dto);
+
+    expect(updateSpy).toHaveBeenCalledTimes(1);
+  });
+
+  test('should return a payable', async () => {
+    const response = await sut.execute(dto);
+
+    expect(response).toStrictEqual(payableRepositoryStub.response);
+  });
+});
