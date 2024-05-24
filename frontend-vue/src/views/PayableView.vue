2f62ce6844836a38792523925f2731817b8f5447
Henrique Mauler <henriquemauler@gmail.com>
2024-05-24 11:22:10 -0300

    added type checking at payable view

--- frontend-vue/src/views/PayableView.vue
+++ frontend-vue/src/views/PayableView.vue
@@ -5,12 +5,28 @@ const assignorName = ref('');
 const payableDescription = ref('');
 const payableAmount = ref('');
 
+const assignorNameError = ref(false);
+const payableDescriptionError = ref(false);
+const payableAmountError = ref(false);
+
+const validateField = (fieldValue: string) => {
+    return fieldValue.trim().length === 0;
+};
+
 const insertPayable = (e: Event) => {
-    e.preventDefault(); // Isso impede que o formulário seja submetido da maneira tradicional
-    console.log(`Assignor Name: ${assignorName.value}`);
-    console.log(`Payable Description: ${payableDescription.value}`);
-    console.log(`Payable Amount: ${payableAmount.value}`);
+    
+    console.log({vv : validateField(assignorName.value)})
+    assignorNameError.value = validateField(assignorName.value);
+    payableDescriptionError.value = validateField(payableDescription.value);
+    payableAmountError.value = validateField(payableAmount.value);
+
+    if (!assignorNameError.value && !payableDescriptionError.value && !payableAmountError.value) {
+        console.log(`Assignor Name: ${assignorName.value}`);
+        console.log(`Payable Description: ${payableDescription.value}`);
+        console.log(`Payable Amount: ${payableAmount.value}`);
+    }
 }
+
 </script>
 
 <template>
@@ -20,24 +36,30 @@ const insertPayable = (e: Event) => {
                 Assignor name
             </label>
             <input v-model="assignorName"
+                :class="{'border-red-500': assignorNameError}"
                 class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                 id="assignorName" type="text" placeholder="">
+            <p v-if="assignorNameError" class="text-red-500 text-xs italic">Please fill out this field.</p>
         </div>
         <div class="mb-4">
             <label class="block text-gray-700 text-sm font-bold mb-2" for="payableDescription">
                 Payable description
             </label>
             <input v-model="payableDescription"
+                :class="{'border-red-500': payableDescriptionError}"
                 class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                 id="payableDescription" type="text" placeholder="">
+            <p v-if="payableDescriptionError" class="text-red-500 text-xs italic">Please fill out this field.</p>
         </div>
         <div class="mb-4">
             <label class="block text-gray-700 text-sm font-bold mb-2" for="payableAmount">
                 Payable Amount
             </label>
             <input v-model="payableAmount"
+                :class="{'border-red-500': payableAmountError}"
                 class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                 id="payableAmount" type="text" placeholder="">
+            <p v-if="payableAmountError" class="text-red-500 text-xs italic">Please fill out this field.</p>
         </div>
         <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
             type="submit">
