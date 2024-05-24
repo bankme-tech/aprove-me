9fad9f39ff1ab63afc6a617c8c18113f0b5fd347
Felipe Rodrigues <neath.felipe@gmail.com>
2024-05-24 19:24:01 -0300

    feat: complete auth controller tests

--- api/src/auth/auth.controller.spec.ts
+++ api/src/auth/auth.controller.spec.ts
@@ -1,18 +1,72 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { UnauthorizedException } from '@nestjs/common';
 
 describe('AuthController', () => {
-  let controller: AuthController;
+  let authController: AuthController;
+  let authSerivce: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
+      providers: [
+        {
+          provide: AuthService,
+          useValue: {
+            signIn: jest.fn().mockResolvedValue({
+              access_token: 'access_token',
+            }),
+          },
+        },
+      ],
     }).compile();
 
-    controller = module.get<AuthController>(AuthController);
+    authController = module.get<AuthController>(AuthController);
+    authSerivce = module.get<AuthService>(AuthService);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(authController).toBeDefined();
+    expect(authSerivce).toBeDefined();
+  });
+
+  describe('signIn', () => {
+    it('should return access token', async () => {
+      // Arrange
+      const signInDto = {
+        login: 'login',
+        password: 'password',
+      };
+
+      // Act
+      const result = await authController.signIn(signInDto);
+
+      // Assert
+      expect(result).toEqual({ access_token: 'access_token' });
+    });
+
+    it('should throw an error', async () => {
+      // Arrange
+      const signInDto = {
+        login: 'login',
+        password: 'password',
+      };
+
+      jest
+        .spyOn(authSerivce, 'signIn')
+        .mockRejectedValue(
+          new UnauthorizedException('Invalid login or password'),
+        );
+
+      // Act
+      try {
+        await authController.signIn(signInDto);
+      } catch (error) {
+        // Assert
+        expect(error).toBeInstanceOf(UnauthorizedException);
+        expect(error.message).toBe('Invalid login or password');
+      }
+    });
   });
 });
