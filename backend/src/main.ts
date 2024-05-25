c5578ef5990d97696dbaf3f707841d78c8b710a1
Henrique Mauler <henriquemauler@gmail.com>
2024-05-25 16:13:07 -0300

    adjusments at settings and enable cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -13,6 +13,21 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  // Habilitar CORS para todos os domínios
+  app.enableCors();
+
+  // Ou, para habilitar CORS com opções específicas:
+  app.enableCors({
+    origin: 'http://localhost:5173', // Substitua com o(s) domínio(s) que você quer permitir
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    allowedHeaders: 'Content-Type, Accept',
+    credentials: true,
+  });
+
+  // enable fot all interfaces (necessary for develoment cause I'm using WSL).
   await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
