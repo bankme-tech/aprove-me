8a40c7e627ae13fd2c03f4a4b888cfb1812a6f98
Lamenkazu <333.erick@gmail.com>
2024-05-24 22:00:36 -0300

    ✨ Register Payable And Assignor Pages

--- web/aproveme-web/src/pages/app/register-assignor.tsx
+++ web/aproveme-web/src/pages/app/register-assignor.tsx
@@ -1,3 +1,49 @@
+import { Helmet } from 'react-helmet-async'
+
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+
 export const RegisterAssignor = () => {
-  return <div>RegisterAssignor</div>
+  return (
+    <>
+      <Helmet />
+      <h1 className="text-3xl font-bold tracking-tight">Registrar Cedente</h1>
+
+      <main className=" w-[500px] self-center">
+        <section className="flex flex-col justify-center gap-6">
+          <form className="space-y-7    ">
+            <div className="space-y-2">
+              <Label htmlFor="name">Nome</Label>
+              <Input id="name" type="text" />
+            </div>
+
+            <div className="space-y-2">
+              <Label htmlFor="email">Email</Label>
+              <Input id="email" type="email" />
+            </div>
+
+            <div className="flex gap-6">
+              <div className="w-full space-y-2">
+                <Label htmlFor="phone">Telefone</Label>
+                <Input id="phone" type="tel" />
+              </div>
+
+              <div className="w-full space-y-2">
+                <Label htmlFor="document">Documento</Label>
+                <span className="text-xs text-muted-foreground">
+                  &nbsp; (CPF | CNPJ)
+                </span>
+                <Input id="document" type="text" />
+              </div>
+            </div>
+
+            <Button type="submit" className="w-full" disabled={false}>
+              Finalizar cadastro
+            </Button>
+          </form>
+        </section>
+      </main>
+    </>
+  )
 }
--- web/aproveme-web/src/pages/app/register-payable.tsx
+++ web/aproveme-web/src/pages/app/register-payable.tsx
@@ -1,3 +1,54 @@
+import { Helmet } from 'react-helmet-async'
+
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select'
+
 export const RegisterPayable = () => {
-  return <div>RegisterPayable</div>
+  return (
+    <>
+      <Helmet title="Novo Recebível" />
+      <h1 className="text-3xl font-bold tracking-tight">Registrar Recebível</h1>
+      <main className="mt-16 w-[500px] self-center">
+        <section className="flex flex-col justify-center gap-6 ">
+          <form className="space-y-8">
+            <div className="space-y-4">
+              <div className="space-y-2">
+                <Label htmlFor="login">Valor</Label>{' '}
+                <span className="text-xs">(R$)</span>
+                <Input id="login" type="number" />
+              </div>
+
+              <div className="space-y-2">
+                <Label htmlFor="password">Cedente</Label>
+                <Select>
+                  <SelectTrigger>
+                    <SelectValue placeholder="" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectItem value="light">MST S.A.</SelectItem>
+                    <SelectItem value="system">
+                      Erick Etiene Simião Ferreira
+                    </SelectItem>
+                    <SelectItem value="dark">Friboi S.A.</SelectItem>
+                  </SelectContent>
+                </Select>
+              </div>
+            </div>
+
+            <Button type="submit" className="w-full" disabled={false}>
+              Registrar
+            </Button>
+          </form>
+        </section>
+      </main>
+    </>
+  )
 }
