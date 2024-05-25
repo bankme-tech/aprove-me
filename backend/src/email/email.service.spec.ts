711d5af910a5e9de8506b37b8f71e8d1734b8b46
Donathan Queiros <66445710+donathanqueiros@users.noreply.github.com>
2024-05-25 10:10:52 -0300

    feat: add email service tests

--- backend/src/email/email.service.spec.ts
+++ backend/src/email/email.service.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { EmailService } from './email.service';
+import { HttpException, HttpStatus } from '@nestjs/common';
 
 describe('EmailService', () => {
   let service: EmailService;
@@ -14,5 +15,48 @@ describe('EmailService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('sendEmail', () => {
+    it('should return the email message object when successful', async () => {
+      const emailOptions = {
+        email: 'test@example.com',
+        subject: 'Test Subject',
+        html: '<p>Test HTML Content</p>',
+      };
+
+      const result = await service.sendEmail(emailOptions);
+
+      expect(result).toEqual({
+        to: emailOptions.email,
+        subject: emailOptions.subject,
+        html: emailOptions.html,
+      });
+    });
+
+    it('should throw an HttpException when an error occurs', async () => {
+      const emailOptions = {
+        email: 'test@example.com',
+        subject: 'Test Subject',
+        html: '<p>Test HTML Content</p>',
+      };
+
+      // Mock the email sending function to throw an error
+      const originalSendEmail = service.sendEmail;
+      jest.spyOn(service, 'sendEmail').mockImplementation(async () => {
+        throw new HttpException('Error', HttpStatus.INTERNAL_SERVER_ERROR);
+      });
+
+      await expect(service.sendEmail(emailOptions)).rejects.toThrow(
+        HttpException,
+      );
+      await expect(service.sendEmail(emailOptions)).rejects.toMatchObject({
+        message: 'Error',
+        status: HttpStatus.INTERNAL_SERVER_ERROR,
+      });
+
+      // Restore the original implementation
+      jest.spyOn(service, 'sendEmail').mockImplementation(originalSendEmail);
+    });
   });
 });
