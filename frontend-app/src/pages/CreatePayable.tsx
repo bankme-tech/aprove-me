40064653b5c58e626884ce3c647c5fb48da3dcb3
Nyllaah <nylla.g@hotmail.com>
2024-05-24 22:01:24 -0300

    fix: removing extra bearer in token

--- frontend-app/src/pages/CreatePayable.tsx
+++ frontend-app/src/pages/CreatePayable.tsx
@@ -1,15 +1,86 @@
+import { useEffect, useState } from "react";
+import { axiosInstance as axios} from "../api";
+
+type Assignor = {
+  id: string,
+  name: string,
+}
+
+const initialState = {
+  assignor: '',
+  value: 0,
+}
+
 function CreatePayable() {
+  const [assignors, setAssignors] = useState<Assignor[]>([]);
+  const [formData, setFormData] = useState(initialState);
+
+  const token = localStorage.getItem('token');
+  const config = { headers: { 'Authorization': token } };
+
+  useEffect(() => {
+    const getAssignors = async () => {
+      try {
+        const { data } = await axios.get('/assignor', config);
+        setAssignors(data);
+      } catch (err) {
+        alert('Você não está logado.')
+        console.log(err);
+      }
+    }
+    
+    getAssignors()
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [setAssignors])
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const { id } = assignors.find(({ name }) => name === formData.assignor) as Assignor;
+
+    const payload = { assignor: id, value: formData.value };
+
+    try {
+      const { data } = await axios.post('/payable', payload, config);
+      console.log(data);
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } catch (err: any) {
+      console.log(err.response.data.message);
+    }
+  }
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+    const { name, value } = e.target;
+    setFormData({...formData, [name]: value})
+  }
+
   return (
     <main>
       <h1>Cadastro de Recebíveis</h1>
-      <form action="submit">
+      <form action="submit" onSubmit={ handleSubmit }>
         <label htmlFor="assignor">
           Cedente:
-          <select name="assignor" id="assignor-select">
-            {/* TO DO carregar cedentes do banco */}
+          <select
+            name="assignor"
+            id="assignor-select"
+            value={ formData.assignor }
+            onChange={ handleChange }
+          >
+            {assignors.map(({name}, index) => {
+              return (
+                <option
+                  key={ index } value={ name }>{ name }</option>
+              )
+            })}
           </select>
         </label>
-        <input type="number" name="value" id="payable-value-input" />
+        <input
+          type="number"
+          placeholder="Valor a receber"
+          name="value"
+          id="payable-value-input"
+          value={ formData.value }
+          onChange={ handleChange }
+        />
         <button>Cadastrar</button>
       </form>
     </main>
