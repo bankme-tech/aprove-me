cc2218cdfb37d4bb3b2a1df902007adf09a86550
jjribeiro1 <jvsr99@hotmail.com>
2024-05-24 22:30:30 -0300

    [backend] Setup rabbitMQ connection

--- bankme-backend/src/app/payable/payable.module.ts
+++ bankme-backend/src/app/payable/payable.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 import { DbModule } from 'src/db/db.module';
 import { AssignorModule } from 'src/app/assignor/assignor.module';
 import { PayableController } from './payable.controller';
@@ -6,8 +7,22 @@ import { PayableService } from './payable.service';
 import { PayableRepository } from './payable.repository';
 
 @Module({
-  imports: [DbModule, AssignorModule],
+  imports: [
+    DbModule,
+    AssignorModule,
+    ClientsModule.register([
+      {
+        name: 'RABBIT_CLIENT',
+        transport: Transport.RMQ,
+        options: {
+          urls: ['amqp://rabbitmq:5672'],
+          queue: 'payable_queue',
+          queueOptions: { durable: true },
+        },
+      },
+    ]),
+  ],
   controllers: [PayableController],
-  providers: [PayableService, PayableRepository]
+  providers: [PayableService, PayableRepository],
 })
 export class PayableModule {}
--- bankme-backend/src/main.ts
+++ bankme-backend/src/main.ts
@@ -3,6 +3,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app/app.module';
 import { PrismaClientExceptionFilter } from './app/filters/prisma.exception.filter';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -28,6 +29,17 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.RMQ,
+    options: {
+      urls: ['amqp://rabbitmq:5672'],
+      queue: 'payable_queue',
+      queueOptions: { durable: true },
+      consumerTag: 'payable_consumer',
+    },
+  });
+
+  await app.startAllMicroservices();
   await app.listen(3001);
 }
 bootstrap();
