1213ba7cc5c234cfe11c272df1d58c946b005dd3
Arthur Almeida <arthursa.dev@gmail.com>
2024-05-25 10:21:44 -0300

    feat: add soft delete middleware to prisma client

--- backend/src/infra/database/prisma.service.ts
+++ backend/src/infra/database/prisma.service.ts
@@ -5,5 +5,32 @@ import { PrismaClient } from '@prisma/client';
 export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
     await this.$connect();
+    this.$use((params, next) => {
+      if (params.action === 'delete') {
+        return next({
+          ...params,
+          action: 'update',
+          args: {
+            ...params.args,
+            data: {
+              deletedAt: new Date(),
+            },
+          },
+        });
+      }
+      if (['findMany', 'findUnique'].includes(params.action)) {
+        return next({
+          ...params,
+          args: {
+            ...params.args,
+            where: {
+              ...params.args?.where,
+              deletedAt: null,
+            },
+          },
+        });
+      }
+      return next(params);
+    });
   }
 }
