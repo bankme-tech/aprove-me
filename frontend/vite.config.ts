d6f937a36fd755bd63c25c8820f4e48650f8c990
Jorge De Jesus Cardoso <tst.jorgecardoso@gmail.com>
2024-05-26 02:00:26 -0300

    feat: dockerizando o frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default {
+  server: {
+    host: '0.0.0.0',
+    port: 3001
+  }
+};
+
