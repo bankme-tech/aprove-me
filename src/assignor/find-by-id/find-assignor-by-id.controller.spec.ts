bfe57bbac1422566d563694af4852b7083a442f1
Matheus Lopes <inolopesm@gmail.com>
2024-05-26 10:35:59 -0300

    cover FindAssignorByIdController with tests

---
+++ src/assignor/find-by-id/find-assignor-by-id.controller.spec.ts
@@ -0,0 +1,40 @@
+import { Test } from "@nestjs/testing";
+import type { Assignor } from "@prisma/client";
+
+import { PrismaProvider } from "../../providers/prisma.provider";
+import { FindAssignorByIdController } from "./find-assignor-by-id.controller";
+
+describe("FindAssignorByIdController", () => {
+  let findAssignorByIdController: FindAssignorByIdController;
+
+  beforeEach(async () => {
+    const moduleRef = await Test.createTestingModule({
+      controllers: [FindAssignorByIdController],
+      providers: [
+        {
+          provide: PrismaProvider,
+          useValue: { assignor: { findUnique: jest.fn() } },
+        },
+      ],
+    }).compile();
+
+    findAssignorByIdController = moduleRef.get(FindAssignorByIdController);
+  });
+
+  it("should be defined", () => {
+    expect(findAssignorByIdController).toBeDefined();
+  });
+
+  it("should return the founded assignor by id", async () => {
+    const assignor: Assignor = {
+      id: "uuid",
+      document: "document",
+      email: "email",
+      phone: "phone",
+      name: "name",
+    };
+
+    const output = await findAssignorByIdController.handle(assignor);
+    expect(output).toEqual(assignor);
+  });
+});
