f46708856850f9e76aa7aea1978e24d094c2b53c
Yanzaum <yandavid80@gmail.com>
2024-05-26 19:55:57 -0300

    refactor: Remove commented out code in lib.ts

--- frontend/src/lib.ts
+++ frontend/src/lib.ts
@@ -30,20 +30,3 @@ export async function getToken() {
   if (!token) return null;
   return token;
 }
-
-// export async function updateSession(request: NextRequest) {
-//   const session = request.cookies.get("session")?.value;
-//   if (!session) return;
-
-//   // Refresh the session so it doesn't expire
-//   const parsed = await decrypt(session);
-//   parsed.expires = new Date(Date.now() + 10 * 1000);
-//   const res = NextResponse.next();
-//   res.cookies.set({
-//     name: "session",
-//     value: await encrypt(parsed),
-//     httpOnly: true,
-//     expires: parsed.expires,
-//   });
-//   return res;
-// }
