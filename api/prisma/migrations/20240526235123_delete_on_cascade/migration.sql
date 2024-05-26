93cd2a7ccaec91f601fb1552081d3a75c209c8b7
Felipe Rodrigues <neath.felipe@gmail.com>
2024-05-26 20:52:34 -0300

    fix: rule delete from cascade

---
+++ api/prisma/migrations/20240526235123_delete_on_cascade/migration.sql
@@ -0,0 +1,14 @@
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Payable" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "value" REAL NOT NULL,
+    "emissionDate" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "assignorId" TEXT NOT NULL,
+    CONSTRAINT "Payable_assignorId_fkey" FOREIGN KEY ("assignorId") REFERENCES "Assignor" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+INSERT INTO "new_Payable" ("assignorId", "emissionDate", "id", "value") SELECT "assignorId", "emissionDate", "id", "value" FROM "Payable";
+DROP TABLE "Payable";
+ALTER TABLE "new_Payable" RENAME TO "Payable";
+PRAGMA foreign_key_check("Payable");
+PRAGMA foreign_keys=ON;
