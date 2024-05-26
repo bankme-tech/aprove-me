b3658a8304792415268f5de42dd294143356a15d
Nyllaah <nylla.g@hotmail.com>
2024-05-25 22:23:16 -0300

    feat: assignor detail page implemented

--- frontend-app/src/pages/assignor/AssignorDetails.tsx
+++ frontend-app/src/pages/assignor/AssignorDetails.tsx
@@ -1,6 +1,34 @@
+import { useEffect, useState } from "react";
+import { Assignor } from "../../types";
+import { axiosInstance as axios } from "../../api";
+import { useParams } from "react-router-dom";
+import { getTokenAndSetHeaders } from "../../helpers";
+
 function AssignorDetails() {
+  const [assignor, setAssignor] = useState<Assignor>()
+  const { id } = useParams();
+  const config = getTokenAndSetHeaders();
+  
+  useEffect(() => {
+    const getAssignor = async () => {
+      try {
+        const { data } = await axios.get(`/assignor/${id}`, config);
+        setAssignor(data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getAssignor();
+  }, [config, id]);
+  
   return (
-    <h1>Detalhes do Cedente</h1>
+    <main>
+      <h1>Detalhes do Cedente</h1>
+      <p>{`Nome: ${assignor?.name}`}</p>
+      <p>{`Documento: ${assignor?.document}`}</p>
+      <p>{`Email: ${assignor?.email}`}</p>
+      <p>{`Telefone: ${assignor?.phone}`}</p>
+    </main>
   )
 }
 
