10b2070a33263cfde5a8dc2a5c5de6eeb06e7af5
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2024-05-26 12:09:02 -0300

    refactor: improve error handling in RabbitMQProducer

--- backend/src/rabbitmq/rabbitmq.producer.ts
+++ backend/src/rabbitmq/rabbitmq.producer.ts
@@ -5,15 +5,23 @@ import { IProducer } from './interfaces/producer.interface';
 
 @Injectable()
 export class RabbitMQProducer<T> implements IProducer<T> {
+  private readonly logger = new Logger(RabbitMQProducer.name);
+  private readonly queue = process.env.RABBITMQ_QUEUE;
+
   constructor(
     @Inject('RABBITMQ_SERVICE') private readonly client: ClientProxy,
   ) {}
 
   async publishMessage(message: T): Promise<void> {
-    lastValueFrom(this.client.emit(process.env.RABBITMQ_QUEUE, message)).then(
-      () => {
-        Logger.log('Message published', RabbitMQProducer.name);
-      },
-    );
+    try {
+      lastValueFrom(this.client.emit(this.queue, message));
+      this.logger.log(`Message published to queue '${this.queue}'`);
+    } catch (error) {
+      this.logger.error(
+        `Failed to publish message to queue '${this.queue}'`,
+        error.stack,
+      );
+      throw error;
+    }
   }
 }
