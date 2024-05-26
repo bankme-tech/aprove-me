58714d18eaa8432b33158c3feed57e1228bcd75e
Yanzaum <yandavid80@gmail.com>
2024-05-26 19:52:10 -0300

    refactor: update login page to use async function for login and redirect after successful login

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,6 +1,8 @@
-import { Button } from "@nextui-org/react";
+import { login } from "@/lib";
+import { redirect } from "next/navigation";
+import LoginButton from "./LoginButton";
 
-export default function Login() {
+export default async function Login() {
     return (
         <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
           <div className="sm:mx-auto sm:w-full sm:max-w-sm">
@@ -10,24 +12,31 @@ export default function Login() {
           </div>
   
           <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-            <form className="space-y-6" action="#" method="POST">
-              <div className="flex flex-col">
-                <label htmlFor="login" className="block text-sm font-medium leading-6 text-gray-900">
-                  Login
-                </label>
-                <div className="mt-2">
-                    <input
-                        id="login"
-                        name="login"
-                        type="text"
-                        autoComplete="login"
-                        required
-                        className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    />
+            <form
+                className="space-y-6"
+                action={async (formData) => {
+                    'use server';
+                    await login(formData);
+                    redirect('/')
+                }}
+                noValidate
+            >
+                <div className="flex flex-col">
+                    <label htmlFor="login" className="block text-sm font-medium leading-6 text-gray-900">
+                    Login
+                    </label>
+                    <div className="mt-2">
+                        <input
+                            type="text"
+                            name="login"
+                            autoComplete="login"
+                            required
+                            className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        />
+                    </div>
                 </div>
-              </div>
   
-              <div className="flex flex-col">
+                <div className="flex flex-col">
                 <div className="flex items-center justify-between">
                     <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
                         Password
@@ -35,23 +44,16 @@ export default function Login() {
                 </div>
                 <div className="mt-2">
                     <input
-                        id="password"
-                        name="password"
                         type="password"
+                        name="password"
                         autoComplete="current-password"
                         required
                         className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                     />
                 </div>
-            </div>
+                </div>
   
-            <Button
-                type="submit"
-                className="w-full"
-                color="primary"
-            >
-                Logar
-            </Button>
+                <LoginButton />
             </form>
           </div>
         </div>
