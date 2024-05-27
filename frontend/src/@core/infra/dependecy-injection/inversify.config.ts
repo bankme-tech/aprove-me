169ee9a78d61675ed3c4c10847fb492ee6298fbe
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2024-05-27 18:21:31 -0300

    chore: update dependency injection configuration

--- frontend/src/@core/infra/dependecy-injection/inversify.config.ts
+++ frontend/src/@core/infra/dependecy-injection/inversify.config.ts
@@ -1,14 +1,20 @@
 import "reflect-metadata";
 import { Container } from "inversify";
-import { IPayableGateway } from "@/@core/domain/gateways/payable.gateway";
 import { TYPES } from "./types";
-import { LocalStoragePayableGateway } from "../gateways/local-storage-payable.gateway";
 import { IPayableService } from "@/@core/domain/services/payable.service.interface";
 import { PayableService } from "@/@core/application/services/payable.service";
+import { AxiosHttpClient } from "../http/axios.http.client";
+import { IPayableGateway } from "@/@core/domain/gateways/payable.gateway";
+import { IAssignorService } from "@/@core/domain/services/assignor.service.interface";
+import { AssignorService } from "@/@core/application/services/assignor.service";
+import { IAssignorGateway } from "@/@core/domain/gateways/assignor.gateway";
+import { HttpAssignorGateway } from "../gateways/http-assignor.gateway";
 
 export const myContainer = new Container();
 
+myContainer.bind(TYPES.IHttpClient).to(AxiosHttpClient);
 myContainer
-  .bind<IPayableGateway>(TYPES.IPayableGateway)
-  .to(LocalStoragePayableGateway);
+  .bind<IAssignorGateway>(TYPES.IAssignorGateway)
+  .to(HttpAssignorGateway);
 myContainer.bind<IPayableService>(TYPES.IPayableService).to(PayableService);
+myContainer.bind<IAssignorService>(TYPES.IAssignorService).to(AssignorService);
--- frontend/src/@core/infra/dependecy-injection/types.ts
+++ frontend/src/@core/infra/dependecy-injection/types.ts
@@ -1,6 +1,9 @@
 const TYPES = {
   IPayableGateway: Symbol.for("IPayableGateway"),
   IPayableService: Symbol.for("IPayableService"),
+  IAssignorGateway: Symbol.for("IAssignorGateway"),
+  IAssignorService: Symbol.for("IAssignorService"),
+  IHttpClient: Symbol.for("IHttpClient"),
 };
 
 export { TYPES };
