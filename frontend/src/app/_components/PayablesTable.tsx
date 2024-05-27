d04cc06a07749e049d54dfd0f4c7c80ec2bedb04
Yanzaum <yandavid80@gmail.com>
2024-05-27 08:23:13 -0300

    feat: add payables table

--- frontend/src/app/_components/PayablesTable.tsx
+++ frontend/src/app/_components/PayablesTable.tsx
@@ -7,100 +7,103 @@ import moment from "@/utils/moment";
 import { Eye, PencilSimple, Plus, Trash } from "phosphor-react";
 import { Key, useCallback } from "react";
 import CreatePayable from "./forms/payable/CreatePayable/form";
+import CreateAssignor from "./forms/assignor/CreateAssignor/form";
 
 const columns = [
-    {name: "ID", uid: "id"},
-    {name: "VALOR", uid: "value"},
-    {name: "DATA DE EMISSÃO", uid: "emissionDate"},
-    {name: "AÇÕES", uid: "actions"},
+  { name: "ID", uid: "id" },
+  { name: "VALOR", uid: "value" },
+  { name: "DATA DE EMISSÃO", uid: "emissionDate" },
+  { name: "AÇÕES", uid: "actions" },
 ];
 
 export default function PayablesTable() {
-    const {
-      data: payables = [],
-      isLoading,
-      isError,
-    } = useQuery({
-      queryKey: ["payables"],
-      queryFn: () => payableService.getAllPayables(),
-    })
+  const {
+    data: payables = [],
+    isLoading,
+    isError,
+  } = useQuery({
+    queryKey: ["payables"],
+    queryFn: () => payableService.getAllPayables(),
+  })
 
-    const { isOpen: isOpenCreatePayable, onOpen: onOpenCreatePayable, onOpenChange: onOpenChangeCreatePayable } = useDisclosure({
-      id: "create-payable",
-    });
+  const { isOpen: isOpenCreatePayable, onOpen: onOpenCreatePayable, onOpenChange: onOpenChangeCreatePayable } = useDisclosure({
+    id: "create-payable",
+  });
 
-    const { isOpen: isOpenCreateAssignor, onOpen: onOpenCreateAssignor, onOpenChange: onOpenChangeCreateAssignor } = useDisclosure({
-      id: "create-assignor",
-    });
+  const { isOpen: isOpenCreateAssignor, onOpen: onOpenCreateAssignor, onOpenChange: onOpenChangeCreateAssignor } = useDisclosure({
+    id: "create-assignor",
+  });
 
-    const renderCell = useCallback((payable: Payable, columnKey: Key) => {
-        const cellValue = payable[columnKey as keyof Payable];
-    
-        switch (columnKey) {
-          case "emissionDate":
-            return moment(cellValue).format("DD/MM/YYYY");
-          case "actions":
-            return (
-              <div className="relative flex items-center gap-2">
-                <Tooltip content="Details">
-                  <span className="text-lg text-default-400 cursor-pointer active:opacity-50">
-                    <Eye />
-                  </span>
-                </Tooltip>
-                <Tooltip content="Edit user">
-                  <span className="text-lg text-default-400 cursor-pointer active:opacity-50">
-                    <PencilSimple />
-                  </span>
-                </Tooltip>
-                <Tooltip color="danger" content="Delete user">
-                  <span className="text-lg text-danger cursor-pointer active:opacity-50">
-                    <Trash />
-                  </span>
-                </Tooltip>
-              </div>
-            );
-          default:
-            return renderCell(payable, columnKey as keyof Payable)
+  const renderCell = useCallback((payable: Payable, columnKey: Key) => {
+    const cellValue = payable[columnKey as keyof Payable];
+
+    switch (columnKey) {
+      case "emissionDate":
+        return moment(cellValue).format("DD/MM/YYYY");
+      case "actions":
+        return (
+          <div className="relative flex items-center gap-2">
+            <Tooltip content="Details">
+              <span className="text-lg text-default-400 cursor-pointer active:opacity-50">
+                <Eye />
+              </span>
+            </Tooltip>
+            <Tooltip content="Edit user">
+              <span className="text-lg text-default-400 cursor-pointer active:opacity-50">
+                <PencilSimple />
+              </span>
+            </Tooltip>
+            <Tooltip color="danger" content="Delete user">
+              <span className="text-lg text-danger cursor-pointer active:opacity-50">
+                <Trash />
+              </span>
+            </Tooltip>
+          </div>
+        );
+      default:
+        return renderCell(payable, columnKey as keyof Payable)
+    }
+  }, []);
+
+  return (
+    <>
+      <Table
+        aria-label="Payables table list"
+        topContent={
+          <div className="flex items-center justify-end gap-2">
+            <Button type="button" color="primary" endContent={<Plus />} onClick={onOpenCreateAssignor}>
+              Adicionar cedente
+            </Button>
+            <Button type="button" color="primary" endContent={<Plus />} onClick={onOpenCreatePayable}>
+              Adicionar recebível
+            </Button>
+          </div>
         }
-    }, []);
-
-    return (
-      <>
-        <Table
-          aria-label="Payables table list"
-          topContent={
-            <div className="flex items-center justify-end gap-2">
-              <Button type="button" color="primary" endContent={<Plus />} onClick={onOpenCreateAssignor}>
-                Adicionar cedente
-              </Button>
-              <Button type="button" color="primary" endContent={<Plus />} onClick={onOpenCreatePayable}>
-                Adicionar recebível
-              </Button>
-            </div>
-          }
+      >
+        <TableHeader columns={columns}>
+          {(column) => (
+            <TableColumn key={column.uid} align={column.uid === "actions" ? "center" : "start"}>
+              {column.name}
+            </TableColumn>
+          )}
+        </TableHeader>
+        <TableBody
+          items={payables}
+          emptyContent={"Não há recebíveis para exibir."}
+          isLoading={isLoading}
+          loadingContent={<Spinner label="Carregando..." color="warning" />}
         >
-            <TableHeader columns={columns}>
-                {(column) => (
-                <TableColumn key={column.uid} align={column.uid === "actions" ? "center" : "start"}>
-                    {column.name}
-                </TableColumn>
-                )}
-            </TableHeader>
-            <TableBody
-              items={payables}
-              emptyContent={"Não há recebíveis para exibir."}
-              isLoading={isLoading}
-              loadingContent={<Spinner label="Carregando..." color="warning" />}
-            >
-              {(item) => (
-                <TableRow key={item.id}>
-                    {(columnKey) => <TableCell>{renderCell(item, columnKey)}</TableCell>}
-                </TableRow>
-              )}
-            </TableBody>
-        </Table>
+          {(item) => (
+            <TableRow key={item.id}>
+              {(columnKey) => <TableCell>{renderCell(item, columnKey)}</TableCell>}
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
 
-        <CreatePayable isOpen={isOpenCreatePayable} onOpenChange={onOpenChangeCreatePayable} />
-      </>
-    )
+      {isOpenCreatePayable && <CreatePayable isOpen={isOpenCreatePayable} onOpenChange={onOpenChangeCreatePayable} />}
+
+      {isOpenCreateAssignor && <CreateAssignor isOpen={isOpenCreateAssignor} onOpenChange={onOpenChangeCreateAssignor} />}
+    </>
+  )
 }
