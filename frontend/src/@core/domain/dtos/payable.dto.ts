640c50cade669a026746ee04ebca04cad6da5664
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2024-05-27 05:55:47 -0300

    feat: add PayableService interface

---
+++ frontend/src/@core/domain/dtos/payable.dto.ts
@@ -0,0 +1,10 @@
+import { Payable } from "../entities/payable.entity";
+
+export type CreatePayableInputDTO = Pick<
+  Payable,
+  "value" | "emissionDate" | "assignorId"
+>;
+
+export type CreatePayableOutputDTO = Payable;
+
+export type FindPayableOutputDTO = Payable;
---
+++ frontend/src/@core/domain/services/payable.service.interface.ts
@@ -0,0 +1,9 @@
+import {
+  CreatePayableInputDTO,
+  CreatePayableOutputDTO,
+} from "../dtos/payable.dto";
+
+export interface IPayableService {
+  create(data: CreatePayableInputDTO): Promise<CreatePayableOutputDTO>;
+  findById(id: string): Promise<CreatePayableOutputDTO> | null;
+}
