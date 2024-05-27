9f3fc5e4f68f751b78abaf75cadc991736d1624a
Henrique Mauler <henriquemauler@gmail.com>
2024-05-27 13:04:05 -0300

    adjustments for better route redirection

--- frontend-vue/src/router/index.ts
+++ frontend-vue/src/router/index.ts
@@ -1,20 +1,20 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import LoginView from '../views/LoginView.vue'
-import PayableView from '@/views/PayableView.vue'
-import ListPayables from '@/views/ListPayables.vue'
-// Could not find a declaration file for module '@/views/ListPayables.vue'. '/home/rickymal/Ubuntu Workspace/aprove-me/frontend-vue/src/views/ListPayables.vue' implicitly has an 'any' type.ts(7016)
+import { createRouter, createWebHistory } from 'vue-router';
+import LoginView from '../views/LoginView.vue';
+import PayableView from '@/views/PayableView.vue';
+import ListPayables from '@/views/ListPayables.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path : '/',
-      redirect : '/login'
+      path: '/',
+      redirect: () => ({ path: '/login', query: { next: '/payableform', message: "" } }) // Define redirect with next parameter
     },
     {
       path: '/login',
       name: 'login',
-      component: LoginView
+      component: LoginView,
+      props: (route) => ({ next: route.query.next }) // Pass the next query parameter as a prop to the LoginView
     },
     {
       path: '/payableform',
@@ -27,6 +27,6 @@ const router = createRouter({
       component: ListPayables
     },
   ]
-})
+});
 
-export default router
+export default router;
