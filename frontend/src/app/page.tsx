f93c3d9a9447e96c17be0892e8398575c5bfe7a3
Yanzaum <yandavid80@gmail.com>
2024-05-26 21:38:47 -0300

    refactor: update import path for PayablesTable component in page.tsx

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,6 @@
-import Image from "next/image";
-import PayablesTable from "./components/PayablesTable";
+import PayablesTable from "./_components/PayablesTable";
 
-export default function Home() {
+export default async function Home() {
   return (
     <main className="min-h-screen md:py-12 container mx-auto">
       <PayablesTable />
