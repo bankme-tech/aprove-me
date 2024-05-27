f47b17dd1dd70ae83d0e8061af86b94191fab027
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2024-05-27 06:02:01 -0300

    feat: add PayableContext

---
+++ frontend/src/context/payable.context.ts
@@ -0,0 +1,24 @@
+"use client";
+
+import { CreatePayableInputDTO } from "@/@core/domain/dtos/payable.dto";
+import { Payable } from "@/@core/domain/entities/payable.entity";
+import { createContext } from "react";
+
+export type PayableContextType = {
+  payables: Payable[];
+  createPayable: (data: CreatePayableInputDTO) => Promise<Payable>;
+  getPayable: (id: string) => Promise<Payable | null>;
+};
+
+const defaultPayable: Payable = {
+  id: "",
+  value: 0,
+  emissionDate: new Date(),
+  assignorId: "",
+};
+
+export const PayableContext = createContext<PayableContextType>({
+  payables: [],
+  createPayable: async () => defaultPayable,
+  getPayable: async () => null,
+});
