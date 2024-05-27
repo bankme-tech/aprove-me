d80f22477ab7deda9f4fdb352630152ee121e063
Yanzaum <yandavid80@gmail.com>
2024-05-26 21:38:29 -0300

    refactor: update import path for Header component in layout.tsx

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Provider from "./provider";
-import Header from "./components/Header";
+import Header from "../components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
