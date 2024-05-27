2a361640245e5c980601bdfca37cd50f484580fd
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-05-27 17:17:45 -0300

    feat(consumer): criando microserviço para consumir fila do rabbitMQ

---
+++ consumer/src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { PayableModule } from './batch/payable/payable.module';
+
+@Module({
+  imports: [PayableModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ consumer/src/main.ts
@@ -0,0 +1,29 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.RMQ,
+      options: {
+        urls: ['amqp://admin:admin@rabbitmq:5672'],
+        queue: 'payable_queue',
+        queueOptions: {
+          durable: false,
+        },
+      },
+    },
+  );
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
+
+  await app.listen();
+}
+bootstrap();
