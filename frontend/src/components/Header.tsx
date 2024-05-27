508ed07093baedea74ecc22a305c08b5618ae404
Yanzaum <yandavid80@gmail.com>
2024-05-27 19:10:34 -0300

    refactor: update navbar color in Header component

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -5,34 +5,34 @@ import { getToken, logout } from "@/lib";
 import { redirect } from "next/navigation";
 
 export default async function Header() {
-    const logged = await getToken();
+  const logged = await getToken();
 
-    return (
-        <Navbar shouldHideOnScroll>
-          <NavbarBrand>
-            <BankmeLogo />
-          </NavbarBrand>
-          <NavbarContent justify="end">
-            <NavbarItem>
-              {logged ? (
-                <form
-                  action={async () => {
-                    "use server";
-                    await logout();
-                    redirect("/");
-                  }}
-                >
-                  <Button type="submit" color="danger" variant="flat">
-                    Sair
-                  </Button>
-                </form>
-              ) : (
-                <Button as={Link} color="primary" href="login" variant="flat">
-                  Entrar
-                </Button>
-              )}
-            </NavbarItem>
-          </NavbarContent>
-        </Navbar>
-    )
+  return (
+    <Navbar shouldHideOnScroll className="bg-gray-100 py-4">
+      <NavbarBrand>
+        <BankmeLogo />
+      </NavbarBrand>
+      <NavbarContent justify="end">
+        <NavbarItem>
+          {logged ? (
+            <form
+              action={async () => {
+                "use server";
+                await logout();
+                redirect("/");
+              }}
+            >
+              <Button type="submit" color="danger" variant="flat">
+                Sair
+              </Button>
+            </form>
+          ) : (
+            <Button as={Link} color="primary" href="login" variant="flat">
+              Entrar
+            </Button>
+          )}
+        </NavbarItem>
+      </NavbarContent>
+    </Navbar>
+  )
 }
