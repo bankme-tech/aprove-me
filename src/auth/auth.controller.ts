7a75183779ba57fbc45a12eb9e99dd3b41d58dc9
Matheus Lopes <inolopesm@gmail.com>
2024-05-27 16:37:21 -0300

    auth with permission

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,25 +3,32 @@ import { ConfigService } from "@nestjs/config";
 import { SignJWT } from "jose";
 
 import { ZodPipe } from "../pipes/zod.pipe";
+import { PrismaProvider } from "../providers/prisma.provider";
 import { AuthInputDTO } from "./auth-input.dto";
 import { AuthInputSchema } from "./auth-input.schema";
 
 @Controller()
 export class AuthController {
   private readonly secret: Uint8Array;
+  private readonly prisma: PrismaProvider;
 
-  constructor(configService: ConfigService) {
+  constructor(configService: ConfigService, prisma: PrismaProvider) {
     const textEncoder = new TextEncoder();
     const secret = configService.getOrThrow("SECRET");
     this.secret = textEncoder.encode(secret);
+    this.prisma = prisma;
   }
 
   @Post("integrations/auth")
   async handle(@Body(new ZodPipe(AuthInputSchema)) input: AuthInputDTO) {
-    const authenticated =
-      input.login === "aprovame" && input.password === "aprovame";
+    const permission = await this.prisma.permission.findFirst({
+      where: {
+        login: input.login,
+        password: input.password,
+      },
+    });
 
-    if (!authenticated) {
+    if (permission === null) {
       throw new BadRequestException("invalid credentials");
     }
 
