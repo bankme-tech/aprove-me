ead13180e87e284d2fcee18ab241033809a5a8e9
Felipe Rodrigues <neath.felipe@gmail.com>
2024-05-26 23:34:38 -0300

    feat: add autocannon test from api

---
+++ api/test/autocannon.js
@@ -0,0 +1,44 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const autocannon = require('autocannon');
+
+// Dados da requisição
+const requestData = [
+  { value: 123.45, assignorId: '0eea6843-fbbc-476b-a72b-6360a69f8286' },
+  { value: 234.56, assignorId: '0eea6843-fbbc-476b-a72b-6360a69f8286' },
+  { value: 234.56, assignorId: '0eea6843-fbbc-476b-a72b-6360a69f8286' },
+];
+const validToken =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIzMDUzNjI4Mi1iYTIwLTQxMjYtODljNC1iODcxMmViODU0YmYiLCJsb2dpbiI6ImZlcm9kZGV2IiwiaWF0IjoxNzE2Nzc0MDQ3LCJleHAiOjE3MTczNzg4NDd9.0YKcfVccCvwW5w7DUas_shfd5473Brk5yKshnmXMgqU';
+// Configurações do Autocannon
+const options = {
+  url: 'http://localhost:3000/integrations/payable/batch',
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    Authorization: `Bearer ${validToken}`,
+  },
+  // Convertendo os dados em JSON antes de enviar
+  body: JSON.stringify(requestData),
+  // Número de requisições a serem feitas
+  connections: 10_000,
+  // O que fazer quando a requisição for bem-sucedida
+  onResponse: (status) => {
+    console.log(`Requisição bem-sucedida: ${status}`);
+  },
+  // O que fazer em caso de erro
+  onError: (error) => {
+    console.error('Erro na requisição:', error);
+  },
+};
+
+// Iniciando o teste com Autocannon
+const instance = autocannon(options);
+
+// Tratamento para quando o teste é concluído
+autocannon.track(instance, { duration: 60 }, (error, result) => {
+  if (error) {
+    console.error('Erro ao rastrear o teste:', error);
+  } else {
+    console.log('Resultados do teste:', result);
+  }
+});
