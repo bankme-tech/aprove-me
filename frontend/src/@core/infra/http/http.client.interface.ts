22fcb130f562043604e7d4aef967dbac657fcaf2
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2024-05-27 18:17:06 -0300

    feat: add HttpClient interface

---
+++ frontend/src/@core/infra/http/http.client.interface.ts
@@ -0,0 +1,21 @@
+export enum HttpMethod {
+  GET = "GET",
+  POST = "POST",
+  DELETE = "DELETE",
+  PATCH = "PATCH",
+}
+
+export type HttpRequestDTO<T = undefined> = {
+  url: string;
+  body: T;
+};
+
+export type HttpResponseDTO<T = undefined> = {
+  statusCode: number;
+  body: T;
+};
+
+export interface IHttpClient<T = unknown, R = unknown> {
+  post: (params: HttpRequestDTO<T>) => Promise<HttpResponseDTO<R>>;
+  get: (params: HttpRequestDTO<undefined>) => Promise<HttpResponseDTO<R>>;
+}
