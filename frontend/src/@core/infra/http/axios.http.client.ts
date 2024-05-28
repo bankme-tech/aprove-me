aba73d53f62e84166a8943376b8cbf18f19f66af
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2024-05-28 18:46:42 -0300

    feat: add error handling in AxiosHttpClient

--- frontend/src/@core/infra/http/axios.http.client.ts
+++ frontend/src/@core/infra/http/axios.http.client.ts
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
 import {
   IHttpClient,
   HttpRequestDTO,
@@ -10,11 +10,19 @@ import { injectable } from "inversify";
 @injectable()
 export class AxiosHttpClient<T, R> implements IHttpClient<T, R> {
   async get(params: HttpRequestDTO<undefined>): Promise<HttpResponseDTO<R>> {
-    const response = await axios.request({
-      url: params.url,
-      method: HttpMethod.GET,
-    });
-
+    let response: AxiosResponse;
+    try {
+      response = await axios.request({
+        url: params.url,
+        method: HttpMethod.GET,
+      });
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        response = error.response as AxiosResponse;
+      } else {
+        throw error;
+      }
+    }
     return {
       statusCode: response.status,
       body: response.data,
@@ -22,12 +30,25 @@ export class AxiosHttpClient<T, R> implements IHttpClient<T, R> {
   }
 
   async post(params: HttpRequestDTO<T>): Promise<HttpResponseDTO<R>> {
-    const response = await axios.request({
-      url: params.url,
-      method: HttpMethod.POST,
-      data: params.body,
-    });
+    let response: AxiosResponse;
+    try {
+      response = await axios.request({
+        url: params.url,
+        method: HttpMethod.POST,
+        data: params.body,
+      });
 
+      return {
+        statusCode: response.status,
+        body: response.data,
+      };
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        response = error.response as AxiosResponse;
+      } else {
+        throw error;
+      }
+    }
     return {
       statusCode: response.status,
       body: response.data,
@@ -35,12 +56,20 @@ export class AxiosHttpClient<T, R> implements IHttpClient<T, R> {
   }
 
   async patch(params: HttpRequestDTO<T>): Promise<HttpResponseDTO<R>> {
-    const response = await axios.request({
-      url: params.url,
-      method: HttpMethod.PATCH,
-      data: params.body,
-    });
-
+    let response: AxiosResponse;
+    try {
+      response = await axios.request({
+        url: params.url,
+        method: HttpMethod.PATCH,
+        data: params.body,
+      });
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        response = error.response as AxiosResponse;
+      } else {
+        throw error;
+      }
+    }
     return {
       statusCode: response.status,
       body: response.data,
@@ -48,11 +77,19 @@ export class AxiosHttpClient<T, R> implements IHttpClient<T, R> {
   }
 
   async delete(params: HttpRequestDTO<undefined>): Promise<HttpResponseDTO<R>> {
-    const response = await axios.request({
-      url: params.url,
-      method: HttpMethod.DELETE,
-    });
-
+    let response: AxiosResponse;
+    try {
+      response = await axios.request({
+        url: params.url,
+        method: HttpMethod.DELETE,
+      });
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        response = error.response as AxiosResponse;
+      } else {
+        throw error;
+      }
+    }
     return {
       statusCode: response.status,
       body: response.data,
