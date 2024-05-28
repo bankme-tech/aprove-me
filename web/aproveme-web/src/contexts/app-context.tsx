666e0b24886445e858ea014676004922054ab151
Lamenkazu <333.erick@gmail.com>
2024-05-28 09:51:06 -0300

    ✨ Update HTTP State

--- web/aproveme-web/src/contexts/app-context.tsx
+++ web/aproveme-web/src/contexts/app-context.tsx
@@ -1,14 +1,16 @@
-import { useMutation } from '@tanstack/react-query'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { ReactNode, useCallback, useMemo, useState } from 'react'
+import { useSearchParams } from 'react-router-dom'
 import { createContext } from 'use-context-selector'
+import { z } from 'zod'
 
+import { deleteAssignor } from '@/api/delete-assignor'
 import { deletePayable } from '@/api/delete-payable'
 import { editAssignor, EditAssignorBody } from '@/api/edit-assignor'
 import { editPayable, EditPayableBody } from '@/api/edit-payable'
-import {
-  fetchAssignorsNames,
-  FetchAssignorsNamesResponse,
-} from '@/api/fetch-assignors-names'
+import { fetchAssignorsNames } from '@/api/fetch-assignors-names'
+import { FetchPayablesResponse } from '@/api/fetch-payables'
+import { GetPayableBody } from '@/api/get-payable'
 import { registerAssignor, RegisterAssignorBody } from '@/api/register-assignor'
 import { registerPayable, RegisterPayableBody } from '@/api/register-payable'
 
@@ -16,22 +18,29 @@ interface AppProviderProps {
   children: ReactNode
 }
 
+interface Assignor {
+  id: string
+  name: string
+}
+
 interface AppContextType {
-  assignorsNames: FetchAssignorsNamesResponse[]
+  assignorsNames: Assignor[]
   registerPayableFn: (data: RegisterPayableBody) => Promise<void>
   refreshAssignorsNames: () => void
   registerAssignorFn: (data: RegisterAssignorBody) => Promise<void>
   deletePayableFn: (payableId: string) => Promise<void>
   editPayableFn: (data: EditPayableBody) => Promise<void>
   editAssignorFn: (data: EditAssignorBody) => Promise<void>
+  deleteAssignorFn: (id: string) => Promise<void>
 }
 
 export const AppContext = createContext({} as AppContextType)
 
 export const AppProvider = ({ children }: AppProviderProps) => {
-  const [assignorsNames, setAssignorsNames] = useState<
-    FetchAssignorsNamesResponse[]
-  >([])
+  const [assignorsNames, setAssignorsNames] = useState<Assignor[]>([])
+
+  const queryClient = useQueryClient()
+  const [searchParams] = useSearchParams()
 
   // Mutations
   const { mutateAsync: fetchAssignorsNamesFn } = useMutation({
@@ -48,9 +57,72 @@ export const AppProvider = ({ children }: AppProviderProps) => {
   })
   const { mutateAsync: editPayableFn } = useMutation({
     mutationFn: editPayable,
+    onSuccess(_, { id, value }) {
+      // Atualiza o Payable Detail
+      const cached = queryClient.getQueryData<GetPayableBody>([
+        'payable-detail',
+        id,
+      ])
+      if (cached) {
+        queryClient.setQueryData<GetPayableBody>(['payable-detail', id], {
+          ...cached,
+          payableWithAssignor: {
+            ...cached.payableWithAssignor,
+            value,
+          },
+        })
+      }
+
+      // Atualiza a tabela de Payables
+      const page = z.coerce.number().parse(searchParams.get('page') ?? '1')
+      const cachedPayables = queryClient.getQueryData<FetchPayablesResponse>([
+        'payables',
+        page,
+      ])
+
+      if (cachedPayables) {
+        // Atualizar o valor específico no array de payables
+        const updatedPayables = cachedPayables.payables.map((payable) =>
+          payable.id === id ? { ...payable, value } : payable,
+        )
+
+        queryClient.setQueryData<FetchPayablesResponse>(['payables', page], {
+          ...cachedPayables,
+          payables: updatedPayables,
+        })
+      }
+    },
   })
   const { mutateAsync: editAssignorFn } = useMutation({
     mutationFn: editAssignor,
+    onSuccess(_, { id, document, email, name, phone, payableId }) {
+      // Atualiza os dados do cedente
+      const cached = queryClient.getQueryData<GetPayableBody>([
+        'payable-detail',
+        payableId,
+      ])
+
+      if (cached) {
+        queryClient.setQueryData<GetPayableBody>(
+          ['payable-detail', payableId],
+          {
+            payableWithAssignor: {
+              ...cached.payableWithAssignor,
+              assignor: {
+                id,
+                document,
+                email,
+                name,
+                phone,
+              },
+            },
+          },
+        )
+      }
+    },
+  })
+  const { mutateAsync: deleteAssignorFn } = useMutation({
+    mutationFn: deleteAssignor,
   })
 
   // Callbacks
@@ -68,6 +140,7 @@ export const AppProvider = ({ children }: AppProviderProps) => {
       deletePayableFn,
       editPayableFn,
       editAssignorFn,
+      deleteAssignorFn,
     }),
     [
       assignorsNames,
@@ -77,6 +150,7 @@ export const AppProvider = ({ children }: AppProviderProps) => {
       deletePayableFn,
       editPayableFn,
       editAssignorFn,
+      deleteAssignorFn,
     ],
   )
 
