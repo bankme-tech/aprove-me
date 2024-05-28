d1ddfd24da34bfceb3d9a7f7292040784f6f0a9b
Arthur Almeida <arthursa.dev@gmail.com>
2024-05-27 23:02:38 -0300

    feat: should get an user by login

--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/infra/database/prisma.service';
 
 @Injectable()
 export class UserService {
-  findOne(login: string) {
-    return;
+  constructor(private prismaService: PrismaService) {}
+
+  async findOne(login: string) {
+    return this.prismaService.user.findUnique({
+      where: {
+        login,
+      },
+    });
   }
 }
