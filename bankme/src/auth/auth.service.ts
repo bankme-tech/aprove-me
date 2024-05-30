b8837f02a310df29f1f577b2e0def3c44f802a3a
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-05-30 12:55:30 -0300

    feat(auth): criando serviço de autenticação

--- bankme/src/auth/auth.service.ts
+++ bankme/src/auth/auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
-import { UsersService } from 'src/users/users.service';
+import { UsersService } from '../users/users.service';
 import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
 
