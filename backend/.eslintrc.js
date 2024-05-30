f10c7ffacf61e449fd1131fbddac34adb006d91d
Arthur Almeida <arthursa.dev@gmail.com>
2024-05-29 22:56:33 -0300

    chore: emulate TypeScript style of exempting names starting with _

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -21,5 +21,17 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      {
+        "args": "all",
+        "argsIgnorePattern": "^_",
+        "caughtErrors": "all",
+        "caughtErrorsIgnorePattern": "^_",
+        "destructuredArrayIgnorePattern": "^_",
+        "varsIgnorePattern": "^_",
+        "ignoreRestSiblings": true
+      }
+    ]
   },
 };
