13434e2dc6fc49008bc407647918d0295ccac017
Arthur Almeida <arthursa.dev@gmail.com>
2024-05-30 15:53:57 -0300

    feat: create jwt passport strategy

--- backend/src/auth/strategies/index.ts
+++ backend/src/auth/strategies/index.ts
@@ -1,1 +1,2 @@
+export * from './jwt.strategy';
 export * from './local.strategy';
---
+++ backend/src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(configService: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: configService.get('JWT_SECRET'),
+    });
+  }
+
+  async validate(payload: any) {
+    return { userId: payload.sub, login: payload.login };
+  }
+}
