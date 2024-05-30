a47a1761c803c6dec8d11ab5effd5663de139bd4
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-05-30 12:55:09 -0300

    feat(auth): criando modulo de autenticação

--- bankme/src/auth/auth.module.ts
+++ bankme/src/auth/auth.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { UsersModule } from 'src/users/users.module';
-import { UsersService } from 'src/users/users.service';
+import { UsersModule } from '../users/users.module';
+import { UsersService } from '../users/users.service';
 
 @Module({
   imports: [UsersModule],
