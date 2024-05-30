9b5dca90b94ced12d85329b8cc35620809c0909d
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-05-30 12:59:41 -0300

    feat(consumer): criando entidades na aplicação consumer

---
+++ consumer/src/batch/entity/Assignor.ts
@@ -0,0 +1,70 @@
+export default class Assignor {
+  private _id: string;
+  private _document: string;
+  private _email: string;
+  private _phone: string;
+  private _name: string;
+
+  constructor(
+    id?: string,
+    document?: string,
+    email?: string,
+    phone?: string,
+    name?: string,
+  ) {
+    this.id = id;
+    this.document = document;
+    this.email = email;
+    this.phone = phone;
+    this.name = name;
+  }
+
+  get id(): string {
+    return this._id;
+  }
+
+  set id(id: string) {
+    this._id = id;
+  }
+
+  get document(): string {
+    return this._document;
+  }
+
+  set document(document: string) {
+    this._document = document;
+  }
+
+  get email(): string {
+    return this._email;
+  }
+
+  set email(email: string) {
+    this._email = email;
+  }
+
+  get phone(): string {
+    return this._phone;
+  }
+
+  set phone(phone: string) {
+    this._phone = phone;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  set name(name: string) {
+    this._name = name;
+  }
+
+  toCreate() {
+    return {
+      document: this.document,
+      email: this.email,
+      phone: this.phone,
+      name: this.name,
+    };
+  }
+}
---
+++ consumer/src/batch/entity/Payable.ts
@@ -0,0 +1,58 @@
+export default class Payable {
+  private _id: string;
+  private _value: number;
+  private _emissionDate: Date;
+  private _assignorId: string;
+
+  constructor(
+    id?: string,
+    value?: number,
+    emissionDate?: Date,
+    assignorId?: string,
+  ) {
+    this.id = id;
+    this.value = value;
+    this.emissionDate = emissionDate;
+    this.assignorId = assignorId;
+  }
+
+  get id(): string {
+    return this._id;
+  }
+
+  set id(id: string) {
+    this._id = id;
+  }
+
+  get value(): number {
+    return this._value;
+  }
+
+  set value(value: number) {
+    this._value = value;
+  }
+
+  get emissionDate(): Date {
+    return this._emissionDate;
+  }
+
+  set emissionDate(emissionDate: Date) {
+    this._emissionDate = emissionDate;
+  }
+
+  get assignorId(): string {
+    return this._assignorId;
+  }
+
+  set assignorId(assignor: string) {
+    this._assignorId = assignor;
+  }
+
+  toCreate() {
+    return {
+      value: this.value,
+      emissionDate: new Date(this.emissionDate),
+      assignorId: this.assignorId,
+    };
+  }
+}
