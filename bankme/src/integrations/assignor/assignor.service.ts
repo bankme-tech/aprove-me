90cf9922f43d2abcf15e6a5df1cc64c4b3b70089
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-06-01 15:03:31 -0300

    refactor(assignor): adicionando busca por email e encriptando senha

--- bankme/src/integrations/assignor/assignor.service.ts
+++ bankme/src/integrations/assignor/assignor.service.ts
@@ -2,12 +2,15 @@ import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import Assignor from '../entity/Assignor';
 import AssignorRepository from './assignor.repository';
 import AssignorDto from '../dto/AssignorDto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AssignorService {
   constructor(private assignorRepository: AssignorRepository) {}
 
   async createAssignorRegister(assignor: Assignor): Promise<AssignorDto> {
+    assignor.password = await bcrypt.hash(assignor.password, 10);
+
     const createdAssignor =
       await this.assignorRepository.createAssignorRegister(assignor);
 
@@ -48,5 +51,15 @@ export class AssignorService {
     }
 
     return;
+  }
+
+  async findAssignorByEmail(email: string): Promise<Assignor> {
+    const user = await this.assignorRepository.findAssignorByEmail(email);
+
+    if (!user) {
+      throw new HttpException('User not found.', HttpStatus.NOT_FOUND);
+    }
+
+    return user;
   }
 }
