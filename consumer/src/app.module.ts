b0d9d741b60f0abbd6f9302eb49b77ad0e44cd2b
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-06-01 15:16:26 -0300

    importando modulos no app

--- consumer/src/app.module.ts
+++ consumer/src/app.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PayableModule } from './batch/payable/payable.module';
+import { PrismaService } from './prisma/prisma.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { EmailModule } from './email/email.module';
 
 @Module({
-  imports: [PayableModule],
+  imports: [PayableModule, PrismaModule, EmailModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
