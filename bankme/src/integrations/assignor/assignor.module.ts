b1fa37d19e4c3dd48530b0ab9cb9c09658eef564
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-06-01 15:02:12 -0300

    refactor(assignor): removendo configuração de middleware

--- bankme/src/integrations/assignor/assignor.module.ts
+++ bankme/src/integrations/assignor/assignor.module.ts
@@ -1,14 +1,8 @@
-import {
-  MiddlewareConsumer,
-  Module,
-  NestModule,
-  RequestMethod,
-} from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { AssignorController } from './assignor.controller';
 import { AssignorService } from './assignor.service';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import AssignorRepository from './assignor.repository';
-import ValidateBodyMiddleware from './middleware/validateBody.middleware';
 
 @Module({
   imports: [PrismaModule],
@@ -16,10 +10,4 @@ import ValidateBodyMiddleware from './middleware/validateBody.middleware';
   providers: [AssignorService, AssignorRepository],
   exports: [AssignorService],
 })
-export class AssignorModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer
-      .apply(ValidateBodyMiddleware)
-      .forRoutes({ path: 'integrations/assignor', method: RequestMethod.POST });
-  }
-}
+export class AssignorModule {}
