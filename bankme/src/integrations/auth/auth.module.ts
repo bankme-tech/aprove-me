ce67373583eebf4d3e4801dad8b3899ae6489171
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-06-02 19:35:51 -0300

    refactor: corrigindo imports

--- bankme/src/integrations/auth/auth.module.ts
+++ bankme/src/integrations/auth/auth.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { AssignorService } from '../assignor/assignor.service';
+import { AssignorModule } from '../assignor/assignor.module';
+import AssignorRepository from '../assignor/assignor.repository';
 
 @Module({
-  imports: [AssignorService],
-  providers: [AuthService, AssignorService],
+  imports: [AssignorModule],
+  providers: [AuthService, AssignorService, AssignorRepository],
   controllers: [AuthController],
 })
 export class AuthModule {}
