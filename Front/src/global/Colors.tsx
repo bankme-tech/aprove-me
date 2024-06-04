f98f333b5c176fa10f2c4fb6309a1f789f7ff884
willersantos <willer.poveda.santos@usp.br>
2024-06-04 14:27:33 -0300

    styles: create global colors

---
+++ Front/src/global/Colors.tsx
@@ -0,0 +1,71 @@
+export type colorsType = {
+  "gold-0": string;
+  "gold-1": string;
+  "gold-2": string;
+  black: string;
+  "black-75%": string;
+  "black-50%": string;
+  "black-25%": string;
+  white: string;
+  "white-75%": string;
+  "white-50%": string;
+  "white-25%": string;
+  "white-10%": string;
+  "gray-0": string;
+  "gray-1": string;
+  "gray-2": string;
+  "gray-3": string;
+  "gray-4": string;
+  "gray-5": string;
+  "blue-0": string;
+  "blue-0-80%": string;
+  "blue-0-25%": string;
+  "blue-1": string;
+  "blue-2": string;
+  "blue-3": string;
+  "blue-4": string;
+  "blue-5": string;
+  alert: string;
+  success: string;
+  "red-0": string;
+  "red-1": string;
+  "green-0": string;
+  "green-1": string;
+  transparent: string;
+};
+
+export const colors = {
+  "gold-0": "#D3A93D",
+  "gold-1": "#E2BD7A",
+  "gold-2": "#FAF2E5",
+  black: "#151515",
+  "black-75%": "rgba(0, 0, 0, 0.75)",
+  "black-50%": "rgba(0, 0, 0, 0.50)",
+  "black-25%": "rgba(0, 0, 0, 0.25)",
+  white: "#ffffff",
+  "white-75%": "rgba(255, 255, 255, 0.75)",
+  "white-50%": "rgba(255, 255, 255, 0.50)",
+  "white-25%": "rgba(255, 255, 255, 0.25)",
+  "white-10%": "rgba(255, 255, 255, 0.25)",
+  "gray-0": "#555555",
+  "gray-1": "#A6A6A6",
+  "gray-2": "#BFBFBF",
+  "gray-3": "#DDDDDD",
+  "gray-4": "#EBEBEB",
+  "gray-5": "#F2F2F2",
+  "blue-0": "#222F44",
+  "blue-0-80%": "rgba(34, 47, 68, 0.80)",
+  "blue-0-25%": "rgba(34, 47, 68, 0.25)",
+  "blue-1": "#4E5969",
+  "blue-2": "#E9EAEC",
+  "blue-3": "#EEEFF1",
+  "blue-4": "#F2F3F4",
+  "blue-5": "#F8F8F9",
+  alert: "#7F2222",
+  success: "#227F52",
+  "red-0": "#BF3333",
+  "red-1": "#F8EAEA",
+  "green-0": "#CDF4E1",
+  "green-1": "#F5FDF9",
+  transparent: "rgba(255, 255, 255, 0)"
+};
