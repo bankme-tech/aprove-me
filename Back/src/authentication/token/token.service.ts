173459a7f49e682024137437d5fb489795036eac
willersantos <willer.poveda.santos@usp.br>
2024-06-05 14:21:57 -0300

    feat: create generate token

---
+++ Back/src/authentication/token/token.service.ts
@@ -0,0 +1,38 @@
+import { DateUtils } from "@/shared/utils/date";
+import { HandleHttpError } from "@/shared/utils/handleError";
+import { Injectable, Logger } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { sign } from "jsonwebtoken";
+import { UserDto } from "../auth/dto/userDto";
+
+@Injectable()
+export class TokenService {
+    private readonly logger = new Logger(TokenService.name);
+    private readonly quantityMinutesToken = this.configService.get("auth.expirationMinutes");
+    private readonly secretToken = this.configService.get("auth.jwtSecret");
+
+    constructor(private readonly configService: ConfigService) {}
+
+    public generateAccessToken(user: UserDto): string {
+        try {
+            this.logger.log(`Start service generateAccessToken - Request - ${JSON.stringify({ user })}`);
+
+            const now = DateUtils.today();
+
+            const payload = {
+                sub: JSON.stringify(user),
+                exp: new Date(now.setMinutes(now.getMinutes() + this.quantityMinutesToken)).getTime(),
+                iat: now.getTime()
+            };
+
+            const token = sign(payload, this.secretToken);
+
+            this.logger.log("End service generateAccessToken");
+
+            return token;
+        } catch (error) {
+            this.logger.error(`Error service generateAccessToken - Error - ${JSON.stringify(error)}`);
+            throw HandleHttpError.next(error);
+        }
+    }
+}
