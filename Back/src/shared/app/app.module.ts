37caf85b558bb403d4ee73f622867ee1e8572a0b
willersantos <willer.poveda.santos@usp.br>
2024-06-05 14:22:19 -0300

    feat: add new modules

--- Back/src/shared/app/app.module.ts
+++ Back/src/shared/app/app.module.ts
@@ -1,23 +1,27 @@
+import { ReceivableModule } from "@/app/receivable/receivable.module";
+import { AuthModule } from "@/authentication/auth/auth.module";
+import { TokenModule } from "@/authentication/token/token.module";
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import "dotenv/config";
-import { ReceivableModule } from "src/app/receivable/receivable.module";
 import { AssignorModule } from "../../app/assignor/assignor.module";
 import appConfig from "../config/app.config";
+import authConfig from "../config/auth.config";
 import { PrismaModule } from "../prisma/prisma.module";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 
-const InternModules = [AssignorModule, PrismaModule, ReceivableModule];
+const Modules = [AssignorModule, AuthModule, PrismaModule, ReceivableModule, TokenModule];
+const Configs = [appConfig, authConfig];
 
 @Module({
     imports: [
         ConfigModule.forRoot({
             isGlobal: true,
-            load: [appConfig],
+            load: Configs,
             envFilePath: [".env"]
         }),
-        ...InternModules
+        ...Modules
     ],
     controllers: [AppController],
     providers: [AppService]
