600f366c755fa8be9a03edd16a818498c2341f8c
willersantos <willer.poveda.santos@usp.br>
2024-06-06 13:15:55 -0300

    fix: adjust dev packages

---
+++ Back/prisma/migrations/20240606161353_init/migration.sql
@@ -0,0 +1,10 @@
+/*
+  Warnings:
+
+  - You are about to drop the `User` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+PRAGMA foreign_keys=off;
+DROP TABLE "User";
+PRAGMA foreign_keys=on;
