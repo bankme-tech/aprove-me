c7729d97e4593b8736c7693cd27f7cc9e81a0232
willersantos <willer.poveda.santos@usp.br>
2024-06-06 23:36:50 -0300

    feat: add new modules

--- Back/src/shared/app/app.module.ts
+++ Back/src/shared/app/app.module.ts
@@ -1,19 +1,34 @@
 import { ReceivableModule } from "@/app/receivable/receivable.module";
 import { AuthModule } from "@/authentication/auth/auth.module";
 import { TokenModule } from "@/authentication/token/token.module";
+import { UserModule } from "@/authentication/user/user.module";
+import { ReceivableConsumerModule } from "@/consumer/receivable/receivable.consumer.module";
+import { ReceivableProducerModule } from "@/producer/receivable/receivable.producer.module";
+import { EmailModule } from "@/provider/email/email.module";
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import "dotenv/config";
 import { AssignorModule } from "../../app/assignor/assignor.module";
 import appConfig from "../config/app.config";
 import authConfig from "../config/auth.config";
+import emailConfig from "../config/email.config";
+import rabbitConfig from "../config/rabbit.config";
 import { PrismaModule } from "../prisma/prisma.module";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
-import { UserModule } from "@/authentication/user/user.module";
 
-const Modules = [AssignorModule, AuthModule, PrismaModule, ReceivableModule, TokenModule, UserModule];
-const Configs = [appConfig, authConfig];
+const Modules = [
+    AssignorModule,
+    AuthModule,
+    EmailModule,
+    PrismaModule,
+    ReceivableConsumerModule,
+    ReceivableModule,
+    ReceivableProducerModule,
+    TokenModule,
+    UserModule
+];
+const Configs = [appConfig, authConfig, emailConfig, rabbitConfig];
 
 @Module({
     imports: [
