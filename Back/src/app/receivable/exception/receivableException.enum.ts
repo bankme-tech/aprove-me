e48d657271e9cde2a61f1e3a8c03ebf8dda06f70
willersantos <willer.poveda.santos@usp.br>
2024-06-06 23:36:38 -0300

    feat: add batch method

--- Back/src/app/receivable/exception/receivableException.enum.ts
+++ Back/src/app/receivable/exception/receivableException.enum.ts
@@ -1,4 +1,5 @@
 export enum ReceivableException {
     ID_NOT_FOUND = "ID não encontrado, contate o suporte",
-    ASSIGNOR_EMAIL_NOT_FOUND = "Email do cedente não encontrado, contate o suporte"
+    ASSIGNOR_EMAIL_NOT_FOUND = "Email do cedente não encontrado, contate o suporte",
+    EXCEED_BATCH_LIMIT = "O limite para salvar recebíveis em lote é 10000"
 }
