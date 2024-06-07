364dc361959ef4e722e7569b1767cb6436ee5412
willersantos <willer.poveda.santos@usp.br>
2024-06-06 23:34:11 -0300

    feat: add consumer config

--- Back/src/main.ts
+++ Back/src/main.ts
@@ -3,10 +3,15 @@ import { ConfigService } from "@nestjs/config";
 import { NestFactory } from "@nestjs/core";
 import "dotenv/config";
 import { AppModule } from "./shared/app/app.module";
+import { consumerConfigInit } from "./shared/config/consumer.config";
 import { securityConfigInit } from "./shared/config/security.config";
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule, { cors: true });
+
+    process.on("uncaughtException", (error) => {
+        console.error("Unhandled Error - Something unexpected occurred.", error);
+    });
 
     app.useGlobalPipes(
         new ValidationPipe({
@@ -22,9 +27,11 @@ async function bootstrap() {
     const configService = app.get(ConfigService);
 
     securityConfigInit(app);
+    consumerConfigInit(app);
 
     const port = configService.get("app.port");
 
+    await app.startAllMicroservices();
     await app.listen(port);
 }
 
